f48fdce142718608f338efe1618eef546ff3a407
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-01-10 11:59:16 +0900

    Update 20190110.php

--- www/20190110.php
+++ www/20190110.php
@@ -6,6 +6,8 @@ $requesturi = $_SERVER['REQUEST_URI'];
 $time_start = microtime(true);
 error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
 
+$mu = new MyUtils();
+
 /*
 require_once 'XML/RPC2/Client.php';
   
@@ -22,7 +24,15 @@ $result = $client->info('mt.supportedMethods');
 error_log(print_r($result, true));
 */
 
+//<methodCall><methodName>mt.supportedMethods</methodName><params></params></methodCall>
 
+$options = [CURLOPT_POST => true,
+            CURLOPT_POSTFIELDS => '<methodCall><methodName>mt.supportedMethods</methodName><params></params></methodCall>',
+            ];
+
+$res = $mu->get_contents_nocache('http://blog.fc2.com/xmlrpc.php', $options);
+
+error_log($res);
 
 $time_finish = microtime(true);
 error_log("${pid} FINISH " . substr(($time_finish - $time_start), 0, 6) . 's');
