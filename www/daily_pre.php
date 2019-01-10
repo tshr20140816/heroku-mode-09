57820f57e72e8ef3abcaa96be07cf7038194d895
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-01-11 08:57:04 +0900

    Update daily_pre.php

--- www/daily_pre.php
+++ www/daily_pre.php
@@ -134,5 +134,7 @@ for ($i = 11; $i > -1; $i--) {
 }
 
 $time_finish = microtime(true);
-error_log("${pid} FINISH " . substr(($time_finish - $time_start), 0, 6) . 's');
+$mu->post_blog($requesturi . ' ' . substr(($time_finish - $time_start), 0, 6) . 's');
+error_log("${pid} FINISH " . substr(($time_finish - $time_start), 0, 6) . 's ' . substr((microtime(true) - $time_start), 0, 6) . 's');
+
 exit();
