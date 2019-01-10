369a8d841ed4b42d3eba571b6d17ad2f27965da2
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-01-10 09:22:46 +0900

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -15,6 +15,10 @@ popd
 wget -q https://github.com/squizlabs/PHP_CodeSniffer/releases/download/3.4.0/phpcs.phar
 wget -q https://github.com/squizlabs/PHP_CodeSniffer/releases/download/3.4.0/phpcbf.phar
 
+whereis pear
+
+php/bin/pear install XML_RPC
+
 chmod 755 ./start_web.sh
 
 date
