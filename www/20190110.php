ff6de764fe7e893a011873fb000d534cb4f6ecc9
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-01-10 13:17:07 +0900

    Update 20190110.php

--- www/20190110.php
+++ www/20190110.php
@@ -10,6 +10,7 @@ $mu = new MyUtils();
 
 require_once 'XML/RPC2/Client.php';
 
+/*
 $url = 'https://blog.fc2.com/xmlrpc.php';
 error_log($url);
 $client = XML_RPC2_Client::create(
@@ -19,6 +20,7 @@ $client = XML_RPC2_Client::create(
 
 $result = $client->supportedMethods('', getenv('FC2_ID'), getenv('FC2_PASSWORD'));
 error_log(print_r($result, true));
+*/
 
 /*
 $post_data = '<?xml version="1.0" ?><methodCall><methodName>mt.supportedMethods</methodName>'
@@ -34,6 +36,18 @@ $res = $mu->get_contents_nocache('http://blog.fc2.com/xmlrpc.php', $options);
 
 error_log($res);
 */
+
+$url = 'https://blog.fc2.com/xmlrpc.php';
+error_log($url);
+$client = XML_RPC2_Client::create(
+  $url,
+  ['prefix' => 'metaWeblog.']
+);
+
+$options = ['title' => 'タイトルテストRPCタイトル', 'description' => '本文テストRPC本文'];
+
+$result = $client->newPost('', getenv('FC2_ID'), getenv('FC2_PASSWORD'), $options, 1);
+error_log(print_r($result, true));
 
 $time_finish = microtime(true);
 error_log("${pid} FINISH " . substr(($time_finish - $time_start), 0, 6) . 's');
