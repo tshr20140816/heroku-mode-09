4b25ea9346b75fd8312ef99d913cbceaaceead70
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-01-10 09:37:50 +0900

    Update 20190110.php

--- www/20190110.php
+++ www/20190110.php
@@ -6,8 +6,8 @@ $requesturi = $_SERVER['REQUEST_URI'];
 $time_start = microtime(true);
 error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
 
-//require_once 'XML_RPC2/Client.php';
-require_once 'XML/RPC2/Client.php';
+require_once 'XML_RPC2/Client.php';
+//require_once 'XML/RPC2/Client.php';
   
 $client = XML_RPC2_Client('http://xmlrpc.example.com/1.0/', 'example.');
 
