3251f790ad7fa2ba0c9cc8c11deb872e7596fd9c
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-01-10 09:08:12 +0900

    Update 20180110.php

--- www/20180110.php
+++ www/20180110.php
@@ -6,7 +6,9 @@ $requesturi = $_SERVER['REQUEST_URI'];
 $time_start = microtime(true);
 error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
 
-$client = new XML_RPC_client('/xmlrpc.php', 'blog.fc2.com', 80);
+// $client = new XML_RPC_client('/xmlrpc.php', 'blog.fc2.com', 80);
+
+phpinfo();
 
 $time_finish = microtime(true);
 error_log("${pid} FINISH " . substr(($time_finish - $time_start), 0, 6) . 's');
