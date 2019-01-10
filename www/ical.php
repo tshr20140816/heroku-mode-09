648d04a90616698b859f8d476948bb28ae0e3b14
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-01-11 08:58:24 +0900

    Update ical.php

--- www/ical.php
+++ www/ical.php
@@ -4,6 +4,7 @@ include(dirname(__FILE__) . '/../classes/MyUtils.php');
 
 $pid = getmypid();
 $requesturi = $_SERVER['REQUEST_URI'];
+$time_start = microtime(true);
 error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
 
 $ueragent = $_SERVER['HTTP_USER_AGENT'];
@@ -37,5 +38,8 @@ if ($ical_data == '') {
     echo gzdecode(base64_decode($ical_data));
 }
 
-error_log("${pid} FINISH");
+$time_finish = microtime(true);
+$mu->post_blog('/ical.php ' . substr(($time_finish - $time_start), 0, 6) . 's');
+error_log("${pid} FINISH " . substr(($time_finish - $time_start), 0, 6) . 's ' . substr((microtime(true) - $time_start), 0, 6) . 's');
+
 exit();
