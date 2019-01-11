d745cee5bfd743b9910f737bbdef310da6edc6eb
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-01-11 10:42:21 +0900

    Update MyUtils.php

--- classes/MyUtils.php
+++ classes/MyUtils.php
@@ -355,6 +355,44 @@ __HEREDOC__;
         $result = $client->newPost($blogid, getenv('WORDPRESS_USERNAME'), getenv('WORDPRESS_PASSWORD'), $post_data);
         error_log($log_prefix . 'RESULT : ' . print_r($result, true));
     }
+    
+    function post_blog_fc2($message_)
+    {
+        $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
+        
+        $url = 'https://blog.fc2.com/xmlrpc.php';
+        error_log($log_prefix . 'url : ' . $url);
+        $client = XML_RPC2_Client::create(
+            $url,
+            ['prefix' => 'metaWeblog.']);
+        
+        error_log($log_prefix . 'xmlrpc : newPost');
+        $options = ['title' => date('Y/m/d H:i:s', strtotime('+9 hours')) . " ${message_}", 'description' => '.'];
+        $result = $client->newPost('', getenv('FC2_ID'), getenv('FC2_PASSWORD'), $options, 1);
+        error_log($log_prefix . 'RESULT : ' . print_r($result, true));
+    }
+    
+    function post_blog_wordpress($message_)
+    {
+        $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
+        
+        $url = 'https://' . getenv('WORDPRESS_USERNAME') . '.wordpress.com/xmlrpc.php';
+        error_log($log_prefix . 'url : ' . $url);
+        $client = XML_RPC2_Client::create($url, ['prefix' => 'wp.']);
+        
+        error_log($log_prefix . 'xmlrpc : getUsersBlogs');
+        $result = $client->getUsersBlogs(getenv('WORDPRESS_USERNAME'), getenv('WORDPRESS_PASSWORD'));
+        error_log($log_prefix . 'RESULT : ' . print_r($result, true));
+        
+        $blogid = $result[0]['blogid'];
+        
+        error_log($log_prefix . 'xmlrpc : newPost');
+        $post_data = ['post_title' => date('Y/m/d H:i:s', strtotime('+9 hours')) . " ${message_}",
+                      'post_content' => '.',
+                      'post_status' => 'publish'];        
+        $result = $client->newPost($blogid, getenv('WORDPRESS_USERNAME'), getenv('WORDPRESS_PASSWORD'), $post_data);
+        error_log($log_prefix . 'RESULT : ' . print_r($result, true));
+    }
 
     function get_contents($url_, $options_ = null, $is_cache_search = false)
     {
