d413d1ff4d80a521558ecdbf7e6bd0a7e2188c8f
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-01-17 16:28:05 +0900

    Update 20190117.php

--- www/20190117.php
+++ www/20190117.php
@@ -10,10 +10,23 @@ error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
 
 $mu = new MyUtils();
 
+if (!isset($_GET['c']) || $_GET['c'] === '' || is_array($_GET['c'])) {
+    exit();
+}
 
+$count = (int)$_GET['c'];
+
+error_log("COUNT : ${count}");
+
+if ($count !== 0) {
+    error_log('SLEEP');
+    sleep(25);
+} else {
+    error_log('OWARI');
+}
 
 $time_finish = microtime(true);
-$mu->post_blog_wordpress($requesturi . ' ' . substr(($time_finish - $time_start), 0, 6) . 's');
+// $mu->post_blog_wordpress($requesturi . ' ' . substr(($time_finish - $time_start), 0, 6) . 's');
 error_log("${pid} FINISH " . substr(($time_finish - $time_start), 0, 6) . 's ' . substr((microtime(true) - $time_start), 0, 6) . 's');
 
 exit();
