a1771a7d87ba2bc54d4426a52745000f0c0d3b4b
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-01-17 16:20:40 +0900

    Create 20190117.php

---
+++ www/20190117.php
@@ -0,0 +1,19 @@
+<?php
+
+include(dirname(__FILE__) . '/../classes/MyUtils.php');
+
+$pid = getmypid();
+$requesturi = $_SERVER['REQUEST_URI'];
+$time_start = microtime(true);
+
+error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
+
+$mu = new MyUtils();
+
+
+
+$time_finish = microtime(true);
+$mu->post_blog_wordpress($requesturi . ' ' . substr(($time_finish - $time_start), 0, 6) . 's');
+error_log("${pid} FINISH " . substr(($time_finish - $time_start), 0, 6) . 's ' . substr((microtime(true) - $time_start), 0, 6) . 's');
+
+exit();
