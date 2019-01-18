a094f664d8196dc3268e8ccdd6a1857cb027fd68
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-01-18 10:22:02 +0900

    Update 20190118.php

--- www/20190118.php
+++ www/20190118.php
@@ -14,6 +14,17 @@ $res = $mu->get_contents($url);
 
 error_log($res);
 
+$doc = new DOMDocument();
+$doc->loadXML($res);
+
+$xpath = new DOMXpath($doc);
+$elements = $xpath->query("*/entry/title");
+
+foreach ($elements as $element) {
+    error_log($element->nodeName);
+}
+
+
 $time_finish = microtime(true);
 // $mu->post_blog_wordpress($requesturi . ' ' . substr(($time_finish - $time_start), 0, 6) . 's');
 error_log("${pid} FINISH " . substr(($time_finish - $time_start), 0, 6) . 's ' . substr((microtime(true) - $time_start), 0, 6) . 's');
