a42d00ee83ee052fe42754207b919f0bcd4afc05
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-01-18 10:37:55 +0900

    Update 20190118.php

--- www/20190118.php
+++ www/20190118.php
@@ -32,6 +32,10 @@ foreach ($elements as $element) {
 
 error_log(print_r($list_version, true));
 
+krsort($list_version);
+
+error_log(print_r($list_version, true));
+
 $time_finish = microtime(true);
 // $mu->post_blog_wordpress($requesturi . ' ' . substr(($time_finish - $time_start), 0, 6) . 's');
 error_log("${pid} FINISH " . substr(($time_finish - $time_start), 0, 6) . 's ' . substr((microtime(true) - $time_start), 0, 6) . 's');
