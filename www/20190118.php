88ef613432c1009e035aac6f13963261037ea00d
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-01-18 11:44:14 +0900

    Update 20190118.php

--- www/20190118.php
+++ www/20190118.php
@@ -9,6 +9,7 @@ error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
 
 $mu = new MyUtils();
 
+/*
 $url = 'https://github.com/apache/httpd/releases.atom';
 $res = $mu->get_contents($url);
 
@@ -40,9 +41,50 @@ $res = $mu->get_contents($url);
 $rc = preg_match('/<strong><a href="http:\/\/httpd.apache.org">Apache<\/a>(.+?)<\/strong> \((.+?)\) and <strong>/s', $res, $match);
 // error_log(print_r($match, true));
 error_log($match[2]);
+*/
+check_version_apache($mu);
 
 $time_finish = microtime(true);
 // $mu->post_blog_wordpress($requesturi . ' ' . substr(($time_finish - $time_start), 0, 6) . 's');
 error_log("${pid} FINISH " . substr(($time_finish - $time_start), 0, 6) . 's ' . substr((microtime(true) - $time_start), 0, 6) . 's');
 
 exit();
+
+function check_version_apache($mu_)
+{
+    $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
+    
+    $url = 'https://github.com/apache/httpd/releases.atom';
+    $res = $mu_->get_contents($url);
+    
+    $doc = new DOMDocument();
+    $doc->loadXML($res);
+    
+    $xpath = new DOMXpath($doc);
+    $xpath->registerNamespace('ns', 'http://www.w3.org/2005/Atom');
+    
+    $elements = $xpath->query("//ns:entry/ns:title");
+    
+    $list_version = [];
+    foreach ($elements as $element) {
+        $tmp = $element->nodeValue;
+        $tmp = explode('.', $tmp);
+        $list_version[(int)$tmp[0] * 1000000 + (int)$tmp[1] * 1000 + (int)$tmp[2]] = $element->nodeValue;
+    }
+    krsort($list_version);
+    $version_latest = array_shift($list_version);
+    
+    $res = file_get_contents('/tmp/apache_current_version');
+    $version_current = trim(str_replace(["\r\n", "\r", "\n", '   ', '  '], ' ', $res));
+    
+    $url = 'https://devcenter.heroku.com/articles/php-support';
+    $res = $mu_->get_contents($url);
+
+    $rc = preg_match('/<strong><a href="http:\/\/httpd.apache.org">Apache<\/a>(.+?)<\/strong> \((.+?)\) and <strong>/s', $res, $match);
+    $version_support = $match[2];
+
+    error_log($log_prefix . '$version_latest : ' . $version_latest);
+    error_log($log_prefix . '$version_support : ' . $version_support);
+    error_log($log_prefix . '$version_current : ' . $version_current);
+}
+
