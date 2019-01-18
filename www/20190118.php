8a4e484c9febb30ca718144a220fb52149069dd1
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-01-18 10:43:49 +0900

    Update 20190118.php

--- www/20190118.php
+++ www/20190118.php
@@ -12,8 +12,6 @@ $mu = new MyUtils();
 $url = 'https://github.com/apache/httpd/releases.atom';
 $res = $mu->get_contents($url);
 
-error_log($res);
-
 $doc = new DOMDocument();
 $doc->loadXML($res);
 
@@ -24,19 +22,13 @@ $elements = $xpath->query("//ns:entry/ns:title");
 
 $list_version = [];
 foreach ($elements as $element) {
-    error_log($element->nodeValue);
     $tmp = $element->nodeValue;
     $tmp = explode('.', $tmp);
     $list_version[(int)$tmp[0] * 1000000 + (int)$tmp[1] * 1000 + (int)$tmp[2]] = $element->nodeValue;
 }
-
-error_log(print_r($list_version, true));
-
 krsort($list_version);
-
-error_log(print_r($list_version, true));
-
-error_log(array_shift($list_version));
+$version_latest = array_shift($list_version);
+error_log($version_latest);
 
 $time_finish = microtime(true);
 // $mu->post_blog_wordpress($requesturi . ' ' . substr(($time_finish - $time_start), 0, 6) . 's');
