95129fc9639c51f0881f9fa7d30ece8809d8565c
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-01-18 10:14:44 +0900

    Create 20190118.php

---
+++ www/20190118.php
@@ -0,0 +1,21 @@
+<?php
+
+include(dirname(__FILE__) . '/../classes/MyUtils.php');
+
+$pid = getmypid();
+$requesturi = $_SERVER['REQUEST_URI'];
+$time_start = microtime(true);
+error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
+
+$mu = new MyUtils();
+
+$url = 'https://github.com/apache/httpd/releases.atom';
+$res = $mu->get_contents($url);
+
+error_log($res);
+
+$time_finish = microtime(true);
+// $mu->post_blog_wordpress($requesturi . ' ' . substr(($time_finish - $time_start), 0, 6) . 's');
+error_log("${pid} FINISH " . substr(($time_finish - $time_start), 0, 6) . 's ' . substr((microtime(true) - $time_start), 0, 6) . 's');
+
+exit();
