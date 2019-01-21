3dd116d4ec8f87761ff915452c6731c69be78e50
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-01-21 12:23:23 +0900

    Update 20190118.php

--- www/20190118.php
+++ www/20190118.php
@@ -33,6 +33,11 @@ function check_version_apache($mu_)
     $url = 'https://www.nao.ac.jp/astro/sky/2019/01.html';
     $res = $mu_->get_contents($url);
     
-    error_log($res);
+    // error_log($res);
+    
+    $rc = preg_match_all('/<tr>(.+?)<\/tr>/s', $res, $matches,  PREG_SET_ORDER);
+    
+    error_log(print_r($matches, true));
+    
 }
 
