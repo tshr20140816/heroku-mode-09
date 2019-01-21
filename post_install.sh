2447c635636688055de52c1666d6272e74aec7e5
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-01-21 09:40:45 +0900

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -4,8 +4,8 @@ set -x
 
 date
 
-# pear channel-update pear.php.net
-# pear install XML_RPC2 &
+pear channel-update pear.php.net
+pear install XML_RPC2 &
 
 # ***** phppgadmin *****
 
@@ -18,11 +18,11 @@ popd
 
 # ***** XML_RPC2 *****
 
-mkdir lib
-pushd lib
-git clone --depth=1 -b 1.1.4 https://github.com/pear/XML_RPC2.git .
-ls -lang
-popd
+# mkdir lib
+# pushd lib
+# git clone --depth=1 -b 1.1.4 https://github.com/pear/XML_RPC2.git .
+# ls -lang
+# popd
 
 wget -q https://github.com/squizlabs/PHP_CodeSniffer/releases/download/3.4.0/phpcs.phar
 wget -q https://github.com/squizlabs/PHP_CodeSniffer/releases/download/3.4.0/phpcbf.phar
