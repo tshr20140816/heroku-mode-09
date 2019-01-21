e5df70034d593f2af8d3b2faf029ff2644e2ff02
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-01-21 09:25:32 +0900

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -4,23 +4,31 @@ set -x
 
 date
 
-pear channel-update pear.php.net
-
-pear install XML_RPC2 &
+# pear channel-update pear.php.net
+# pear install XML_RPC2 &
 
 # ***** phppgadmin *****
 
 pushd www
-git clone --depth 1 https://github.com/phppgadmin/phppgadmin.git phppgadmin
+# git clone --depth 1 https://github.com/phppgadmin/phppgadmin.git phppgadmin
+git clone --depth=1 -b REL_5-6-0  https://github.com/phppgadmin/phppgadmin.git phppgadmin
 cp ../config.inc.php phppgadmin/conf/
-cp ../Connection.php phppgadmin/classes/database/
+# cp ../Connection.php phppgadmin/classes/database/
 popd
+
+# ***** XML_RPC2 *****
+
+mkdir lib
+push lib
+git clone --depth=1 -b 1.1.4 https://github.com/pear/XML_RPC2.git .
+ls -lang
+pop
 
 wget -q https://github.com/squizlabs/PHP_CodeSniffer/releases/download/3.4.0/phpcs.phar
 wget -q https://github.com/squizlabs/PHP_CodeSniffer/releases/download/3.4.0/phpcbf.phar
 
 chmod 755 ./start_web.sh
 
-wait
+# wait
 
 date
