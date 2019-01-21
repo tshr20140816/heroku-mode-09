6f81b26b0d21f2e96d934d3f4bafbcdfc837e8b9
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-01-21 15:30:07 +0900

    Update 20190118.php

--- www/20190118.php
+++ www/20190118.php
@@ -11,7 +11,7 @@ $mu = new MyUtils();
 
 $access_token = $mu->get_access_token();
 
-check_version_apache2($mu);
+check_version_apache($mu);
 
 $time_finish = microtime(true);
 error_log("${pid} FINISH " . ($time_finish - $time_start) . 's ');
@@ -31,9 +31,10 @@ function check_version_apache2($mu_)
     $add_task_template = '{"title":"__TITLE__","duedate":"__DUEDATE__","context":"__CONTEXT__","tag":"WEATHER2","folder":"'
       . $folder_id_label . '"}';
     
-    $y = date('Y');
+    $y = (int)date('Y') - 1;
     for ($i = 0; $i < 3; $i++) {
-        $url = 'https://e-moon.net/calendar_list/calendar_moon_' . ($y + $i) . '/';
+        $y++;
+        $url = "https://e-moon.net/calendar_list/calendar_moon_${y}/";
         if ($i > 0) {
             $res = $mu_->get_contents($url, [CURLOPT_NOBODY => true]);
             if ($res != '') {
@@ -45,7 +46,7 @@ function check_version_apache2($mu_)
         $pattern = '/<td class="embed_link_to_star_mall_fullmoon">(\d+).+?(\d+).+?(\d+).+?(\d+)(.*?)</s';
         $rc = preg_match_all($pattern, $res, $matches,  PREG_SET_ORDER);
         
-        foreach($matches as $match) {
+        foreach ($matches as $match) {
             for ($j = 1; $j < 5; $j++) {
                 $match[$j] = str_pad($match[$j], 2, '0', STR_PAD_LEFT);
             }
@@ -54,7 +55,10 @@ function check_version_apache2($mu_)
                 $match[5] = '満月';
             }
             $title = $match[1] . '/' . $match[2] . ' ' . $match[3] . ':' . $match[4] . ' ' .  $match[5] . ' ★';
-            $timestamp = mktime(0, 0, 0, $match[1], $match[2], $y + $i);
+            $timestamp = mktime(0, 0, 0, $match[1], $match[2], $y);
+            if ($timestamp < strtotime('+3 days')) {
+                continue;
+            }
             $hash = date('Ymd', $timestamp) . hash('sha512', $title);
             
             $tmp = str_replace('__TITLE__', $title, $add_task_template);
@@ -72,57 +76,37 @@ function check_version_apache($mu_)
 {
     $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
     
-    $url = 'https://e-moon.net/calendar_list/calendar_moon_2019/';
-    $res = $mu_->get_contents($url);
+    // Get Folders
+    $folder_id_label = $mu_->get_folder_id('LABEL');
+    // Get Contexts
+    $list_context_id = $mu_->get_contexts();
     
-    // error_log($res);
+    $list_add_task = [];
+    $add_task_template = '{"title":"__TITLE__","duedate":"__DUEDATE__","context":"__CONTEXT__","tag":"WEATHER2","folder":"'
+      . $folder_id_label . '"}';
     
-    $rc = preg_match_all('/<td class="embed_link_to_star_mall_fullmoon">(\d+).+?(\d+).+?(\d+).+?(\d+)(.*?)</s', $res, $matches,  PREG_SET_ORDER);
-    
-    // error_log(print_r($matches, true));
-    
-    $y = 2019;
-    foreach($matches as $match) {
-        for ($i = 1; $i < 5; $i++) {
-            $match[$i] = str_pad($match[$i], 2, '0', STR_PAD_LEFT);
+    $y = date('Y');
+    $m = date('m');
+    for ($i = 0; $i < 36; $i++) {
+        $url = "https://www.nao.ac.jp/astro/sky/${y}/" . str_pad($m, 2, '0', STR_PAD_LEFT) . '.html;
+        $res = $mu_->get_contents($url, [CURLOPT_NOBODY => true]);
+        if ($res != '') {
+            break;
         }
-        $match[5] = trim($match[5]);
-        if ($match[5] == '') {
-            $match[5] = '満月';
+        $res = $mu_->get_contents($url);
+
+        $pattern = '/<tr>.+?<td.*?>(\d+).+?<\/td>.*?<td.*?>(.+?)<\/td>.*?<\/tr>/s';
+        $rc = preg_match_all($pattern, $res, $matches,  PREG_SET_ORDER);
+
+        foreach ($matches as $match) {
+            $title = str_pad($m, 2, '0', STR_PAD_LEFT) . '/' . str_pad($match[1], 2, '0', STR_PAD_LEFT) . ' ' . mb_convert_kana(strip_tags($match[2]), 'a') . ' ★';
+            $timestamp = mktime(0, 0, 0, $m, $match[1], $y);
+            
         }
-        $list[] = $match[1] . '/' . $match[2] . ' ' . $match[3] . ':' . $match[4] . ' ' .  $match[5] . ' ★';
+        $m = $m == 12 ? 1 : $m + 1;
+        $y = $m == 1 ? $y + 1 : $y;
     }
-    error_log(print_r($list, true));
     
-    $url = 'https://www.nao.ac.jp/astro/sky/2019/01.html';
-    $res = $mu_->get_contents($url);
-    
-    // error_log($res);
-    
-    $rc = preg_match_all('/<tr>.+?<td.*?>(\d+).+?<\/td>.*?<td.*?>(.+?)<\/td>.*?<\/tr>/s', $res, $matches,  PREG_SET_ORDER);
-    
-    // error_log(print_r($matches, true));
-    
-    $y = 2019;
-    $m = 1;
-    foreach($matches as $match) {
-        $list[] = str_pad($m, 2, '0', STR_PAD_LEFT) . '/' . str_pad($match[1], 2, '0', STR_PAD_LEFT) . ' ' . mb_convert_kana(strip_tags($match[2]), 'a') . ' ★';
-        /*
-        array_shift($match);
-        $match[0] = trim($match[0], '日');
-        $match[1] = mb_convert_kana(strip_tags($match[1]), 'a');
-        error_log(print_r($match, true));
-        */
-    }
-    error_log(print_r($list, true));
-    
-    $url = 'https://www.nao.ac.jp/astro/sky/2020/01.html';
-    $res = $mu_->get_contents($url, [CURLOPT_NOBODY => true]);
-    
-    error_log($res);
-    
-    $url = 'https://e-moon.net/calendar_list/calendar_moon_2021/';
-    $res = $mu_->get_contents($url, [CURLOPT_NOBODY => true]);
-    
-    error_log($res);
+    error_log($log_prefix . 'FULL MOON : ' . print_r($list_add_task, true));
+    return $list_add_task;
 }
