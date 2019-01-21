e8e021e6c99f54126223d369f03b7132bba6690b
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-01-21 15:44:35 +0900

    Update 20190118.php

--- www/20190118.php
+++ www/20190118.php
@@ -106,6 +106,9 @@ function check_version_apache($mu_)
             $title = str_pad($m, 2, '0', STR_PAD_LEFT) . '/' . str_pad($match[1], 2, '0', STR_PAD_LEFT)
                 . ' ' . mb_convert_kana(strip_tags($match[2]), 'a') . ' ★';
             $timestamp = mktime(0, 0, 0, $m, $match[1], $y);
+            if ($timestamp < strtotime('+3 days')) {
+                continue;
+            }
             
             $hash = date('Ymd', $timestamp) . hash('sha512', $title);
             
