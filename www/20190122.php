95d02afa218a47f9589629152e7685147ef0e4da
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-01-22 19:21:16 +0900

    Update 20190122.php

--- www/20190122.php
+++ www/20190122.php
@@ -78,6 +78,26 @@ error_log($res);
 
 // exit();
 
+$url = 'https://' . parse_url(getenv('TEST_URL_010'))['host'] . '/api/v1/me/coin';
+
+$options = [
+    CURLOPT_ENCODING => 'gzip, deflate, br',
+    CURLOPT_HTTPHEADER => [
+        'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Language: ja,en-US;q=0.7,en;q=0.3',
+        'Cache-Control: no-cache',
+        'Connection: keep-alive',
+        'DNT: 1',
+        'Upgrade-Insecure-Requests: 1',
+        ],
+    CURLOPT_COOKIEJAR => $cookie,
+    CURLOPT_COOKIEFILE => $cookie,
+];
+
+$res = $mu->get_contents($url, $options);
+
+error_log($res);
+
 for ($j = $n; $j < 1500; $j++) {
     for ($i = 0; $i < 12; $i++) {
         $continue_flag = false;
