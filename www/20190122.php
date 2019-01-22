4eb000fd8012af9a91596c76b90feea5330924dd
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-01-22 09:58:58 +0900

    Update 20190122.php

--- www/20190122.php
+++ www/20190122.php
@@ -60,7 +60,6 @@ $options = [
     CURLOPT_COOKIEFILE => $cookie,
     CURLOPT_POST => true,
     CURLOPT_POSTFIELDS => http_build_query($post_data),
-    CURLOPT_NOBODY => true,
 ];
 
 $res = $mu->get_contents($url, $options);
