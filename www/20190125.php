5e554242cfe851e0a6e4f6bf90f85ea1e389a3da
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-01-25 08:46:57 +0900

    Update 20190125.php

--- www/20190125.php
+++ www/20190125.php
@@ -22,9 +22,7 @@ ini_set('max_execution_time', 3600);
 
 $mu = new MyUtils();
 
-$cookie = $tmpfname = tempnam("/tmp", time());
-
-$url = getenv('TEST_URL_030');
+$url = 'https://' . parse_url(getenv('TEST_URL_010'))['host'];
 
 $options1 = [
     CURLOPT_ENCODING => 'gzip, deflate, br',
@@ -36,91 +34,15 @@ $options1 = [
         'DNT: 1',
         'Upgrade-Insecure-Requests: 1',
         ],
-    CURLOPT_COOKIEJAR => $cookie,
-    CURLOPT_COOKIEFILE => $cookie,
-    CURLOPT_TIMEOUT => 20,
 ];
 
-$res = $mu->get_contents($url, $options1);
-
-$rc = preg_match_all('/<a href=".*?\/series\/(\d+)"/s', $res, $matches);
-
-$list_number = array_unique($matches[1]);
-sort($list_number, SORT_NUMERIC);
-
-$res = file_get_contents('https://raw.githubusercontent.com/tshr20140816/heroku-mode-07/master/data.txt');
-$list_number = array_merge($list_number, explode(',', $res));
-
-$list_number = array_unique($list_number);
-array_unshift($list_number, 2);
-array_unshift($list_number, 549);
-
-for ($i = 0; $i < count($list_number); $i++) {
-    if ($list_number[$i] == $n) {
-        $list_number = array_slice($list_number, $i);
-        break;
-    }
-}
-
-error_log(print_r($list_number, true));
-
-$url = getenv('TEST_URL_010');
-
-$res = $mu->get_contents($url, $options1);
-
-$rc = preg_match('/<input.+?name="utf8".+?value="(.*?)".+?<input.+?name="authenticity_token".+?value="(.*?)"/s', $res, $match);
-// error_log(print_r($match, true));
-$utf8 = $match[1];
-$authenticity_token = $match[2];
-
-$post_data = [
-    'utf8' => $utf8,
-    'authenticity_token' => $authenticity_token,
-    'account[email]' => getenv('TEST_ID'),
-    'account[password]' => getenv('TEST_PASSWORD'),    
-];
-
-$options2 = [
-    CURLOPT_ENCODING => 'gzip, deflate, br',
-    CURLOPT_HTTPHEADER => [
-        'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-        'Accept-Language: ja,en-US;q=0.7,en;q=0.3',
-        'Cache-Control: no-cache',
-        'Connection: keep-alive',
-        'DNT: 1',
-        'Upgrade-Insecure-Requests: 1',
-        ],
-    CURLOPT_COOKIEJAR => $cookie,
-    CURLOPT_COOKIEFILE => $cookie,
-    CURLOPT_POST => true,
-    CURLOPT_POSTFIELDS => http_build_query($post_data),
-];
-
-$res = $mu->get_contents($url, $options2);
-
-$options3 = [
-    CURLOPT_ENCODING => 'gzip, deflate, br',
-    CURLOPT_HTTPHEADER => [
-        'Accept: application/json, text/javascript, */*; q=0.01',
-        'Accept-Language: ja,en-US;q=0.7,en;q=0.3',
-        'Cache-Control: no-cache',
-        'Connection: keep-alive',
-        'DNT: 1',
-        'Upgrade-Insecure-Requests: 1',
-        ],
-    CURLOPT_COOKIEJAR => $cookie,
-    CURLOPT_COOKIEFILE => $cookie,
-];
-
-foreach ($list_number as $number) {
-    if ((int)date('i') < 8) {
-        error_log('STOP TIME');
-        break;
-    }
+for ($number = 0; $number < 1; $number++) {
     
     $url = str_replace('__NUMBER__', $number, getenv('TEST_URL_020')) . '1';
     $res = $mu->get_contents($url, $options1);
     
+    error_log($res);
+    break;
     $rc = preg_match_all('/page=(\d+)"/s', $res, $matches);
     
     $list_page = array_unique($matches[1]);
@@ -181,12 +103,6 @@ foreach ($list_number as $number) {
         }
     }
 }
-
-$url = 'https://' . parse_url(getenv('TEST_URL_010'))['host'] . '/api/v1/me/coin';
-$res = $mu->get_contents($url, $options3);
-error_log($res);
-
-error_log(file_get_contents($cookie));
 
 $time_finish = microtime(true);
 error_log("${pid} FINISH " . substr(($time_finish - $time_start), 0, 6) . 's');
