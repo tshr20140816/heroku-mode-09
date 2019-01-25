87567b81fcb6d380af31a9cf485b7a7e95780986
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-01-25 13:05:35 +0900

    Create 20190126.php

---
+++ www/20190126.php
@@ -0,0 +1,16 @@
+<?php
+
+include(dirname(__FILE__) . '/../classes/MyUtils.php');
+
+$pid = getmypid();
+$requesturi = $_SERVER['REQUEST_URI'];
+$time_start = microtime(true);
+error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
+
+$mu = new MyUtils();
+
+$url = 'http://www.carp.co.jp/_calendar/list.html';
+
+$res = $mu->get_contents($url);
+
+error_log(res);
