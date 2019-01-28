5cf8a275bf57f907a655d3d26aef389cc56d28c9
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-01-28 12:29:37 +0900

    Update 20190128.php

--- www/20190128.php
+++ www/20190128.php
@@ -14,5 +14,27 @@ $time_finish = microtime(true);
 error_log("${pid} FINISH " . ($time_finish - $time_start) . 's ');
 
 function check_uq($mu_) {
+    
+    $cookie = $tmpfname = tempnam("/tmp", time());
+    
     $url = 'https://my.uqmobile.jp/leo-bs-ptl-web/view/PSYSATH001_90/init?screenId=PSYSATH001_90&menuType=01';
+    
+    $options = [
+        CURLOPT_ENCODING => 'gzip, deflate, br',
+        CURLOPT_HTTPHEADER => [
+            'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Language: ja,en-US;q=0.7,en;q=0.3',
+            'Cache-Control: no-cache',
+            'Connection: keep-alive',
+            'DNT: 1',
+            'Upgrade-Insecure-Requests: 1',
+            ],
+        CURLOPT_COOKIEJAR => $cookie,
+        CURLOPT_COOKIEFILE => $cookie,
+        CURLOPT_TIMEOUT => 20,
+    ];
+    
+    $res = $mu_->get_contents($url, $options);
+    
+    error_log($res);
 }
