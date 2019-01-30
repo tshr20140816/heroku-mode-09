48a0c3f3790741f0c580d6216c9a16530aaf3fb1
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-01-30 17:23:08 +0900

    Update lib_info.php

--- www/lib_info.php
+++ www/lib_info.php
@@ -21,7 +21,8 @@ if (!isset($_GET['n'])
 check_lib($mu, (int)$_GET['n']);
 
 $time_finish = microtime(true);
-error_log("${pid} FINISH " . substr(($time_finish - $time_start), 0, 6) . 's');
+$mu->post_blog_wordpress($requesturi . ' ' . substr(($time_finish - $time_start), 0, 6) . 's');
+error_log("${pid} FINISH " . substr(($time_finish - $time_start), 0, 6) . 's ' . substr((microtime(true) - $time_start), 0, 6) . 's');
 
 function check_lib($mu_, $order_) {
     $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
