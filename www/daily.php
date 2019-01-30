cb78225c4518d35c119509fb25249382dbb48372
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-01-30 10:16:21 +0900

    Update daily.php

--- www/daily.php
+++ www/daily.php
@@ -241,6 +241,7 @@ check_version_apache($mu);
 $time_finish = microtime(true);
 $mu->post_blog_wordpress("${requesturi} add : ${count_add_task} / delete : ${count_delete_task} "
                          . substr(($time_finish - $time_start), 0, 6) . 's');
+error_log($pid . ' Web Access Count : ' . $mu->_count_web_access);
 error_log("${pid} FINISH " . substr(($time_finish - $time_start), 0, 6) . 's ' . substr((microtime(true) - $time_start), 0, 6) . 's');
 
 exit();
