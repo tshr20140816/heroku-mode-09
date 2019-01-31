872ad8b4f29919e3dcb9d141b732843a662ccac2
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-01-31 15:45:53 +0900

    Update rainfall.php

--- www/rainfall.php
+++ www/rainfall.php
@@ -78,7 +78,7 @@ if ($count !== 0) {
 }
 
 $time_finish = microtime(true);
-if ($count === 0) {
+if ((int)$_GET['c'] === 0) {
     $mu->post_blog_wordpress($requesturi . ' [' . substr(($time_finish - $time_start), 0, 6) . 's]');
 }
 error_log("${pid} FINISH " . substr(($time_finish - $time_start), 0, 6) . 's ' . substr((microtime(true) - $time_start), 0, 6) . 's');
