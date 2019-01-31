863dc3d8f9fa19c911f4676482d29a4e4b49cffb
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-01-31 15:55:11 +0900

    Create 20190201.php

---
+++ www/20190201.php
@@ -0,0 +1,22 @@
+<?php
+
+include(dirname(__FILE__) . '/../classes/MyUtils.php');
+
+$pid = getmypid();
+$requesturi = $_SERVER['REQUEST_URI'];
+$time_start = microtime(true);
+
+error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
+
+$mu = new MyUtils();
+
+
+
+$time_finish = microtime(true);
+
+error_log("${pid} FINISH " . substr(($time_finish - $time_start), 0, 6) . 's ' . substr((microtime(true) - $time_start), 0, 6) . 's');
+exit();
+
+function check_bus($mu) {
+    $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
+}
