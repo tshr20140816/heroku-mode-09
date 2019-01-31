4f8030dd6127414798d996bdcaf13d1b1f8120bd
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-01-31 16:21:24 +0900

    Update 20190201.php

--- www/20190201.php
+++ www/20190201.php
@@ -20,23 +20,26 @@ exit();
 function check_bus($mu_) {
     $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
     
-    $url = getenv('TEST_URL_100');
-    $res = $mu_->get_contents($url);
-    
-    //error_log($res);
-    
-    $rc = preg_match_all('/<li.*?>(.+?)<\/li>/s', $res, $matches,  PREG_SET_ORDER);
-    
-    // error_log(print_r($matches, true));
-    
-    $pattern = '/<meta itemprop="name" content="(.+?)".+?itemprop="departureTime">(.+?)<.+?itemprop="name">(.+?)</s';
-    foreach ($matches as $item) {
-        $rc = preg_match($pattern, $item[1], $match);
-        if ($rc != 1) {
-            continue;
+    $urls[] = getenv('TEST_URL_100');
+    $urls[] = getenv('TEST_URL_101');
+    foreach ($urls as $url) {
+        $res = $mu_->get_contents($url);
+
+        //error_log($res);
+
+        $rc = preg_match_all('/<li.*?>(.+?)<\/li>/s', $res, $matches,  PREG_SET_ORDER);
+
+        // error_log(print_r($matches, true));
+
+        $pattern = '/<meta itemprop="name" content="(.+?)".+?itemprop="departureTime">(.+?)<.+?itemprop="name">(.+?)</s';
+        foreach ($matches as $item) {
+            $rc = preg_match($pattern, $item[1], $match);
+            if ($rc != 1) {
+                continue;
+            }
+            $match[2] = trim($match[2]);
+            array_shift($match);
+            error_log(print_r($match, true));
         }
-        $match[2] = trim($match[2]);
-        array_shift($match);
-        error_log(print_r($match, true));
     }
 }
