8e6f238c4053572afae390125ac260acff1ee9b9
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-02-04 13:22:38 +0900

    Create 20190203.php

---
+++ www/20190203.php
@@ -0,0 +1,35 @@
+<?php
+
+include(dirname(__FILE__) . '/../classes/MyUtils.php');
+
+$pid = getmypid();
+$requesturi = $_SERVER['REQUEST_URI'];
+$time_start = microtime(true);
+error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
+
+$mu = new MyUtils();
+
+$cookie = $tmpfname = tempnam("/tmp", time());
+
+$url = 'https://www.toodledo.com/signin.php?redirect=/tools/backup.php';
+
+$options = [
+    CURLOPT_ENCODING => 'gzip, deflate, br',
+    CURLOPT_HTTPHEADER => [
+        'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Language: ja,en-US;q=0.7,en;q=0.3',
+        'Cache-Control: no-cache',
+        'Connection: keep-alive',
+        'DNT: 1',
+        'Upgrade-Insecure-Requests: 1',
+        ],
+    CURLOPT_COOKIEJAR => $cookie,
+    CURLOPT_COOKIEFILE => $cookie,
+    CURLOPT_TIMEOUT => 20,
+];
+
+$res = $mu->get_contents($url, $options);
+
+$rc = preg_match_all('/<input .+? name="(.*?)" value="(.*?)"/s', $res, $matches);
+
+error_log(print_r($matches, true));
