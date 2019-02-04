7e401ad3260a7bc012c8e1579fc2e69492476ad7
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-02-04 12:14:49 +0900

    Update 20190204.php

--- www/20190204.php
+++ www/20190204.php
@@ -1,14 +1,13 @@
 <?php
 
-// include(dirname(__FILE__) . '/../classes/MyUtils.php');
+include(dirname(__FILE__) . '/../classes/MyUtils.php');
 
 $pid = getmypid();
 $requesturi = $_SERVER['REQUEST_URI'];
 $time_start = microtime(true);
 error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
 
-// $mu = new MyUtils();
+$mu = new MyUtils();
 
-$client = new HTTP_WebDAV_Client_Stream();
 // https://webcache.googleusercontent.com/search?q=cache:4GQ-z2i8mrgJ:https://sebastiaandejonge.com/blog/2013/january/22/php-upload-to-webdav-using-curl.html+&cd=1&hl=ja&ct=clnk&gl=jp
 
