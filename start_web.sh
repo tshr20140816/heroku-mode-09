5bf2145a9bb55920d178c36b5e876eeb07bc1846
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-02-04 11:33:18 +0900

    Update start_web.sh

--- start_web.sh
+++ start_web.sh
@@ -26,13 +26,9 @@ curl --version
 echo "$(httpd -v)" > /tmp/apache_current_version
 
 # heroku-buildpack-php
-# current_version=$(cat composer.lock | grep version | awk '{print $2}' | tr -d ,)
 composer update > /dev/null 2>&1 &
 
-# echo $current_version > /tmp/current_version
-
 export USER_AGENT=$(curl https://raw.githubusercontent.com/tshr20140816/heroku-mode-07/master/useragent.txt)
-# echo $USER_AGENT
 
 htpasswd -c -b .htpasswd ${BASIC_USER} ${BASIC_PASSWORD}
 
@@ -54,10 +50,5 @@ popd
 set -x
 
 wait
-
-# heroku-buildpack-php
-# latest_version=$(cat composer.lock | grep version | awk '{print $2}' | tr -d ,)
-# echo "heroku-buildpack-php : current ${current_version} latest ${latest_version}"
-# echo $latest_version > /tmp/latest_version
 
 vendor/bin/heroku-php-apache2 -C apache.conf www
