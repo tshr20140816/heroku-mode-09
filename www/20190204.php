fecaacc0e4ae95e528c377c0f589ec00c2b2b668
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-02-04 09:45:05 +0900

    Create 20190204.php

---
+++ www/20190204.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once 'HTTP/WebDAV/Client.php';
+
+include(dirname(__FILE__) . '/../classes/MyUtils.php');
+
+$pid = getmypid();
+$requesturi = $_SERVER['REQUEST_URI'];
+$time_start = microtime(true);
+error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
+
+$mu = new MyUtils();
+
+$client = new HTTP_WebDAV_Client_Stream();
