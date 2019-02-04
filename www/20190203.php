74ffb2ae16574ec0b1aaf444f848787b74a980bf
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-02-04 13:36:39 +0900

    Update 20190203.php

--- www/20190203.php
+++ www/20190203.php
@@ -13,7 +13,7 @@ $cookie = $tmpfname = tempnam("/tmp", time());
 
 $url = 'https://www.toodledo.com/signin.php?redirect=/tools/backup.php';
 
-$options = [
+$options1 = [
     CURLOPT_ENCODING => 'gzip, deflate, br',
     CURLOPT_HTTPHEADER => [
         'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
@@ -28,8 +28,40 @@ $options = [
     CURLOPT_TIMEOUT => 20,
 ];
 
-$res = $mu->get_contents($url, $options);
+$res = $mu->get_contents($url, $options1);
 
-$rc = preg_match_all('/<input .+? name="(.*?)" value="(.*?)"/s', $res, $matches);
+$rc = preg_match('/<input .+? name="csrf1" value="(.*?)"/s', $res, $matches);
+$csrf1 = $matches[1];
+$rc = preg_match('/<input .+? name="csrf2" value="(.*?)"/s', $res, $matches);
+$csrf2 = $matches[1];
 
-error_log(print_r($matches, true));
+$post_data = [
+    'csrf1' => $csrf1,
+    'csrf2' => $csrf2,
+    'redirect' => /tools/backup.php,
+    'email' => getenv('TOODLEDO_EMAIL'),
+    'password' => getenv('TOODLEDO_PASSWORD'),
+];
+
+$options2 = [
+    CURLOPT_ENCODING => 'gzip, deflate, br',
+    CURLOPT_HTTPHEADER => [
+        'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Language: ja,en-US;q=0.7,en;q=0.3',
+        'Cache-Control: no-cache',
+        'Connection: keep-alive',
+        'DNT: 1',
+        'Upgrade-Insecure-Requests: 1',
+        ],
+    CURLOPT_COOKIEJAR => $cookie,
+    CURLOPT_COOKIEFILE => $cookie,
+    CURLOPT_TIMEOUT => 20,
+    CURLOPT_POST => true,
+    CURLOPT_POSTFIELDS => http_build_query($post_data),
+];
+
+$url = 'https://www.toodledo.com/signin.php';
+
+$res = $mu->get_contents($url, $options2);
+
+error_log(strlen($res));
