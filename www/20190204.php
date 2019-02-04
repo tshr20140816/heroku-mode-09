8fe6cadcdfbbd6c87ddbf26bf1fe318e6c2ae6cc
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-02-04 10:04:06 +0900

    Update 20190204.php

--- www/20190204.php
+++ www/20190204.php
@@ -11,6 +11,14 @@ error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
 
 $mu = new MyUtils();
 
-$client = new HTTP_WebDAV_Client_Stream();
-
+// $client = new HTTP_WebDAV_Client_Stream();
 // https://stackoverflow.com/questions/3369675/php-idisk-webdav-client
+
+$url = 'https://raw.githubusercontent.com/tshr20140816/heroku-mode-07/master/composer.lock';
+$res = $mu->get_contents($url);
+
+error_log($res);
+
+$res = json_decode($res, true);
+
+error_log($res);
