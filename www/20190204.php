3617f0e53f3042746f0b210484f7eccd8cddb556
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-02-04 12:22:15 +0900

    Update 20190204.php

--- www/20190204.php
+++ www/20190204.php
@@ -11,3 +11,10 @@ $mu = new MyUtils();
 
 // https://webcache.googleusercontent.com/search?q=cache:4GQ-z2i8mrgJ:https://sebastiaandejonge.com/blog/2013/january/22/php-upload-to-webdav-using-curl.html+&cd=1&hl=ja&ct=clnk&gl=jp
 
+$cmd = 'pg_dump --dbname=' . getenv('DATABASE_URL') . ' >/tmp/pg_dump.dat';
+exec($cmd);
+
+if (file_exists('/tmp/pg_dump.dat')) {
+    error_log(filesize('/tmp/pg_dump.dat'));
+}
+  
