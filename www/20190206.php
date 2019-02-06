cfd052bd9929bf949df7307fac3fd85e4c6fd915
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-02-06 12:44:56 +0900

    Update 20190206.php

--- www/20190206.php
+++ www/20190206.php
@@ -9,16 +9,20 @@ error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
 
 $mu = new MyUtils();
 
-error_log(openssl_cipher_iv_length('AES-256-CBC'));
-
-exit;
-
 $file_name = '/tmp/pg_dump.dat';
 $cmd = 'pg_dump --format=plain --dbname=' . getenv('DATABASE_URL') . ' >' . $file_name;
 exec($cmd);
 
 $res = bzcompress(file_get_contents($file_name), 9);
-$res = openssl_encrypt($res, 'AES-256-CBC', getenv('BACKUP_PASSWORD'), OPENSSL_RAW_DATA, '0123456789012345');
+
+$method = 'AES-256-CBC';
+$password = getenv('BACKUP_PASSWORD');
+$IV = substr(sha1($password), 0, openssl_cipher_iv_length($method));
+$res = openssl_encrypt($res, $method, $password, OPENSSL_RAW_DATA, $IV);
+
+$res = base64_encode($res);
+
+file_put_contents($file_name);
 
 
 $filename = 'useragent.txt';
@@ -28,7 +32,7 @@ $filesize = filesize($filepath);
 error_log($filesize);
 $fh = fopen($filepath, 'r');
 
-$ch = curl_init('https://webdav.hidrive.strato.com/users/' . getenv('HIDRIVE_USER'). '/test3.txt');
+$ch = curl_init('https://webdav.hidrive.strato.com/users/' . getenv('HIDRIVE_USER'). '/test4.txt');
 
 curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
 curl_setopt($ch, CURLOPT_USERPWD, getenv('HIDRIVE_USER') . ':' . getenv('HIDRIVE_PASSWORD'));
@@ -39,7 +43,12 @@ curl_setopt($ch, CURLOPT_INFILESIZE, $filesize);
 
 $res = curl_exec($ch);
 
+$options = [
+    CURLOPT_HTTPAUTH => CURLAUTH_ANY
+];
+
 fclose($fh);
 
 error_log(print_r($res, true));
 
+@unlink($file_name);
