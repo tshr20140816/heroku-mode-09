8a97963de00deed954961e3402a3b532d9813850
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-02-07 11:35:18 +0900

    Update 20190203.php

--- www/20190203.php
+++ www/20190203.php
@@ -16,7 +16,7 @@ $mbox = imap_open('{imap.mail.yahoo.co.jp:993/ssl}', getenv('TEST2_ID'), getenv(
 // error_log($mbox);
 error_log(print_r($mbox, true));
 
-$res = imap_search($mbox, ALL);
+$res = imap_search($mbox, 'ALL');
 
 error_log(print_r($res, true));
 
