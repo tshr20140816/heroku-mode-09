c9fffe4677eab264ca2e6b1006200de26626896d
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-02-07 16:37:19 +0900

    Update 20190203.php

--- www/20190203.php
+++ www/20190203.php
@@ -9,9 +9,7 @@ error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
 
 $mu = new MyUtils();
 
-// imap.mail.yahoo.co.jp 993 w36_5
-
-$imap = imap_open('{imap.mail.yahoo.co.jp:993/ssl}w36_5', getenv('TEST2_ID'), getenv('TEST2_PASSWORD'));
+$imap = imap_open('{imap.mail.yahoo.co.jp:993/ssl}', getenv('TEST2_ID'), getenv('TEST2_PASSWORD'));
 
 // error_log($mbox);
 error_log(print_r($imap, true));
@@ -35,6 +33,7 @@ error_log('COUNT : ' . count($res));
 // $header = imap_header($imap, $res[0]);
 // error_log(print_r($header, true));
 
+/*
 for ($i = 0; $i < 300; $i++) {
     $rc = imap_delete($imap, $res[$i]);
     // error_log($i . ' ' . $rc);
@@ -46,5 +45,6 @@ for ($i = 0; $i < 300; $i++) {
 
 $rc = imap_expunge($imap);
 error_log($rc);
+*/
 
 imap_close($imap);
