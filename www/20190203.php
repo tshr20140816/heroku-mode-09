c2ce5c378612167fe1eb46c34808ec1d7d79d18f
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-02-07 11:28:03 +0900

    Update 20190203.php

--- www/20190203.php
+++ www/20190203.php
@@ -13,7 +13,7 @@ $mu = new MyUtils();
 
 $mbox = imap_open('{imap.mail.yahoo.co.jp:993/ssl}', getenv('TEST2_ID'), getenv('TEST2_PASSWORD'));
 
-error_log($mbox);
+// error_log($mbox);
 error_log(print_r($mbox, true));
 
 imap_close($mbox);
