300296ebca4c07f41d444646397f072eb0eb423b
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-02-12 09:37:34 +0900

    Create 20190212.php

---
+++ www/20190212.php
@@ -0,0 +1,69 @@
+<?php
+
+include(dirname(__FILE__) . '/../classes/MyUtils.php');
+
+$pid = getmypid();
+$requesturi = $_SERVER['REQUEST_URI'];
+$time_start = microtime(true);
+error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
+
+$mu = new MyUtils();
+
+backup_opml($mu);
+
+function backup_opml($mu) {
+    $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
+
+	$cookie = $tmpfname = tempnam("/tmp", time());
+
+    $url = 'https://www.inoreader.com/';
+
+    $post_data = [
+        'warp_action' => 'login',
+        'hash_action' => '',
+        'sendback' => '',
+        'username' => base64_decode(getenv('INOREADER_USER')),
+        'password' => base64_decode(getenv('INOREADER_PASSWORD')),
+        'remember_me' => 'on',
+    ];
+
+	$options = [
+        CURLOPT_ENCODING => 'gzip, deflate, br',
+        CURLOPT_HTTPHEADER => [
+            'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Language: ja,en-US;q=0.7,en;q=0.3',
+            'Cache-Control: no-cache',
+            'Connection: keep-alive',
+            'DNT: 1',
+            'Upgrade-Insecure-Requests: 1',
+            ],
+        CURLOPT_COOKIEJAR => $cookie,
+        CURLOPT_COOKIEFILE => $cookie,
+        CURLOPT_TIMEOUT => 20,
+        CURLOPT_POST => true,
+        CURLOPT_POSTFIELDS => http_build_query($post_data),
+    ];
+
+    $res = $mu_->get_contents($url, $options);
+
+	$url = 'https://www.inoreader.com/reader/subscriptions/export?download=1';
+
+	$options = [
+        CURLOPT_ENCODING => 'gzip, deflate, br',
+        CURLOPT_HTTPHEADER => [
+            'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Language: ja,en-US;q=0.7,en;q=0.3',
+            'Cache-Control: no-cache',
+            'Connection: keep-alive',
+            'DNT: 1',
+            'Upgrade-Insecure-Requests: 1',
+            ],
+        CURLOPT_COOKIEJAR => $cookie,
+        CURLOPT_COOKIEFILE => $cookie,
+        CURLOPT_TIMEOUT => 20,
+    ];
+
+    $res = $mu_->get_contents($url, $options);
+
+	error_log($res);
+}
