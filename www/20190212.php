8ffe3c5d67bb80c8335d9ac6c2db884bb08c29dd
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-02-12 13:16:03 +0900

    Create 20190212.php

---
+++ www/20190212.php
@@ -0,0 +1,16 @@
+<?php
+
+include(dirname(__FILE__) . '/../classes/MyUtils.php');
+
+$pid = getmypid();
+$requesturi = $_SERVER['REQUEST_URI'];
+$time_start = microtime(true);
+error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
+
+$mu = new MyUtils();
+
+check_php_version($mu);
+
+function check_php_version($mu_) {
+
+}
