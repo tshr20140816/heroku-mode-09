b8618b0eba01a458d77c289fefe2085e4535f3bf
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-02-12 13:23:04 +0900

    Update 20190212.php

--- www/20190212.php
+++ www/20190212.php
@@ -14,4 +14,9 @@ check_php_version($mu);
 function check_php_version($mu_) {
     $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
 
+    $url = 'https://devcenter.heroku.com/articles/php-support?4nocache' . date('Ymd', strtotime('+9 hours'));
+    $res = $mu_->get_contents($url, null, true);
+    
+    $rc = preg_match('/<h4 id="supported-versions-php">PHP<\/h4>.*?<ul>(.+?)<\/ul>/s', $res, $match);
+    error_log(print_r($match, true));
 }
