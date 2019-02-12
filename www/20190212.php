3f071f06daa27345ea3235e525b61bb56a0264a5
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-02-12 13:51:25 +0900

    Update 20190212.php

--- www/20190212.php
+++ www/20190212.php
@@ -25,13 +25,37 @@ function check_php_version($mu_) {
     
     $list_version = [];
     foreach ($matches[1] as $item) {
-        error_log($item);
+        // error_log($item);
         $tmp = explode('.', $item);
         $list_version[$tmp[0] * 10000 + $tmp[1] * 100 + $tmp[2]] = $item;
     }
     krsort($list_version);
-    error_log(print_r($list_version, true));
+    // error_log(print_r($list_version, true));
     
-    $version = array_shift($list_version);
-    error_log($version);
+    $version_support = array_shift($list_version);
+    error_log($version_support);
+    
+    $url = 'https://github.com/php/php-src/releases.atom?4nocache' . date('Ymd', strtotime('+9 hours'));
+    $res = $mu_->get_contents($url, null, true);
+    
+    $doc = new DOMDocument();
+    $doc->loadXML($res);
+    
+    $xpath = new DOMXpath($doc);
+    $xpath->registerNamespace('ns', 'http://www.w3.org/2005/Atom');
+    
+    $elements = $xpath->query("//ns:entry/ns:title");
+    
+    $list_version = [];
+    foreach ($elements as $element) {
+        $tmp = $element->nodeValue;
+        if (strpos($tmp, 'RC') > 0) {
+            continue;
+        }
+        $tmp = explode('.', $tmp);
+        $list_version[(int)$tmp[0] * 10000 + (int)$tmp[1] * 100 + (int)$tmp[2]] = $element->nodeValue;
+    }
+    krsort($list_version);
+    error_log(print_r($list_version, true));
+    $version_latest = array_shift($list_version);
 }
