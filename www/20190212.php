b2ace7fdb0f526eedd4f88782c4d9c08db358aec
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-02-12 14:04:15 +0900

    Update 20190212.php

--- www/20190212.php
+++ www/20190212.php
@@ -13,27 +13,6 @@ check_php_version($mu);
 
 function check_php_version($mu_) {
     $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
-
-    $url = 'https://devcenter.heroku.com/articles/php-support?4nocache' . date('Ymd', strtotime('+9 hours'));
-    $res = $mu_->get_contents($url, null, true);
-    
-    $rc = preg_match('/<h4 id="supported-versions-php">PHP<\/h4>.*?<ul>(.+?)<\/ul>/s', $res, $match);
-    // error_log(print_r($match, true));
-    
-    $rc = preg_match_all('/<li>(.+?)<\/li>/s', $match[1], $matches);
-    // error_log(print_r($matches, true));
-    
-    $list_version = [];
-    foreach ($matches[1] as $item) {
-        // error_log($item);
-        $tmp = explode('.', $item);
-        $list_version[$tmp[0] * 10000 + $tmp[1] * 100 + $tmp[2]] = $item;
-    }
-    krsort($list_version);
-    // error_log(print_r($list_version, true));
-    
-    $version_support = array_shift($list_version);
-    error_log($version_support);
     
     $url = 'https://github.com/php/php-src/releases.atom?4nocache' . date('Ymd', strtotime('+9 hours'));
     $res = $mu_->get_contents($url, null, true);
@@ -59,5 +38,33 @@ function check_php_version($mu_) {
     krsort($list_version);
     error_log(print_r($list_version, true));
     $version_latest = array_shift($list_version);
-    error_log($version_latest);
+    // error_log($version_latest);
+    
+    $res = file_get_contents('/tmp/php_current_version');
+    $version_current = trim(str_replace(["\r\n", "\r", "\n", '   ', '  '], ' ', $res));
+
+    $url = 'https://devcenter.heroku.com/articles/php-support?4nocache' . date('Ymd', strtotime('+9 hours'));
+    $res = $mu_->get_contents($url, null, true);
+    
+    $rc = preg_match('/<h4 id="supported-versions-php">PHP<\/h4>.*?<ul>(.+?)<\/ul>/s', $res, $match);
+    // error_log(print_r($match, true));
+    
+    $rc = preg_match_all('/<li>(.+?)<\/li>/s', $match[1], $matches);
+    // error_log(print_r($matches, true));
+    
+    $list_version = [];
+    foreach ($matches[1] as $item) {
+        // error_log($item);
+        $tmp = explode('.', $item);
+        $list_version[$tmp[0] * 10000 + $tmp[1] * 100 + $tmp[2]] = $item;
+    }
+    krsort($list_version);
+    // error_log(print_r($list_version, true));
+    
+    $version_support = array_shift($list_version);
+    // error_log($version_support);
+    
+    error_log($log_prefix . '$version_latest : ' . $version_latest);
+    error_log($log_prefix . '$version_support : ' . $version_support);
+    error_log($log_prefix . '$version_current : ' . $version_current);
 }
