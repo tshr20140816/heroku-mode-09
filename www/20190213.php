f8af325a0a0ca5555682d341d23ed4353ecc3823
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-02-13 21:12:44 +0900

    Update 20190213.php

--- www/20190213.php
+++ www/20190213.php
@@ -7,10 +7,21 @@ $time_start = microtime(true);
 error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
 $mu = new MyUtils();
 
+$username = base64_decode(getenv('WORDPRESS_USERNAME'));
+
 $url = 'https://inoreader.superfeedr.com/';
 
 $post_data = [
     'hub.mode' => 'subscribe',
-    'hub.callback' => '',
-    'hub.topic' => '',
+    'hub.callback' => 'https://' . $username . '.wordpress.com/',
+    'hub.topic' => 'https://' . $username . '.wordpress.com/feed/',
 ];
+
+$res = $mu->get_contents(
+    $url,
+    [CURLOPT_POST => true,
+    CURLOPT_POSTFIELDS => http_build_query($post_data),
+    ]
+);
+
+error_log($res);
