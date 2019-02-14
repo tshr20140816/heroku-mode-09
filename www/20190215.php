038a533b5439a613ee3aa4d581133cd7eeaf63a4
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-02-15 08:50:46 +0900

    Create 20190215.php

---
+++ www/20190215.php
@@ -0,0 +1,30 @@
+<?php
+
+include(dirname(__FILE__) . '/../classes/MyUtils.php');
+
+$pid = getmypid();
+$requesturi = $_SERVER['REQUEST_URI'];
+$time_start = microtime(true);
+error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
+
+$mu = new MyUtils();
+
+error_log($pid . ' '. print_r(getallheaders(), true));
+
+$html = <<< __HEREDOC__
+<html><body>
+<form method="POST" action="./20190215.php">
+<input type="text" name="file_name" />
+<input type="submit" /> 
+</form>
+</body></html>
+__HEREDOC__;
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+} else {
+    echo $html;
+}
+
+$time_finish = microtime(true);
+error_log("${pid} FINISH " . substr(($time_finish - $time_start), 0, 6) . 's');
