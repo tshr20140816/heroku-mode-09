bab89fb8663e8a815b8ac04810d21036697867a5
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-02-14 09:15:20 +0900

    Update 20190214.php

--- www/20190214.php
+++ www/20190214.php
@@ -1,7 +1,11 @@
 <?php
 
+$url = $_GET['u'];
+
+$ch = curl_init();
+
 $options = [
-    CURLOPT_URL => $url_,
+    CURLOPT_URL => $url,
     CURLOPT_USERAGENT => getenv('USER_AGENT'),
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_ENCODING => '',
@@ -10,4 +14,18 @@ $options = [
     CURLOPT_PATH_AS_IS => true,
     CURLOPT_TCP_FASTOPEN => true,
     CURLOPT_SSL_VERIFYPEER => false,
+    CURLOPT_HEADER => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_2TLS,
 ];
+
+foreach ($options as $key => $value) {
+    $rc = curl_setopt($ch, $key, $value);
+    if ($rc == false) {
+        error_log("curl_setopt : ${key} ${value}");
+    }
+}
+
+$res = curl_exec($ch);
+$info = curl_getinfo($ch);
+
+error_log(substr($res, 0, $info['header_size']));
