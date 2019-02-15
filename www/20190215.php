3b99c685bf3cf1ac7a7f87a5ed2ff153507b9853
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-02-15 09:04:01 +0900

    Update 20190215.php

--- www/20190215.php
+++ www/20190215.php
@@ -21,15 +21,25 @@ $html = <<< __HEREDOC__
 __HEREDOC__;
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $file_name = $_POST['file_name'];
     $user = base64_decode(getenv('HIDRIVE_USER'));
     $password = base64_decode(getenv('HIDRIVE_PASSWORD'));
-    $url = "https://webdav.hidrive.strato.com/users/${user}/" . $_POST['file_name'];
+    $url = "https://webdav.hidrive.strato.com/users/${user}/${file_name}";
     $options = [
         CURLOPT_HTTPAUTH => CURLAUTH_ANY,
         CURLOPT_USERPWD => "${user}:${password}",
         CURLOPT_CUSTOMREQUEST => 'GET',
     ];
     $res = $mu->get_contents($url, $options);
+    
+    $res = base64_decode($res);
+    
+    $method = 'AES-256-CBC';
+    $password = base64_encode(getenv('HIDRIVE_USER')) . base64_encode(getenv('HIDRIVE_PASSWORD'));
+    $IV = substr(sha1($file_name), 0, openssl_cipher_iv_length($method));
+    $res = openssl_decrypt($res, $method, $password, OPENSSL_RAW_DATA, $IV);
+    
+    $res = bzdecompress($res);
 } else {
     echo $html;
 }
