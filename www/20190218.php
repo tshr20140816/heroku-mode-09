165516baf63993383da9c9bb5988636524ae98a4
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-02-18 18:01:28 +0900

    Create 20190218.php

---
+++ www/20190218.php
@@ -0,0 +1,34 @@
+<?php
+
+include(dirname(__FILE__) . '/../classes/MyUtils.php');
+$pid = getmypid();
+$requesturi = $_SERVER['REQUEST_URI'];
+$time_start = microtime(true);
+error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
+
+$mu = new MyUtils();
+
+$cookie = tempnam('/tmp', time());
+
+$url = 'http://www.ticket.carp.co.jp/shop/o_kuuseki/kuuseki.csv';
+
+curl_setopt($ch, CURLOPT_HEADER, 1);
+$options = [
+    CURLOPT_ENCODING => 'gzip, deflate, br',
+    CURLOPT_HTTPHEADER => [
+        'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Language: ja,en-US;q=0.7,en;q=0.3',
+        'Cache-Control: no-cache',
+        'Connection: keep-alive',
+        'DNT: 1',
+        'Upgrade-Insecure-Requests: 1',
+        ],
+    CURLOPT_HEADER => true,
+    CURLOPT_COOKIEJAR => $cookie,
+    CURLOPT_COOKIEFILE => $cookie,
+];
+
+$res = $mu->get_contents($url, $options);
+
+error_log($res);
+
