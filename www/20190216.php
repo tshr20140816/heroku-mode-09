d24cac1ff20e5918b42cab301f111d3f02834041
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-02-18 10:21:00 +0900

    Update 20190216.php

--- www/20190216.php
+++ www/20190216.php
@@ -8,10 +8,47 @@ error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
 
 $mu = new MyUtils();
 
-get_record_count($mu, '/tmp/dummy');
+// get_record_count($mu, '/tmp/dummy');
+backup_db($mu, '/tmp/dummy');
+
+error_log(file_get_contents('/tmp/dummy'));
 
 error_log("${pid} FINISH " . substr((microtime(true) - $time_start), 0, 6) . 's');
 exit();
+
+function backup_db($mu_, $file_name_blog_)
+{
+    $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
+
+    $file_name = '/tmp/' . getenv('HEROKU_APP_NAME')  . '_' .  date('d', strtotime('+9 hours')) . '_pg_dump.txt';
+    error_log($log_prefix . $file_name);
+    $cmd = 'pg_dump --format=plain --dbname=' . getenv('DATABASE_URL') . ' >' . $file_name;
+    exec($cmd);
+
+    $file_size = $mu_->backup_data(file_get_contents($file_name), $file_name);
+    $file_size = number_format($file_size);
+
+    $sql = <<< __HEREDOC__
+SELECT SUM(T1.reltuples) cnt
+  FROM pg_class T1
+ WHERE EXISTS ( SELECT 'X'
+                  FROM pg_stat_user_tables T2
+                 WHERE T2.relname = T1.relname
+                   AND T2.schemaname='public'
+              )
+__HEREDOC__;
+
+    $pdo = $mu_->get_pdo();
+    $record_count = 0;
+    foreach ($pdo->query($sql) as $row) {
+        error_log($log_prefix . print_r($row, true));
+        $record_count = $row['cnt'];
+        $record_count = number_format($record_count);
+    }
+    $pdo = null;
+
+    file_put_contents($file_name_blog_, "Database backup size : ${file_size}Byte\nRecord Count : ${record_count}", FILE_APPEND);
+}
 
 function get_record_count($mu_, $file_name_blog_)
 {
