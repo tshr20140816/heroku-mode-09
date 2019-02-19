bb0e6c2ed1a8feafee3ea73634b280ac06779238
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-02-19 09:21:22 +0900

    Update 20190219.php

--- www/20190219.php
+++ www/20190219.php
@@ -8,7 +8,7 @@ error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
 
 $mu = new MyUtils();
 
-$url = 'https://search.travel.rakuten.co.jp/ds/hotellist/Japan-Mie-Tsu-low?f_nen1=2019&f_tuki1=09&f_hi1=28&f_nen2=2019&f_tuki2=09&f_hi2=29&f_otona_su=2&f_s1=0&f_s2=0&f_y1=0&f_y2=0&f_y3=0&f_y4=0&f_heya_su=1&f_kin2=0&f_ido=0&f_kdo=0&f_km=7.0&f_hyoji=30&f_image=1&f_tab=hotel&f_datumType=WGS&f_point_min=0';
+$cookie = tempnam("/tmp", time());
 
 $options = [
     CURLOPT_ENCODING => 'gzip, deflate, br',
@@ -20,8 +20,18 @@ $options = [
         'DNT: 1',
         'Upgrade-Insecure-Requests: 1',
         ],
+    CURLOPT_COOKIEJAR => $cookie,
+    CURLOPT_COOKIEFILE => $cookie,
 ];
+
+$url = 'https://travel.rakuten.co.jp/';
+
+$res = $mu->get_contents($url, $options);
+
+$url = 'https://search.travel.rakuten.co.jp/ds/hotellist/Japan-Mie-Tsu-low?f_nen1=2019&f_tuki1=09&f_hi1=28&f_nen2=2019&f_tuki2=09&f_hi2=29&f_otona_su=2&f_s1=0&f_s2=0&f_y1=0&f_y2=0&f_y3=0&f_y4=0&f_heya_su=1&f_kin2=0&f_ido=0&f_kdo=0&f_km=7.0&f_hyoji=30&f_image=1&f_tab=hotel&f_datumType=WGS&f_point_min=0';
 
 $res = $mu->get_contents($url, $options);
 
 error_log($res);
+
+unlink($cookie);
