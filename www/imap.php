8973910a5c17539b46e21f78ac9a49b8b4e9feea
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-02-19 13:50:24 +0900

    Update and rename 20190210.php to imap.php

---
+++ www/imap.php
@@ -0,0 +1,42 @@
+<?php
+
+include(dirname(__FILE__) . '/../classes/MyUtils.php');
+
+$pid = getmypid();
+$requesturi = $_SERVER['REQUEST_URI'];
+$time_start = microtime(true);
+error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
+
+$mu = new MyUtils();
+
+$html = <<< __HEREDOC__
+<html><body>
+<form method="POST" action="./imap.php">
+<input type="text" name="user" />
+<input type="password" name="password" />
+<input type="text" name="message_number" />
+<input type="submit" /> 
+</form>
+</body></html>
+__HEREDOC__;
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $user = $_POST['user'];
+    $password = $_POST['password'];
+    $message_number = $_POST['message_number'];
+    
+    $imap = imap_open('{imap.mail.yahoo.co.jp:993/ssl}', $user, $password);
+    
+    $header = imap_header($imap, $message_number);
+    $body = imap_body($imap, $message_number);
+    
+    error_log(print_r($header, true));
+    error_log(print_r($body, true));
+    
+    imap_close($imap);
+} else {
+    echo $html;
+}
+
+$time_finish = microtime(true);
+error_log("${pid} FINISH " . substr(($time_finish - $time_start), 0, 6) . 's');
