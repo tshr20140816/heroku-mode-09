8b29bfb95252ac9c243ba26562410e2f6d3f4604
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-02-22 00:57:35 +0900

    Update 20190220.php

--- www/20190220.php
+++ www/20190220.php
@@ -37,25 +37,6 @@ function func_test($mu_)
     error_log(print_r($matches[1], true));
     
     $size = 0;
-    
-    /*
-    foreach($matches[1] as $file_name) {
-        $url = "https://webdav.hidrive.strato.com/users/${user}/" . $file_name;
-
-        $options = [
-            CURLOPT_HTTPAUTH => CURLAUTH_ANY,
-            CURLOPT_USERPWD => "${user}:${password}",
-            CURLOPT_HEADER => true,
-            CURLOPT_NOBODY => true,
-            CURLOPT_HTTPHEADER => ['Connection: keep-alive',],
-        ];
-        $res = $mu_->get_contents($url, $options);
-
-        $rc = preg_match('/Content-Length: (\d+)/', $res, $match);
-        error_log(print_r($match, true));
-        $size += (int)$match[1];
-    }
-    */
     $options = [
         CURLOPT_HTTPAUTH => CURLAUTH_ANY,
         CURLOPT_USERPWD => "${user}:${password}",
