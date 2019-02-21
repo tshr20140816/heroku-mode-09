ba3409007615ceb269ab87f0da263c3b430cd1a1
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-02-21 19:14:29 +0900

    Update 20190219.php

--- www/20190219.php
+++ www/20190219.php
@@ -8,76 +8,75 @@ error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
 
 $mu = new MyUtils();
 
-$cookie = tempnam("/tmp", time());
+backup_opml2($mu, '/tmp/dummy');
 
-$options = [
-    CURLOPT_ENCODING => 'gzip, deflate, br',
-    CURLOPT_HTTPHEADER => [
-        'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-        'Accept-Language: ja,en-US;q=0.7,en;q=0.3',
-        'Cache-Control: no-cache',
-        'Connection: keep-alive',
-        'DNT: 1',
-        'Upgrade-Insecure-Requests: 1',
-        ],
-    CURLOPT_COOKIEJAR => $cookie,
-    CURLOPT_COOKIEFILE => $cookie,
-];
+function backup_opml2($mu_, $file_name_blog_)
+{
+    $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
 
-$url = 'https://theoldreader.com/users/sign_in';
+    $cookie = tempnam("/tmp", time());
 
-$res = $mu->get_contents($url, $options);
+    $url = 'https://theoldreader.com/users/sign_in';
 
-// error_log($res);
+    $options1 = [
+        CURLOPT_ENCODING => 'gzip, deflate, br',
+        CURLOPT_HTTPHEADER => [
+            'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Language: ja,en-US;q=0.7,en;q=0.3',
+            'Cache-Control: no-cache',
+            'Connection: keep-alive',
+            'DNT: 1',
+            'Upgrade-Insecure-Requests: 1',
+            ],
+        CURLOPT_COOKIEJAR => $cookie,
+        CURLOPT_COOKIEFILE => $cookie,
+    ];
 
-$rc = preg_match('/"authenticity_token".+?value="(.+?)"/', $res, $match);
+    $res = $mu->get_contents($url, $options1);
 
-error_log(print_r($match, true));
+    $rc = preg_match('/"authenticity_token".+?value="(.+?)"/', $res, $match);
 
-$post_data = ['authenticity_token' => $match[1],
-             'utf8' => '&#x2713;',
-             'user[login]' => getenv('TEST3_USER'),
-             'user[password]' => getenv('TEST3_PASSWORD'),
-             ];
+    error_log($log_prefix . print_r($match, true));
 
-$options = [
-    CURLOPT_ENCODING => 'gzip, deflate, br',
-    CURLOPT_HTTPHEADER => [
-        'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-        'Accept-Language: ja,en-US;q=0.7,en;q=0.3',
-        'Cache-Control: no-cache',
-        'Connection: keep-alive',
-        'DNT: 1',
-        'Upgrade-Insecure-Requests: 1',
-        ],
-    CURLOPT_COOKIEJAR => $cookie,
-    CURLOPT_COOKIEFILE => $cookie,
-    CURLOPT_POST => true,
-    CURLOPT_POSTFIELDS => http_build_query($post_data),
-];
+    $post_data = ['authenticity_token' => $match[1],
+                 'utf8' => '&#x2713;',
+                 'user[login]' => getenv('TEST3_USER'),
+                 'user[password]' => getenv('TEST3_PASSWORD'),
+                 ];
 
-$res = $mu->get_contents($url, $options);
+    $options2 = [
+        CURLOPT_ENCODING => 'gzip, deflate, br',
+        CURLOPT_HTTPHEADER => [
+            'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Language: ja,en-US;q=0.7,en;q=0.3',
+            'Cache-Control: no-cache',
+            'Connection: keep-alive',
+            'DNT: 1',
+            'Upgrade-Insecure-Requests: 1',
+            ],
+        CURLOPT_COOKIEJAR => $cookie,
+        CURLOPT_COOKIEFILE => $cookie,
+        CURLOPT_POST => true,
+        CURLOPT_POSTFIELDS => http_build_query($post_data),
+    ];
 
-// error_log($res);
+    $res = $mu->get_contents($url, $options2);
 
-$options = [
-    CURLOPT_ENCODING => 'gzip, deflate, br',
-    CURLOPT_HTTPHEADER => [
-        'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-        'Accept-Language: ja,en-US;q=0.7,en;q=0.3',
-        'Cache-Control: no-cache',
-        'Connection: keep-alive',
-        'DNT: 1',
-        'Upgrade-Insecure-Requests: 1',
-        ],
-    CURLOPT_COOKIEJAR => $cookie,
-    CURLOPT_COOKIEFILE => $cookie,
-];
+    $url = 'https://theoldreader.com/feeds.opml';
 
-$url = 'https://theoldreader.com/feeds.opml';
+    $res = $mu->get_contents($url, $options1);
 
-$res = $mu->get_contents($url, $options);
+    error_log($log_prefix . $res);
 
-error_log($res);
+    unlink($cookie);
 
-unlink($cookie);
+    $feed_count = preg_match_all('/ xmlUrl="/', $res);
+
+    $file_name = '/tmp/' . getenv('HEROKU_APP_NAME')  . '_' .  date('d', strtotime('+9 hours')) . '_OPML2.txt';
+
+    $file_size = $mu_->backup_data($res, $file_name);
+    $file_size = number_format($file_size);
+
+    file_put_contents($file_name_blog_, "OPML backup size : ${file_size}Byte\nFeed count : ${feed_count}\n", FILE_APPEND);
+}
+
