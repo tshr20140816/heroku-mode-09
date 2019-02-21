bf2fe5db020f1cf0f63ce9992cfd038611fdf497
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-02-21 19:33:05 +0900

    Update 20190219.php

--- www/20190219.php
+++ www/20190219.php
@@ -8,75 +8,22 @@ error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
 
 $mu = new MyUtils();
 
-backup_opml2($mu, '/tmp/dummy');
+func_test($mu);
 
-function backup_opml2($mu_, $file_name_blog_)
+function func_test($mu_)
 {
     $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
 
-    $cookie = tempnam("/tmp", time());
-
-    $url = 'https://theoldreader.com/users/sign_in';
-
-    $options1 = [
-        CURLOPT_ENCODING => 'gzip, deflate, br',
-        CURLOPT_HTTPHEADER => [
-            'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-            'Accept-Language: ja,en-US;q=0.7,en;q=0.3',
-            'Cache-Control: no-cache',
-            'Connection: keep-alive',
-            'DNT: 1',
-            'Upgrade-Insecure-Requests: 1',
-            ],
-        CURLOPT_COOKIEJAR => $cookie,
-        CURLOPT_COOKIEFILE => $cookie,
+    $user = base64_decode(getenv('HIDRIVE_USER'));
+    $password = base64_decode(getenv('HIDRIVE_PASSWORD'));
+    
+    $url = "https://webdav.hidrive.strato.com/users/${user}/";
+    $options = [
+        CURLOPT_HTTPAUTH => CURLAUTH_ANY,
+        CURLOPT_USERPWD => "${user}:${password}",
     ];
-
-    $res = $mu_->get_contents($url, $options1);
-
-    $rc = preg_match('/"authenticity_token".+?value="(.+?)"/', $res, $match);
-
-    error_log($log_prefix . print_r($match, true));
-
-    $post_data = ['authenticity_token' => $match[1],
-                 'utf8' => '&#x2713;',
-                 'user[login]' => getenv('TEST3_USER'),
-                 'user[password]' => getenv('TEST3_PASSWORD'),
-                 ];
-
-    $options2 = [
-        CURLOPT_ENCODING => 'gzip, deflate, br',
-        CURLOPT_HTTPHEADER => [
-            'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-            'Accept-Language: ja,en-US;q=0.7,en;q=0.3',
-            'Cache-Control: no-cache',
-            'Connection: keep-alive',
-            'DNT: 1',
-            'Upgrade-Insecure-Requests: 1',
-            ],
-        CURLOPT_COOKIEJAR => $cookie,
-        CURLOPT_COOKIEFILE => $cookie,
-        CURLOPT_POST => true,
-        CURLOPT_POSTFIELDS => http_build_query($post_data),
-    ];
-
-    $res = $mu_->get_contents($url, $options2);
-
-    $url = 'https://theoldreader.com/feeds.opml';
-
-    $res = $mu_->get_contents($url, $options1);
-
-    // error_log($log_prefix . $res);
-
-    unlink($cookie);
-
-    $feed_count = preg_match_all('/ xmlUrl="/', $res);
-
-    $file_name = '/tmp/' . getenv('HEROKU_APP_NAME')  . '_' .  date('d', strtotime('+9 hours')) . '_OPML2.txt';
-
-    $file_size = $mu_->backup_data($res, $file_name);
-    $file_size = number_format($file_size);
-
-    file_put_contents($file_name_blog_, "OPML backup size : ${file_size}Byte\nFeed count : ${feed_count}\n", FILE_APPEND);
+    $res = $mu_->get_contents($url, $options);
+    
+    error_log($res);
 }
 
