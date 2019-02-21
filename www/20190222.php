bb3063152f8ff7799564727bf59533631e0b7621
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-02-22 00:30:41 +0900

    Create 20190222.php

---
+++ www/20190222.php
@@ -0,0 +1,18 @@
+<?php
+
+include(dirname(__FILE__) . '/../classes/MyUtils.php');
+$pid = getmypid();
+$requesturi = $_SERVER['REQUEST_URI'];
+$time_start = microtime(true);
+error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
+
+$mu = new MyUtils();
+
+func_test($mu);
+
+error_log("${pid} FINISH " . substr((microtime(true) - $time_start), 0, 6) . 's');
+
+function func_test($mu_)
+{
+
+}
