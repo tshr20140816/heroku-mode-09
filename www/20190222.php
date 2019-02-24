0d8cddc6e5906191d4bbad089e54298908afc7c6
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-02-25 05:37:50 +0900

    Update 20190222.php

--- www/20190222.php
+++ www/20190222.php
@@ -16,18 +16,26 @@ function func_test($mu_)
 {
     $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
 
+    // Get Folders
+    $folder_id_label = $mu_->get_folder_id('LABEL');
+    // Get Contexts
+    $list_context_id = $mu_->get_contexts();
+    
+    $list_add_task = [];
+    $add_task_template = '{"title":"__TITLE__","duedate":"__DUEDATE__","context":"__CONTEXT__","tag":"F1","folder":"'
+      . $folder_id_label . '"}';
+    
     $url = 'http://otn.fujitv.co.jp/b_hp/918200222.html';
     $res = $mu_->get_contents($url);
     
     $rc = preg_match('/<title>(\d+)/', $res, $match);
     
-    error_log(print_r($match, true));
+    error_log($log_prefix . print_r($match, true));
+    $yyyy = $match[1];
     
     $rc = preg_match_all('/<li>(.+?)<\/li>/s', $res, $matches);
     
     // error_log(print_r($matches, true));
-    
-    $list_add_task = [];
     foreach ($matches[1] as $item) {
         if (strpos($item, '生放送') === false) {
             continue;
@@ -38,8 +46,15 @@ function func_test($mu_)
         $item = str_replace('～', '-', $item);
         $item = preg_replace('/\s+/s', ' ', strip_tags($item));
         $item = trim(preg_replace('/\(.+?\)/', '', $item));
-        error_log($item);
-        $yyyymmdd = date('Y') . '/' . substr($item, 0, 5);
-        error_log($yyyymmdd);
+        // error_log($item);
+        $timestamp = strtotime($yyyy . '/' . substr($item, 0, 5));
+        if ($timestamp < time()) {
+            continue;
+        }
+        $tmp = str_replace('__TITLE__', $item, $add_task_template);
+        $tmp = str_replace('__DUEDATE__', $timestamp, $tmp);
+        $list_add_task[] = str_replace('__CONTEXT__', $list_context_id[date('w', $timestamp)], $tmp);
     }
+    $list_add_task = array_unique($list_add_task);
+    error_log(print_r($list_add_task, true));
 }
