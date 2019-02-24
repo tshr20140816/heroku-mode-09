390a3da6f116db0ca8778f407f997d3b344cc0cd
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-02-25 05:12:24 +0900

    Update start_web.sh

--- start_web.sh
+++ start_web.sh
@@ -27,9 +27,7 @@ echo "$(httpd -v)" > /tmp/apache_current_version
 echo "$(php -v | head -n 1)" > /tmp/php_current_version
 echo "$(curl -V | head -n 1)" > /tmp/curl_current_version
 
-minute=`date +%-M`
-
-if [ minute -lt 10 ]; then
+if [ $(date +%-M) -lt 10 ]; then
     echo 'EXECUTE'
 fi
 # heroku-buildpack-php
