9887ea0a281dd882a2b9f74d2fd43257bbab9fc0
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-02-27 08:57:25 +0900

    Update 20190225.php

--- www/20190225.php
+++ www/20190225.php
@@ -8,7 +8,7 @@ error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
 
 $mu = new MyUtils();
 
-$url = 'https://secure.reservation.jp/sanco-inn/stay_pc/rsv/rsv_src_pln.aspx?cond=or&dt_tbd=0&le=1&rc=1&pmin=0&ra=&pa=&cl_tbd=0&mc=2&rt=4%3a8&st=0&pmax=2147483647&cc=&smc_id=&hi_id=10&dt=2019/8/25&lang=ja-JP';
+$url = 'https://www.pcloud.com/';
 
 $res = $mu->get_contents($url);
 
