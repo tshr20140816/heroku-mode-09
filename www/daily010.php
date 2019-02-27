8057249bc7e1307db993fa55cd0d8140a3f2fef8
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-02-27 10:36:05 +0900

    Create daily010.php

---
+++ www/daily010.php
@@ -0,0 +1,20 @@
+<?php
+
+include(dirname(__FILE__) . '/../classes/MyUtils.php');
+
+$pid = getmypid();
+$requesturi = $_SERVER['REQUEST_URI'];
+$time_start = microtime(true);
+
+error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
+
+$mu = new MyUtils();
+
+$url = 'https://' . getenv('HEROKU_APP_NAME') . '.herokuapp.com/daily020.php';
+$options = [
+    CURLOPT_TIMEOUT => 3,
+    CURLOPT_HTTPAUTH => CURLAUTH_BASIC,
+    CURLOPT_USERPWD => getenv('BASIC_USER') . ':' . getenv('BASIC_PASSWORD')];
+$res = $mu->get_contents($url, $options);
+
+error_log("${pid} FINISH " . substr((microtime(true) - $time_start), 0, 6) . 's');
