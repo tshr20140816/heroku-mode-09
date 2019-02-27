ca70b3ce3469c9a1e04c52c5abd792ee2a0b34ba
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-02-27 10:58:09 +0900

    Update daily010.php

--- www/daily010.php
+++ www/daily010.php
@@ -14,7 +14,8 @@ $url = 'https://' . getenv('HEROKU_APP_NAME') . '.herokuapp.com/daily020.php';
 $options = [
     CURLOPT_TIMEOUT => 3,
     CURLOPT_HTTPAUTH => CURLAUTH_BASIC,
-    CURLOPT_USERPWD => getenv('BASIC_USER') . ':' . getenv('BASIC_PASSWORD')];
+    CURLOPT_USERPWD => getenv('BASIC_USER') . ':' . getenv('BASIC_PASSWORD'),
+];
 $res = $mu->get_contents($url, $options);
 
 error_log("${pid} FINISH " . substr((microtime(true) - $time_start), 0, 6) . 's');
