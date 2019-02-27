1cd70157a82dd86ad45c626f2777fcd9794017db
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-02-27 15:47:30 +0900

    Update 20190222.php

--- www/20190222.php
+++ www/20190222.php
@@ -17,6 +17,17 @@ error_log("${pid} FINISH " . substr((microtime(true) - $time_start), 0, 6) . 's'
 
 function func_test($mu_, $file_name_blog_)
 {
+    $xml = <<< __HEREDOC__
+<?xml version="1.0" encoding="utf-8"?>
+<entry xmlns="http://www.w3.org/2005/Atom" xmlns:app="http://www.w3.org/2007/app">
+  <title>__TITLE__</title>
+  <content type="text/plain">__CONTENT__</content>
+</entry>
+__HEREDOC__;
+    
+    $xml = str_replace('__TITLE__', '日本語テスト', $xml);
+    $xml = str_replace('__CONTENT__', '日本語', $xml);
+    
     $hatena_id = getenv('HATENA_ID');
     $hatena_blog_id = getenv('HATENA_BLOG_ID');
     $hatena_api_key = getenv('HATENA_API_KEY');
@@ -26,15 +37,10 @@ function func_test($mu_, $file_name_blog_)
     $options = [
         CURLOPT_HTTPAUTH => CURLAUTH_BASIC,
         CURLOPT_USERPWD => "${hatena_id}:${hatena_api_key}",
+        CURLOPT_POST => true,
+        CURLOPT_POSTFIELDS => $xml,
+        CURLOPT_HEADER => true,
     ];
     
-    $xml = <<< __HEREDOC__
-<?xml version="1.0" encoding="utf-8"?>
-<entry xmlns="http://www.w3.org/2005/Atom" xmlns:app="http://www.w3.org/2007/app">
-  <title>__TITLE__</title>
-  <content type="text/plain">__CONTENT__</content>
-</entry>
-__HEREDOC__;
-    
-    
+    $rc = $mu_->get_contents($url, $options);
 }
