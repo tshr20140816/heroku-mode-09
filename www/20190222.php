f5b3d6d7e81de0888b4e82bb2aef49ef7f2c5d8e
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-02-27 15:41:31 +0900

    Update 20190222.php

--- www/20190222.php
+++ www/20190222.php
@@ -17,5 +17,24 @@ error_log("${pid} FINISH " . substr((microtime(true) - $time_start), 0, 6) . 's'
 
 function func_test($mu_, $file_name_blog_)
 {
-    $a = 0;
+    $hatena_id = getenv('HATENA_ID');
+    $hatena_blog_id = getenv('HATENA_BLOG_ID');
+    $hatena_api_key = getenv('HATENA_API_KEY');
+
+    $url = "https://blog.hatena.ne.jp/${hatena_id}/${hatena_blog_id}/atom";
+    
+    $options = [
+        CURLOPT_HTTPAUTH => CURLAUTH_BASIC,
+        CURLOPT_USERPWD => "${hatena_id}:${hatena_api_key}",
+    ];
+    
+    $xml = <<< __HEREDOC__
+<?xml version="1.0" encoding="utf-8"?>
+<entry xmlns="http://www.w3.org/2005/Atom" xmlns:app="http://www.w3.org/2007/app">
+  <title>__TITLE__</title>
+  <content type="text/plain">__CONTENT__</content>
+</entry>
+__HEREDOC__;
+    
+    
 }
