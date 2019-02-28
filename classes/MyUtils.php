80a25ec5bca26c0ee06186719ba7551c51e12589
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-02-28 11:06:37 +0900

    Update MyUtils.php

--- classes/MyUtils.php
+++ classes/MyUtils.php
@@ -390,6 +390,44 @@ __HEREDOC__;
         }
     }
 
+    public function post_blog_hatena($title_, $description_ = null)
+    {
+        $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
+
+        if (is_null($description_)) {
+            $description_ = '.';
+        }
+
+        $hatena_id = base64_decode(getenv('HATENA_ID'));
+        $hatena_blog_id = base64_decode(getenv('HATENA_BLOG_ID'));
+        $hatena_api_key = base64_decode(getenv('HATENA_API_KEY'));
+
+        $xml = <<< __HEREDOC__
+<?xml version="1.0" encoding="utf-8"?>
+<entry xmlns="http://www.w3.org/2005/Atom" xmlns:app="http://www.w3.org/2007/app">
+  <title>__TITLE__</title>
+  <content type="text/plain">__CONTENT__</content>
+</entry>
+__HEREDOC__;
+
+        $xml = str_replace('__TITLE__', date('Y/m/d H:i:s', strtotime('+9 hours')) . " ${title_}", $xml);
+        $xml = str_replace('__CONTENT__', $description_, $xml);
+
+        $url = "https://blog.hatena.ne.jp/${hatena_id}/${hatena_blog_id}/atom/entry";
+
+        $options = [
+            CURLOPT_HTTPAUTH => CURLAUTH_BASIC,
+            CURLOPT_USERPWD => "${hatena_id}:${hatena_api_key}",
+            CURLOPT_POST => true,
+            CURLOPT_POSTFIELDS => $xml,
+            CURLOPT_HEADER => true,
+        ];
+
+        $res = $this->get_contents($url, $options);
+
+        error_log($log_prefix . 'RESULT : ' . $res);
+    }
+
     public function get_contents($url_, $options_ = null, $is_cache_search = false)
     {
         $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
