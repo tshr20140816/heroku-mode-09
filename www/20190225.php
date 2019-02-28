0ade7a816e5d5afed2b5b363c9eeceb062af3e60
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-02-28 20:00:54 +0900

    Update 20190225.php

--- www/20190225.php
+++ www/20190225.php
@@ -15,7 +15,7 @@ $options = [
     CURLOPT_USERPWD => getenv('PCLOUD_USER') . ':' . getenv('PCLOUD_PASSWORD'),
 ];
 
-// $res = $mu->get_contents($url, $options);
+$res = $mu->get_contents($url, $options);
 
 $url = 'https://api.pcloud.com/userinfo?getauth=1&logout=1&username=' . getenv('PCLOUD_USER') . '&password=' . getenv('PCLOUD_PASSWORD');
 
