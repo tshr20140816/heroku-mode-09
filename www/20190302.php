f0d4d9adf1841104beca0380790bcc771fe99b63
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-03-02 09:34:29 +0900

    Update 20190302.php

--- www/20190302.php
+++ www/20190302.php
@@ -6,3 +6,29 @@ $time_start = microtime(true);
 error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
 $mu = new MyUtils();
 
+$user_teracloud = base64_decode(getenv('TERACLOUD_USER'));
+$password_teracloud = base64_decode(getenv('TERACLOUD_PASSWORD'));
+
+$file_name = '/tmp/test.txt';
+
+file_put_contents($file_name, 'TEST');
+
+$file_size = filesize($file_name);
+$fh = fopen($file_name, 'r');
+
+$url = 'https://uno.teracloud.jp/dav/' . pathinfo($file_name)['basename'];
+
+$options = [
+    CURLOPT_HTTPAUTH => CURLAUTH_BASIC,
+    CURLOPT_USERPWD => "${user_teracloud}:${password_teracloud}",
+    CURLOPT_PUT => true,
+    CURLOPT_INFILE => $fh,
+    CURLOPT_INFILESIZE => $file_size,
+];
+$res = $mu->get_contents($url, $options);
+
+error_log($res);
+
+fclose($fh);
+
+unlink($file_name);
