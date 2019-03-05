25aa99726a9c593dc9daed203b0d4c4ff94d2cc2
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-03-05 13:13:56 +0900

    Update 20190131.php

--- www/20190131.php
+++ www/20190131.php
@@ -27,7 +27,7 @@ function delete_blog_wordpress($mu) {
     $blogid = $result[0]['blogid'];
     
     $client = XML_RPC2_Client::create($url, ['prefix' => 'wp.']);
-    $result = $client->getPosts($blogid, $username, $password, ['number' => 10, 'offset' => 2000,]);
+    $result = $client->getPosts($blogid, $username, $password, ['number' => 10, 'offset' => 3000,]);
     
     error_log(print_r($result, true));
 }
