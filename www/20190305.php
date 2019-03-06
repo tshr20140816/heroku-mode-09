1c958336b2de85c2c3c96e66ef66ea09e2b6a1ae
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-03-06 22:35:25 +0900

    Update 20190305.php

--- www/20190305.php
+++ www/20190305.php
@@ -5,7 +5,6 @@ $requesturi = $_SERVER['REQUEST_URI'];
 $time_start = microtime(true);
 error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
 $mu = new MyUtils();
-$access_token = $mu->get_access_token();
 
 $rc = func_test($mu, '/tmp/dummy');
 
@@ -13,9 +12,9 @@ function func_test($mu_, $file_name_blog_)
 {
     $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
 
-    // Get Folders
-    $folder_id_label = $mu_->get_folder_id('LABEL');
-    // Get Contexts
-    $list_context_id = $mu_->get_contexts();
-
+    $url = 'https://member.livedoor.com/login/';
+    
+    $mu_->get_contents($url);
+    
+    error_log($url);
 }
