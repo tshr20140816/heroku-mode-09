22afb1df860aef9ed6409e80e46dbb05b0a714c6
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-03-06 22:54:38 +0900

    Update 20190305.php

--- www/20190305.php
+++ www/20190305.php
@@ -12,18 +12,4 @@ function func_test($mu_, $file_name_blog_)
 {
     $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
 
-    $url = 'https://member.livedoor.com/login/';
-    
-    $res = $mu_->get_contents($url);
-    
-    error_log($res);
-    
-    $rc = preg_match('/<input type="hidden" name="_token" value="(.+?)" \/>/s', $res, $match);
-    
-    error_log(print_r($match, true));
-    
-    $livedoor_id = base64_decode(getenv('LIVEDOOR_ID'));
-    $livedoor_password = base64_decode(getenv('LIVEDOOR_PASSWORD'));
-    
-    $post_data = ['_token' => $match[1], '.next' => '', '.sv' => '', 'livedoor_id' => $livedoor_id, 'password' => $livedoor_password, 'x' => 80, 'y' => 22];
 }
