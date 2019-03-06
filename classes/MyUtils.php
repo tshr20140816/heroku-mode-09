0835189e9fa78db01fcaf6cf75e8482baa3c2f19
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-03-07 03:01:49 +0900

    Update MyUtils.php

--- classes/MyUtils.php
+++ classes/MyUtils.php
@@ -430,6 +430,45 @@ __HEREDOC__;
         error_log($log_prefix . 'RESULT : ' . $res);
     }
 
+    function post_blog_livedoor($title_, $description_ = null)
+    {
+        $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
+
+        if (is_null($description_)) {
+            $description_ = '.';
+        }
+
+        $livedoor_id = base64_decode(getenv('LIVEDOOR_ID'));
+        $livedoor_atom_password = base64_decode(getenv('LIVEDOOR_ATOM_PASSWORD'));
+
+        $xml = <<< __HEREDOC__
+<?xml version="1.0" encoding="utf-8"?>
+<entry xmlns="http://www.w3.org/2005/Atom" xmlns:app="http://www.w3.org/2007/app">
+  <title>__TITLE__</title>
+  <content type="text/plain">__CONTENT__</content>
+</entry>
+__HEREDOC__;
+
+        $xml = str_replace('__TITLE__', date('Y/m/d H:i:s', strtotime('+9 hours')) . " ${title_}", $xml);
+        $xml = str_replace('__CONTENT__', htmlspecialchars(nl2br($description_)), $xml);
+
+        $url = "https://livedoor.blogcms.jp/atompub/${livedoor_id}/article";
+
+        $options = [
+            CURLOPT_HTTPAUTH => CURLAUTH_BASIC,
+            CURLOPT_USERPWD => "${livedoor_id}:${livedoor_atom_password}",
+            CURLOPT_HEADER => true,
+            CURLOPT_POST => true,
+            CURLOPT_POSTFIELDS => $xml,
+            CURLOPT_BINARYTRANSFER => true,
+            CURLOPT_HTTPHEADER => ['Accept: application/atom+xml;type=entry',],
+        ];
+
+        $res = $this->get_contents($url, $options);
+
+        error_log($log_prefix . 'RESULT : ' . $res);
+    }
+
     public function get_contents($url_, $options_ = null, $is_cache_search = false)
     {
         $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
