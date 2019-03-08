8e926955f08ad8ebd53ccbad8e7b70a816003cc7
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-03-08 13:35:07 +0900

    Update 20190308.php

--- www/20190308.php
+++ www/20190308.php
@@ -7,13 +7,13 @@ error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
 $mu = new MyUtils();
 
 $user_cloudme = getenv('CLOUDME_USER');
-$user_password = getenv('CLOUDME_PASSWORD');
+$password_cloudme = getenv('CLOUDME_PASSWORD');
 
-$url = "https://webdav.cloudme.com/${user_cloudme}";
+$url = "https://webdav.cloudme.com/${user_cloudme}/";
 
 $options = [
-    CURLOPT_HTTPAUTH => CURLAUTH_BASIC,
-    CURLOPT_USERPWD => "${user_cloudme}:${user_password}",
+    CURLOPT_HTTPAUTH => CURLAUTH_ANY,
+    CURLOPT_USERPWD => "${user_cloudme}:${password_cloudme}",
 ];
 
 $res = $mu->get_contents($url, $options);
