e5862b12d40fb206d4e7aed51b9e7277fca36d7b
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-03-08 13:25:56 +0900

    Create 20190308.php

---
+++ www/20190308.php
@@ -0,0 +1,21 @@
+<?php
+include(dirname(__FILE__) . '/../classes/MyUtils.php');
+$pid = getmypid();
+$requesturi = $_SERVER['REQUEST_URI'];
+$time_start = microtime(true);
+error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
+$mu = new MyUtils();
+
+$user_cloudme = getenv('CLOUDME_USER');
+$user_password = getenv('CLOUDME_PASSWORD');
+
+$url = 'https://webdav.cloudme.com/${user_cloudme}';
+
+$options = [
+    CURLOPT_HTTPAUTH => CURLAUTH_BASIC,
+    CURLOPT_USERPWD => "${user_hidrive}:${password_hidrive}",
+];
+
+$res = $mu->get_contents($url, $options);
+
+error_log($res);
