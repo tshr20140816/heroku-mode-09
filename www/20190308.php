3eed79aca1d122182289892ba36592e584c99bf9
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-03-12 09:40:40 +0900

    Update 20190308.php

--- www/20190308.php
+++ www/20190308.php
@@ -6,17 +6,21 @@ $time_start = microtime(true);
 error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
 $mu = new MyUtils();
 
+/*
 error_log($mu->get_encrypt_string(base64_decode(getenv('TERACLOUD_API_KEY'))));
 error_log($mu->get_encrypt_string(base64_decode(getenv('TERACLOUD_NODE'))));
 error_log($mu->get_encrypt_string(base64_decode(getenv('TERACLOUD_PASSWORD'))));
 error_log($mu->get_encrypt_string(base64_decode(getenv('TERACLOUD_USER'))));
-
-/*
-error_log(base64_encode($mu->get_env('WORDPRESS_PASSWORD', true)));
-error_log(getenv('WORDPRESS_PASSWORD'));
-error_log(base64_encode($mu->get_env('WORDPRESS_USERNAME', true)));
-error_log(getenv('WORDPRESS_USERNAME'));
 */
+
+error_log(base64_encode($mu->get_env('TERACLOUD_API_KEY', true)));
+error_log(getenv('TERACLOUD_API_KEY'));
+error_log(base64_encode($mu->get_env('TERACLOUD_NODE', true)));
+error_log(getenv('TERACLOUD_NODE'));
+error_log(base64_encode($mu->get_env('TERACLOUD_PASSWORD', true)));
+error_log(getenv('TERACLOUD_PASSWORD'));
+error_log(base64_encode($mu->get_env('TERACLOUD_USER', true)));
+error_log(getenv('TERACLOUD_USER'));
 
 /*
 $user_cloudme = getenv('CLOUDME_USER');
