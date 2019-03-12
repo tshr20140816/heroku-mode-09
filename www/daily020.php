f2256214c5355a579ecc08f409666e4e08c1c9b3
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-03-12 09:29:13 +0900

    Update daily020.php

--- www/daily020.php
+++ www/daily020.php
@@ -230,7 +230,7 @@ for ($i = 11; $i > -1; $i--) {
     $res = $mu->get_contents($url, null, true);
 }
 
-file_put_contents('/tmp/' . basename(__FILE__) . '.txt', 'DUMMY');
+file_put_contents('/tmp/' . basename(__FILE__) . '.txt', time());
 
 $time_finish = microtime(true);
 $mu->post_blog_wordpress("${requesturi} [" . substr(($time_finish - $time_start), 0, 6) . 's]',
