dec4c11e71e12dba766112ddf925783d6ba63c87
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-03-12 11:05:11 +0900

    Update 20190308.php

--- www/20190308.php
+++ www/20190308.php
@@ -6,10 +6,8 @@ $time_start = microtime(true);
 error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
 $mu = new MyUtils();
 
-/*
-error_log($mu->get_encrypt_string(base64_decode(getenv('OPENDRIVE_USER'))));
-error_log($mu->get_encrypt_string(base64_decode(getenv('OPENDRIVE_PASSWORD'))));
-*/
+error_log($mu->get_encrypt_string(base64_decode(getenv('THEOLDREADER_USER'))));
+error_log($mu->get_encrypt_string(base64_decode(getenv('THEOLDREADER_PASSWORD'))));
 
 /*
 error_log(base64_encode($mu->get_env('OPENDRIVE_USER', true)));
@@ -35,32 +33,3 @@ $res = $mu->get_contents($url, $options);
 
 error_log($res);
 */
-
-check_version_postgresql($mu, '/tmp/dummy');
-
-function check_version_postgresql($mu_, $file_name_blog_)
-{
-    $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
-
-    $url = 'https://www.postgresql.org/?4nocache' . date('Ymd', strtotime('+9 hours'));
-    $res = $mu_->get_contents($url, null, true);
-    $tmp = explode('<h2>Latest Releases</h2>', $res);
-    $tmp = explode('</ul>', $tmp[1]);
-    $tmp = str_replace('&middot;', '', $tmp[0]);
-    $rc = preg_match_all('/<li .+?>(.+?)<a/s', $tmp, $matches);
-    
-    $version_latest = '';
-    foreach ($matches[1] as $match) {
-        $version_latest .= str_replace('  ', ' ', strip_tags($match)) . "\n";
-    }
-
-    $pdo = $mu_->get_pdo();
-    $version_current = '';
-    foreach ($pdo->query('SELECT version();') as $row) {
-        $version_current = $row[0];
-    }
-    $pdo = null;
-
-    $content = "\nPostgreSQL Version\nlatest : ${version_latest}current : ${version_current}\n";
-    file_put_contents($file_name_blog_, $content, FILE_APPEND);
-}
