9ed530f4577150eb52370259dd0d900791817560
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-03-13 13:08:44 +0900

    Update 20190308.php

--- www/20190308.php
+++ www/20190308.php
@@ -6,18 +6,32 @@ $time_start = microtime(true);
 error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
 $mu = new MyUtils();
 
+$cookie = tempnam("/tmp", md5(microtime(true)));
+
+$url = "https://webdav.4shared.com";
+
+$options = [
+    CURLOPT_COOKIEJAR => $cookie,
+    CURLOPT_COOKIEFILE => $cookie,
+];
+
+$res = $mu->get_contents($url, $options);
+
 $user_cloudme = getenv('CLOUDME_USER');
 $password_cloudme = getenv('CLOUDME_PASSWORD');
 
-$url = "https://webdav.cloudme.com/${user_cloudme}";
 $url = "https://webdav.4shared.com";
 
 $options = [
     CURLOPT_HTTPAUTH => CURLAUTH_ANY,
     CURLOPT_USERPWD => "${user_cloudme}:${password_cloudme}",
     CURLOPT_HEADER => true,
+    CURLOPT_COOKIEJAR => $cookie,
+    CURLOPT_COOKIEFILE => $cookie,
 ];
 
 $res = $mu->get_contents($url, $options);
 
 error_log($res);
+
+@unlink($cookie);
