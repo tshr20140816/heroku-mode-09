6e16e20dc76804e72327d430aa8eacaaece91210
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-03-14 09:46:13 +0900

    Update daily020.php

--- www/daily020.php
+++ www/daily020.php
@@ -64,6 +64,9 @@ check_version_curl($mu, $file_name_blog);
 // PostgreSQL version check
 check_version_postgresql($mu, $file_name_blog);
 
+// Ruby version check
+check_version_ruby($mu, $file_name_blog);
+
 //
 
 $pdo = $mu->get_pdo();
@@ -938,3 +941,30 @@ function check_version_postgresql($mu_, $file_name_blog_)
     $content = "\nPostgreSQL Version\nlatest : ${version_latest}current : ${version_current}\n";
     file_put_contents($file_name_blog_, $content, FILE_APPEND);
 }
+
+function check_version_ruby($mu_, $file_name_blog_)
+{
+    $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
+
+    $url = 'https://devcenter.heroku.com/articles/ruby-support?' . date('Ymd', strtotime('+9 hours'));
+    $res = $mu_->get_contents($url, null, true);
+
+    $tmp = explode('<p><strong>MRI:</strong></p>', $res);
+    $tmp = explode('</ul>', $tmp[1]);
+    $rc = preg_match_all('/<li>(.+?)<\/li>/s', $tmp[0], $matches);
+
+    rsort($matches[1]);
+    $version_support = '';
+    foreach ($matches[1] as $line) {
+        $version_support .= trim(strip_tags($line)) . "\n";
+    }
+
+    $url = getenv('TARGET_GEM_FILE') . '?' . date('Ymd', strtotime('+9 hours'));
+    $res = $mu_->get_contents($url, null, true);
+    $rc = preg_match('/ruby "(.+?)"/', $res, $match);
+    $version_current = $match[1];
+
+    $content = "\nRuby Version\ncurrent : ${version_current}\nsupport : ${version_support}";
+    file_put_contents($file_name_blog_, $content, FILE_APPEND);
+}
+
