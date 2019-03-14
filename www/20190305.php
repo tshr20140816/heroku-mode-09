6352c2620eeb18c4c5451d3fa2ba793f63bff926
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-03-14 12:50:49 +0900

    Update 20190305.php

--- www/20190305.php
+++ www/20190305.php
@@ -26,15 +26,6 @@ function func_test($mu_, $file_name_blog_)
     $login_user = getenv('TEST_USER_01');
     $login_password = getenv('TEST_PASSWORD_01');
     $json = '{"op":"login","user":"' . $login_user .'","password":"' . $login_password . '"}';
-    /*
-    $options = [
-        CURLOPT_HTTPAUTH => CURLAUTH_BASIC,
-        CURLOPT_USERPWD => "${basic_user}:${basic_password}",
-        CURLOPT_HTTPHEADER => ['Content-Type: application/json',],
-        CURLOPT_POST => true,
-        CURLOPT_POSTFIELDS => $json,
-    ];
-    */
     $options = $options_base + [CURLOPT_POSTFIELDS => $json,];
     $res = $mu_->get_contents($url, $options);
     error_log($res);
@@ -45,15 +36,6 @@ function func_test($mu_, $file_name_blog_)
     $url_feed = "http://blog.livedoor.jp/${livedoor_id}/atom.xml";
     
     $json = '{"sid":"' . $session_id . '","op":"getFeeds","cat_id":-3}';
-    /*
-    $options = [
-        CURLOPT_HTTPAUTH => CURLAUTH_BASIC,
-        CURLOPT_USERPWD => "${basic_user}:${basic_password}",
-        CURLOPT_HTTPHEADER => ['Content-Type: application/json',],
-        CURLOPT_POST => true,
-        CURLOPT_POSTFIELDS => $json,
-    ];
-    */
     $options = $options_base + [CURLOPT_POSTFIELDS => $json,];
     $res = $mu_->get_contents($url, $options);
     $data = json_decode($res);
@@ -62,15 +44,6 @@ function func_test($mu_, $file_name_blog_)
         // error_log($feed->id);
         if ($url_feed == $feed->feed_url) {
             $json = '{"sid":"' . $session_id . '","op":"updateFeed","feed_id":' . $feed->id . '}';
-            /*
-            $options = [
-                CURLOPT_HTTPAUTH => CURLAUTH_BASIC,
-                CURLOPT_USERPWD => "${basic_user}:${basic_password}",
-                CURLOPT_HTTPHEADER => ['Content-Type: application/json',],
-                CURLOPT_POST => true,
-                CURLOPT_POSTFIELDS => $json,
-            ];
-            */
             $options = $options_base + [CURLOPT_POSTFIELDS => $json,];
             $res = $mu_->get_contents($url, $options);
             error_log(print_r(json_decode($res), true));
