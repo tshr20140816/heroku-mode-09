b4c25f0927b91766cb03f95341ce895cde913736
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-03-16 07:42:08 +0900

    Update 20190316.php

--- www/20190316.php
+++ www/20190316.php
@@ -22,5 +22,5 @@ function func_test($mu_, $file_name_blog_)
     //error_log(trim(trim($tmp[0]), ';'));
     //error_log(print_r(json_decode(trim(trim($tmp[0]), ';')), true));
     $json = json_decode(trim(trim($tmp[0]), ';'));
-    error_log(print_r($json->contents->twoColumnBrowseResultsRenderer->tabs[1], true));
+    error_log(print_r($json->contents->twoColumnBrowseResultsRenderer->tabs[1]->tabRenderer->content->sectionListRenderer->contents, true));
 }
