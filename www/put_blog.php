941191a118aa9b4c46682a86cd28c802eaee257f
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-03-15 10:06:41 +0900

    Update put_blog.php

--- www/put_blog.php
+++ www/put_blog.php
@@ -8,3 +8,20 @@ $time_start = microtime(true);
 error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
 
 $mu = new MyUtils();
+
+if (!isset($_GET['access_key']) || $_GET['access_key'] != base64_decode(getenv('ACCESS_KEY')) || strlen(getenv('ACCESS_KEY')) == 0) {
+    error_log("${pid} FINISH Invalid Param");
+    exit();
+}
+
+$title = $_GET['title'];
+$content = $_GET['content'];
+
+if (strlen($title) == 0) {
+    error_log("${pid} FINISH Invalid Param");
+    exit();
+}
+
+$time_finish = microtime(true);
+$mu->post_blog_wordpress($title, $content);
+error_log("${pid} FINISH " . substr(($time_finish - $time_start), 0, 6) . 's ' . substr((microtime(true) - $time_start), 0, 6) . 's');
