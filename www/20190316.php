3b441a5585530e4b45b63882e047d2e5a67aa80c
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-03-16 12:57:32 +0900

    Update 20190316.php

--- www/20190316.php
+++ www/20190316.php
@@ -7,6 +7,8 @@ error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
 $mu = new MyUtils();
 $rc = func_test($mu, '/tmp/dummy');
 
+error_log("${pid} FINISH " . substr((microtime(true) - $time_start), 0, 6) . 's');
+
 function func_test($mu_, $file_name_blog_)
 {
     $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
