f585c7bbcd9f5db0e42117212e4228e1486c9981
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-03-22 17:25:50 +0900

    Update 20190316.php

--- www/20190316.php
+++ www/20190316.php
@@ -32,10 +32,10 @@ function func_test($mu_, $file_name_blog_)
         ['post_title', 'post_date_gmt'],
     );
     
-    error_log(print_r($results, true));
-    /*
+    // error_log(print_r($results, true));
     foreach ($results as $result) {
         error_log($result['post_title']);
+        error_log(time() - $result['post_date_gmt']->timestamp);
     }
-    */
+
 }
