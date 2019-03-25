507bfcf0a0a0ea92bfcef71d714e3e5b4c479b8c
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-03-25 11:09:27 +0900

    Update 20190325.php

--- www/20190325.php
+++ www/20190325.php
@@ -71,7 +71,7 @@ __HEREDOC__;
     $res = $mu_->get_contents($url, $options3);
     
     // error_log($res);
-    $res = preg_replace('/\n+/s', '\n', $res);
+    $res = preg_replace('/\n+/s', "\n", $res);
     $rc = preg_match_all('/<li>(.+?)<\/li>/s', $res, $matches);
     
     //error_log(print_r($matches[1], true));
