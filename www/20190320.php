cd4b91de4f9c9531c8650ad73bdf49e917c3286f
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-03-25 14:33:26 +0900

    Update 20190320.php

--- www/20190320.php
+++ www/20190320.php
@@ -5,6 +5,7 @@ $requesturi = $_SERVER['REQUEST_URI'];
 $time_start = microtime(true);
 error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
 $mu = new MyUtils();
+$access_token = $mu->get_access_token();
 $rc = func_test3($mu, '/tmp/dummy');
 error_log("${pid} FINISH " . substr((microtime(true) - $time_start), 0, 6) . 's');
 
@@ -12,6 +13,10 @@ function func_test3($mu_, $file_name_blog_)
 {
     $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
 
+    $folder_id_private = $mu_->get_folder_id('PRIVATE');
+    $list_context_id = $mu_->get_contexts();
+    $list_add_task = [];
+    
     $yyyy = date('Y');
     $ymd = date('Ymd', strtotime('+9 hours'));
     for ($i = 3; $i < 10; $i++) {
@@ -27,6 +32,23 @@ function func_test3($mu_, $file_name_blog_)
             // error_log($item);
             $rc = preg_match('/<.+?>(\d+)\/(\d+).+?>.*?<.+?>(.+?)<.+?>.*?<.+?>(.+?)<.+?>.*?<.+?>(.+?)<.+?>.*?<.+?>(.+?)<.+?>.*?<.+?>(.+?)<.+?>/s', $item, $match);
             error_log(print_r($match, true));
+            
+            $timestamp = strtotime($yyyy . '/' . $match[1] . '/' . $match[2]);
+            if ($timestamp < time()) {
+                continue;
+            }
+            
+            $title = $match[1] . '/' . $match[2] . ' ' . $match[3] . ' ' . $match[4] . 'v' . $match[6] . ' ' . $match[7];
+            
+            $list_add_task[$hash] = '{"title":"' . $title
+                . '","duedate":"' . $timestamp
+                . '","context":"' . $list_context_id[date('w', $timestamp)]
+                . '","tag":"CARP","folder":"' . $folder_id_private . '"}';
         }
     }
+    $count_task = count($list_add_task);
+    
+    file_put_contents($file_name_blog_, "Firm Task Add : ${count_task}\n", FILE_APPEND);
+    error_log($log_prefix . 'Tasks Firm : ' . print_r($list_add_task, true));
+    return $list_add_task;
 }
