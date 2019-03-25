1e7acbb57b092f1dbe58b97ae7c0e5fd3bda8b68
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-03-25 14:21:57 +0900

    Update 20190320.php

--- www/20190320.php
+++ www/20190320.php
@@ -11,20 +11,22 @@ error_log("${pid} FINISH " . substr((microtime(true) - $time_start), 0, 6) . 's'
 function func_test3($mu_, $file_name_blog_)
 {
     $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
+
+    $yyyy = date('Y');
+    $ymd = date('Ymd', strtotime('+9 hours'));
+    for ($i = 3; $i < 10; $i++) {
+        $url = "https://elevensports.jp/schedule/farm/${yyyy}/" . str_pad($i, 2, '0', STR_PAD_LEFT) . "?4nocache${ymd}";
+        $res = $mu_->get_contents($url, null, true);
         
-    $url = 'https://elevensports.jp/schedule/farm/2019/03';
+        $rc = preg_match_all('/<tr>(.+?)<\/tr>/s', $res, $matches);
     
-    $res = $mu_->get_contents($url);
-    
-    //error_log($res);
-    $rc = preg_match_all('/<tr>(.+?)<\/tr>/s', $res, $matches);
-    
-    foreach ($matches[1] as $item) {
-        if (mb_strpos($item, '広島') === false) {
-            continue;
+        foreach ($matches[1] as $item) {
+            if (mb_strpos($item, '広島') === false) {
+                continue;
+            }
+            // error_log($item);
+            $rc = preg_match('/<.+?>(\d+)\/(\d+).+?>.*?<.+?>(.+?)<.+?>.*?<.+?>(.+?)<.+?>.*?<.+?>(.+?)<.+?>.*?<.+?>(.+?)<.+?>.*?<.+?>(.+?)<.+?>/s', $item, $match);
+            error_log(print_r($match, true));
         }
-        // error_log($item);
-        $rc = preg_match('/<.+?>(\d+)\/(\d+).+?>.*?<.+?>(.+?)<.+?>.*?<.+?>(.+?)<.+?>.*?<.+?>(.+?)<.+?>.*?<.+?>(.+?)<.+?>.*?<.+?>(.+?)<.+?>/s', $item, $match);
-        error_log(print_r($match, true));
     }
 }
