923cfa0f285a592236663d0bf7a1c6630d587dd5
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-03-26 11:42:20 +0900

    Create 20190326.php

---
+++ www/20190326.php
@@ -0,0 +1,62 @@
+<?php
+
+$html = <<< __HEREDOC__
+<html>
+<head>
+<title>__TITLE__</title>
+</head>
+<body>
+<table>
+__BODY__
+</table>
+</body></html>
+__HEREDOC__;
+
+
+function get_contents($url_, $options_ = null)
+{
+    $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
+    error_log($log_prefix . 'URL : ' . $url_);
+    error_log($log_prefix . 'options : ' . print_r($options_, true));
+    $options = [
+        CURLOPT_URL => $url_,
+        CURLOPT_USERAGENT => getenv('USER_AGENT'),
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_MAXREDIRS => 3,
+        CURLOPT_PATH_AS_IS => true,
+        CURLOPT_TCP_FASTOPEN => true,
+        CURLOPT_SSL_VERIFYPEER => false,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_2TLS,
+        CURLOPT_HTTPHEADER => [
+            'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Language: ja,en-US;q=0.7,en;q=0.3',
+            'Cache-Control: no-cache',
+            'Connection: keep-alive',
+            'DNT: 1',
+            'Upgrade-Insecure-Requests: 1',
+        ],
+    ];
+    
+    $ch = curl_init();
+    foreach ($options as $key => $value) {
+        $rc = curl_setopt($ch, $key, $value);
+        if ($rc == false) {
+            error_log($log_prefix . "curl_setopt : ${key} ${value}");
+        }
+    }
+    if (is_null($options_) === false) {
+        foreach ($options_ as $key => $value) {
+            $rc = curl_setopt($ch, $key, $value);
+            if ($rc == false) {
+                error_log($log_prefix . "curl_setopt : ${key} ${value}");
+            }
+        }
+    }
+    $res = curl_exec($ch);
+    $http_code = (string)curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+    
+    return $res;
+}
