2da11187ed4baff500cdbdd43439b8485a1e7a36
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-03-26 10:20:25 +0900

    Update 20190320.php

--- www/20190320.php
+++ www/20190320.php
@@ -19,7 +19,10 @@ $res = get_contents($url, [CURLOPT_HEADER => true, CURLOPT_NOBODY => true]);
 
 // error_log($res);
 $rc = preg_match('/Last-Modified.+/', $res, $match);
-error_log(date('Ymd', strtotime(trim(explode(':', $match[0])[1]))));
+// error_log(date('Ymd', strtotime(trim(explode(':', $match[0])[1]))));
+$tmp = trim(explode(':', $match[0])[1]);
+error_log($tmp);
+error_log(strtotime($tmp));
 
 $body = '<tr><td>' . $url . '</td><td>' . $match[0] . '</td></tr>' . "\n";
 
