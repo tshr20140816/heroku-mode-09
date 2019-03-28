374e6760c004fea710846d0d0f12df82a8dbebfd
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-03-28 11:34:11 +0900

    Update 20190328.php

--- www/20190328.php
+++ www/20190328.php
@@ -47,7 +47,7 @@ function func_test20190328($mu_, $file_name_blog_)
     
     $blog_id = json_decode($res)->primary_blog;
     
-    $url = "https://public-api.wordpress.com/wp/v2/sites/${blog_id}/posts?search=rental";
+    $url = "https://public-api.wordpress.com/wp/v2/sites/${blog_id}/posts?number=1&search=rental";
     $options = [CURLOPT_HTTPHEADER => ['Authorization: Bearer ' . $access_token,],];
     $res = $mu_->get_contents($url, $options);
     error_log(print_r(json_decode($res), true));
