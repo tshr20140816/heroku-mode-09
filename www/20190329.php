4fc7b6ea6e5d7f4a07bdcf93de462d9c47596117
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-03-29 15:26:11 +0900

    Update 20190329.php

--- www/20190329.php
+++ www/20190329.php
@@ -14,57 +14,14 @@ function func2019329($mu_)
 {
     $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
     
-    $user_4shared = $mu_->get_env('4SHARED_USER', true);
-    $password_4shared = $mu_->get_env('4SHARED_PASSWORD', true);
+    $user_hidrive = $this->get_env('HIDRIVE_USER', true);
+    $password_hidrive = $this->get_env('HIDRIVE_PASSWORD', true);
     
-    $url = 'https://webdav.4shared.com/';
-    
+    $url = "https://webdav.hidrive.strato.com/users/${user_hidrive}/";
     $options = [
         CURLOPT_HTTPAUTH => CURLAUTH_BASIC,
-        CURLOPT_USERPWD => "${user_4shared}:${password_4shared}",
-        CURLOPT_HEADER => true,
-        CURLOPT_CUSTOMREQUEST => 'PROPFIND',        
-    ];
-    
-    $res = $mu_->get_contents($url, $options);
-    
-    error_log($res);
-    
-    $rc = preg_match_all('/<D\:getcontentlength>(.+?)<\/D\:getcontentlength>/', $res, $matches);
-    //$rc = preg_match_all('/getcontentlength>(.+?)</s', $res, $matches);
-    error_log(print_r($matches, true));
-    
-    $size = 0;
-    foreach ($matches[1] as $item) {
-        $size += $item;
-    }
-    error_log($size);
-    
-    /*
-    $ftp_link_id = ftp_connect('ftp.4shared.com');
-    $rc = ftp_login($ftp_link_id, $user_4shared, $password_4shared);
-    error_log('ftp_login : ' . $rc);
-    
-    $rc = ftp_close($ftp_link_id);
-    error_log('ftp_close : ' . $rc);
-    */
-    
-    return;
-    
-    $file_name_ = '/tmp/dummy1.txt';
-    file_put_contents($file_name_, 'DUMMY1');
-    
-    $file_size = filesize($file_name_);
-    $fh = fopen($file_name_, 'r');
-    
-    $url = 'https://webdav.4shared.com/' . pathinfo($file_name_)['basename'];
-    
-    $options = [
-        CURLOPT_HTTPAUTH => CURLAUTH_BASIC,
-        CURLOPT_USERPWD => "${user_4shared}:${password_4shared}",
-        CURLOPT_PUT => true,
-        CURLOPT_INFILE => $fh,
-        CURLOPT_INFILESIZE => $file_size,
+        CURLOPT_USERPWD => "${user_hidrive}:${password_hidrive}",
+        CURLOPT_CUSTOMREQUEST => 'PROPFIND',
         CURLOPT_HEADER => true,
     ];
     
@@ -72,18 +29,4 @@ function func2019329($mu_)
     
     error_log($res);
     
-    fclose($fh);
-    unlink($file_name_);
-    
-    $url = 'https://webdav.4shared.com/' . pathinfo($file_name_)['basename'];
-    
-    $options = [
-        CURLOPT_HTTPAUTH => CURLAUTH_BASIC,
-        CURLOPT_USERPWD => "${user_4shared}:${password_4shared}",
-        CURLOPT_HEADER => true,
-    ];
-    
-    $res = $mu_->get_contents($url, $options);
-    
-    error_log($res);
 }
