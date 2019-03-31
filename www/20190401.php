0461a1f361b7acfacddd3e1811a67425bd31b8ea
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-04-01 08:58:42 +0900

    Update 20190401.php

--- www/20190401.php
+++ www/20190401.php
@@ -13,5 +13,22 @@ function func_20190401($mu_, $file_name_blog_)
 {
     $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
     
+    $user_zoho = getenv('ZOHO_USER');
+    $password_zoho = getenv('ZOHO_PASSWORD');
+    $url = 'https://accounts.zoho.com/apiauthtoken/nb/create';
     
+    $post_data = ['SCOPE' => 'docsapi',
+                  'EMAIL_ID' => $user_zoho,
+                  'PASSWORD' => $password_zoho,
+                  'DISPLAY_NAME' => 'ZOHODOCS',
+                 ];
+    
+    $res = $mu_->get_contents(
+        $url,
+        [CURLOPT_POST => true,
+        CURLOPT_POSTFIELDS => http_build_query($post_data),
+        ]
+    );
+    
+    error_log($res);
 }
