d4ad79d5ee1bd891343a298d78a8929ed2d99eec
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-04-02 08:51:32 +0900

    Update 20190401.php

--- www/20190401.php
+++ www/20190401.php
@@ -4,7 +4,7 @@ $pid = getmypid();
 $requesturi = $_SERVER['REQUEST_URI'];
 $time_start = microtime(true);
 error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
-$rc = apcu_clear_cache();
+
 $mu = new MyUtils();
 
 func_20190401($mu, '/tmp/dummy');
