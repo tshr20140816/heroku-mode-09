4e8b0189e49a53f9573eea8439767e2635d8bdc2
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-04-01 09:00:25 +0900

    Update 20190401.php

--- www/20190401.php
+++ www/20190401.php
@@ -17,7 +17,7 @@ function func_20190401($mu_, $file_name_blog_)
     $password_zoho = getenv('ZOHO_PASSWORD');
     $url = 'https://accounts.zoho.com/apiauthtoken/nb/create';
     
-    $post_data = ['SCOPE' => 'docsapi',
+    $post_data = ['SCOPE' => 'ZohoPC/docsapi',
                   'EMAIL_ID' => $user_zoho,
                   'PASSWORD' => $password_zoho,
                   'DISPLAY_NAME' => 'ZOHODOCS',
