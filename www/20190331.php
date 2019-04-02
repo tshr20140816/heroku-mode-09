8e9a3262a33b7e509e3f9f78e02a50e583e611ae
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-04-02 16:00:29 +0900

    Update 20190331.php

--- www/20190331.php
+++ www/20190331.php
@@ -13,80 +13,8 @@ function func_20190331($mu_, $file_name_blog_)
 {
     $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
     
-    file_put_contents('/tmp/dummy1.txt', 'DUMMY');
-    
-    $user_cloudapp = $mu_->get_env('CLOUDAPP_USER', true);
-    $user_cloudpassword = $mu_->get_env('CLOUDAPP_PASSWORD', true);
-    
-    $url = 'http://my.cl.ly/items?per_page=1000&page=1';
-        
-    $res = $mu_->get_contents(
-        $url,
-        [CURLOPT_HTTPAUTH => CURLAUTH_DIGEST,
-         CURLOPT_USERPWD => "${user_cloudapp}:${user_cloudpassword}",
-         CURLOPT_HTTPHEADER => ['Accept: application/json',],
-        ]
-    );
-    error_log(print_r(json_decode($res), true));
-    
-    $url = json_decode($res)[0]->href;
-        
-    $res = $mu_->get_contents(
-        $url,
-        [CURLOPT_HTTPAUTH => CURLAUTH_DIGEST,
-         CURLOPT_USERPWD => "${user_cloudapp}:${user_cloudpassword}",
-         CURLOPT_HTTPHEADER => ['Accept: application/json',],
-         CURLOPT_CUSTOMREQUEST => 'DELETE',
-        ]
-    );
-    error_log(print_r(json_decode($res), true));
-    
-    return;
-    
-    $url = 'http://my.cl.ly/account';
-    $url = 'http://my.cl.ly/account/stats';
-    $url = 'http://my.cl.ly/items/new';
-        
-    $res = $mu_->get_contents(
-        $url,
-        [CURLOPT_HTTPAUTH => CURLAUTH_DIGEST,
-         CURLOPT_USERPWD => "${user_cloudapp}:${user_cloudpassword}",
-         CURLOPT_HTTPHEADER => ['Accept: application/json',],
-        ]
-    );
-    error_log(print_r(json_decode($res), true));
-    $json = json_decode($res);
-        
-    $url = $json->url;
-    $post_data = ['AWSAccessKeyId' => $json->params->AWSAccessKeyId,
-                  'key' => $json->params->key,
-                  'policy' => $json->params->policy,
-                  'signature' => $json->params->signature,
-                  'success_action_redirect' => $json->params->success_action_redirect,
-                  'acl' => $json->params->acl,
-                  'file' => new CURLFile('/tmp/dummy1.txt', 'text/plain', 'dummy1.txt'),
-                 ];
-    
-    $res = $mu_->get_contents(
-        $url,
-        [CURLOPT_POST => true,
-         CURLOPT_POSTFIELDS => $post_data,
-         CURLOPT_HEADER => true,
-         CURLOPT_FOLLOWLOCATION => false,
-        ]);
+    $url = 'https://amefootlive.jp/live';
+    $res = $mu_->get_contents($url);
     
     error_log($res);
-    
-    $rc = preg_match('/Location: (.+)/i', $res, $match);
-    error_log(print_r($match, true));
-    
-    $url = trim($match[1]);
-    $res = $mu_->get_contents(
-        $url,
-        [CURLOPT_HTTPAUTH => CURLAUTH_DIGEST,
-         CURLOPT_USERPWD => "${user_cloudapp}:${user_cloudpassword}",
-         CURLOPT_HTTPHEADER => ['Accept: application/json',],
-        ]
-    );
-    error_log(print_r(json_decode($res), true));
 }
