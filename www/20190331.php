262e00cf89ed43902eceb11b60c34f68456264d4
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-04-02 12:27:02 +0900

    Update 20190331.php

--- www/20190331.php
+++ www/20190331.php
@@ -13,7 +13,7 @@ function func_20190331($mu_, $file_name_blog_)
 {
     $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
     
-    file_put_contents('/tmp/dummy.txt', 'DUMMY');
+    file_put_contents('/tmp/dummy1.txt', 'DUMMY');
     
     $user_cloudapp = getenv('CLOUDAPP_USER');
     $user_cloudpassword = getenv('CLOUDAPP_PASSWORD');
@@ -39,7 +39,7 @@ function func_20190331($mu_, $file_name_blog_)
                   'signature' => $json->params->signature,
                   'success_action_redirect' => $json->params->success_action_redirect,
                   'acl' => $json->params->acl,
-                  'file' => new CURLFile('/tmp/dummy.txt', 'text/plain', 'dummy.txt'),
+                  'file' => new CURLFile('/tmp/dummy1.txt', 'text/plain', 'dummy1.txt'),
                  ];
     
     $res = $mu_->get_contents(
@@ -48,7 +48,7 @@ function func_20190331($mu_, $file_name_blog_)
          CURLOPT_POSTFIELDS => $post_data,
         ]);
     
-    error_log(print_r(json_decode($res), true));
+    error_log($res);
         
-    unlink('/tmp/dummy.txt');
+    unlink('/tmp/dummy1.txt');
 }
