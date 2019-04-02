fe4b4df3091dff1768b54c4d3519987754105eef
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-04-02 10:21:48 +0900

    Update 20190401.php

--- www/20190401.php
+++ www/20190401.php
@@ -59,5 +59,6 @@ function func_20190401($mu_, $file_name_blog_)
     
     $url = "https://apidocs.zoho.com/files/v1/files?authtoken=${authtoken_zoho}&scope=docsapi";
     $res = $mu_->get_contents($url);
-    error_log($res);
+    //error_log($res);
+    error_log(print_r(json_decode($res), true));
 }
