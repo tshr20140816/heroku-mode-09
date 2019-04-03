60fd62fbe2e4378021c6e6472208c38a7d3d289b
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-04-03 10:51:21 +0900

    Update 20190403.php

--- www/20190403.php
+++ www/20190403.php
@@ -10,4 +10,24 @@ func_20190403($mu, '/tmp/dummy');
 
 function func_20190403($mu_, $file_name_blog_)
 {
+    for (;;) {
+        $page++;
+        $url = 'http://my.cl.ly/items?per_page=100&page=' . $page;
+        $options = [
+            CURLOPT_HTTPAUTH => CURLAUTH_DIGEST,
+            CURLOPT_USERPWD => "${user_cloudapp}:${password_cloudapp}",
+            CURLOPT_HTTPHEADER => ['Accept: application/json',],
+        ];
+        $res = $this->get_contents($url, $options);
+        $json = json_decode($res);
+        if (count($json) === 0) {
+            break;
+        }
+        foreach ($json as $item) {
+            if ($item->file_name == $base_name) {
+                $url_target = $item->href;
+                break 2;
+            }
+        }
+    }
 }
