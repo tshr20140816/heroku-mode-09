536a58f41e282cb72be3058c052c427b11ffb240
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-04-05 11:01:30 +0900

    Update 20190405.php

--- www/20190405.php
+++ www/20190405.php
@@ -9,21 +9,11 @@ error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
 
 $mu = new MyUtils();
 
-$sub_address = $mu->get_env('SUB_ADDRESS');
-for ($i = 11; $i > -1; $i--) {
-    $url = 'https://feed43.com/' . $sub_address . ($i * 5 + 11) . '-' . ($i * 5 + 15) . '.xml';
-    $urls[$url] = null;
-}
+$url = getenv('URL_YOUTUBE');
+$res = $mu->get_contents($url);
 
-$multi_options = [
-    CURLMOPT_PIPELINING => 3,
-    CURLMOPT_MAX_HOST_CONNECTIONS => 1,
-];
-
-// multi
-$list_contents = $mu->get_contents_multi($urls, null, $multi_options);
-
-// error_log(print_r($list_contents, true));
+$tmp = explode('window["ytInitialData"] = ', $res);
+error_log(strlen($tmp[0]));
 
 error_log("${pid} FINISH " . substr((microtime(true) - $time_start), 0, 6) . 's');
 
