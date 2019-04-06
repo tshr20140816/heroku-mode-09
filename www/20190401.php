2847047923b35820bc6b9f18b013b3a74e46e6a2
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-04-06 09:38:23 +0900

    Update 20190401.php

--- www/20190401.php
+++ www/20190401.php
@@ -7,7 +7,7 @@ error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
 
 $mu = new MyUtils();
 
-func_20190401($mu, '/tmp/dummy');
+func_20190401_b($mu, '/tmp/dummy');
 
 function func_20190401($mu_, $file_name_blog_)
 {
@@ -22,6 +22,7 @@ function func_20190401_b($mu_, $file_name_blog_)
 {
     $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
     
+    /*
     $user_zoho = getenv('ZOHO_USER');
     $password_zoho = getenv('ZOHO_PASSWORD');
     $url = 'https://accounts.zoho.com/apiauthtoken/nb/create';
@@ -44,10 +45,10 @@ function func_20190401_b($mu_, $file_name_blog_)
     error_log(print_r($match, true));
     
     return;
+    */
     
-    $authtoken_zoho = getenv('ZOHO_AUTHTOKEN');
+    $authtoken_zoho = $mu_->get_env('ZOHO_AUTHTOKEN', true);
     
-    /*
     $url = 'https://apidocs.zoho.com/files/v1/upload?authtoken=' . $authtoken_zoho . '&scope=docsapi';
     
     file_put_contents('/tmp/dummy.txt', 'DUMMY');
@@ -64,8 +65,7 @@ function func_20190401_b($mu_, $file_name_blog_)
         ]
     );
     error_log($res);
-    */
-    
+        
     $url = "https://apidocs.zoho.com/files/v1/files?authtoken=${authtoken_zoho}&scope=docsapi";
     $res = $mu_->get_contents($url);
     //error_log($res);
