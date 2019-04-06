5a438ea29792adc6156395f131e5d194b30cb76a
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-04-06 14:41:47 +0900

    Update 20190331.php

--- www/20190331.php
+++ www/20190331.php
@@ -40,13 +40,7 @@ __HEREDOC__;
     $res = $mu_->get_contents($url, $options);
 
     error_log($log_prefix . $res);
-    
-    $doc = new DOMDocument();
-    $doc->loadXML($res);
-    $doc->encoding = 'UTF-8';
-    $doc->formatOutput = true;
-    error_log($log_prefix . $doc->saveXML());
-    
+        
     $url = 'https://m.youtube.com/watch?v=TwzRhp1Y4eU';
     $res = $mu_->get_contents($url, [CURLOPT_HEADER => true, CURLOPT_USERAGENT => 'Mozilla/5.0 (Linux; Android 9; Pixel 3 Build/PQ1A.181105.013) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Mobile Safari/537.36']);
     
