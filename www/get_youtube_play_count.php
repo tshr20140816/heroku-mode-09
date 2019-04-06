5ce419b94fb5e31b6a4c238bd2f24f8939c49a24
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-04-06 15:58:40 +0900

    Update and rename 20190406.php to get_youtube_play_count.php

--- www/get_youtube_play_count.php
+++ www/get_youtube_play_count.php
@@ -1,16 +1,18 @@
 <?php
 include(dirname(__FILE__) . '/../classes/MyUtils.php');
+
 $pid = getmypid();
 $requesturi = $_SERVER['REQUEST_URI'];
 $time_start = microtime(true);
 error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
+
 $mu = new MyUtils();
 
-func_20190406($mu);
+get_youtube_play_count($mu);
 
 error_log("${pid} FINISH " . substr((microtime(true) - $time_start), 0, 6) . 's');
 
-function func_20190406($mu_)
+function get_youtube_play_count($mu_)
 {
     $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
 
