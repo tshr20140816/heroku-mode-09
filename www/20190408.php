b2696bacd88bf5f7a14c115b0089f8c4d2de7bf3
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-04-08 15:13:35 +0900

    Update 20190408.php

--- www/20190408.php
+++ www/20190408.php
@@ -19,12 +19,11 @@ function func_20190408($mu_, $file_name_blog_)
     $rc = preg_match('/<div class="article-body-inner">(.+?)<\/div>/s', $res, $match);
     // error_log(print_r($match, true));
     error_log(trim($match[1]));
-    
-    return;
+    $base_record = trim($match[1]);
     
     $name = '野間 峻祥';
     $title = 'NOMA Takayoshi';
-    $timestamp = mktime(0, 0, 0, 3, 29, 2019);
+    $timestamp = mktime(0, 0, 0, 3, 30, 2019);
     $ymd = date('Ymd', $timestamp);
     $url = 'https://baseball.yahoo.co.jp/npb/schedule/?date=' . $ymd;
     
@@ -62,7 +61,7 @@ function func_20190408($mu_, $file_name_blog_)
                     $tmp = preg_replace('/<.+?>/s', ' ', $tmp);
                     $tmp = str_replace($name, '', $tmp);
                     $tmp = date('Y/m/d', $timestamp) . ' ' . trim(preg_replace('/ +/', ' ', $tmp));
-                    error_log($tmp);
+                    error_log($tmp . "\n" . $base_record);
                     // $mu_->post_blog_wordpress($title, $tmp);
                     break 2;
                 }
