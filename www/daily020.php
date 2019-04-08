fe504cc3691ed5e9c4dcc514df32b691068f3925
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-04-08 09:46:26 +0900

    Update daily020.php

--- www/daily020.php
+++ www/daily020.php
@@ -732,13 +732,10 @@ function check_zoho_usage($mu_, $file_name_blog_)
     $res = $mu_->get_contents($url);
 
     $urls = [];
-    $options = [CURLOPT_HEADER => true,
-                CURLOPT_NOBODY => true,
-               ];
     foreach (json_decode($res)->FILES as $item) {
         $docid = $item->DOCID;
         $url = "https://apidocs.zoho.com/files/v1/content/${docid}?authtoken=${authtoken_zoho}&scope=docsapi";
-        $urls[$url] = $options;
+        $urls[$url] = null;
     }
 
     $multi_options = [
@@ -746,12 +743,9 @@ function check_zoho_usage($mu_, $file_name_blog_)
         CURLMOPT_MAX_HOST_CONNECTIONS => 10,
     ];
     $list_contents = $mu_->get_contents_multi($urls, null, $multi_options);
-    error_log($log_prefix . 'memory_get_usage : ' . number_format(memory_get_usage()) . 'byte');
-
     $size = 0;
     foreach ($list_contents as $res) {
-        $rc = preg_match('/Content-Length: (\d+)/', $res, $match);
-        $size += (int)$match[1];
+        $size += strlen($res);
     }
 
     $percentage = substr($size / (5 * 1024 * 1024 * 1024) * 100, 0, 5);
