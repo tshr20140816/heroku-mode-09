3bb7a2af1392b747bf35ccbf200994ab5b50ba58
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-04-08 09:06:53 +0900

    Update 20190408.php

--- www/20190408.php
+++ www/20190408.php
@@ -34,6 +34,7 @@ function check_zoho_usage20190408($mu_, $file_name_blog_)
     $list_contents = $mu_->get_contents_multi($urls, null, $multi_options);
     error_log($log_prefix . 'memory_get_usage : ' . number_format(memory_get_usage()) . 'byte');
 
+    error_log($log_prefix . print_r($list_contents, true));
     $size = 0;
     foreach ($list_contents as $res) {
         $rc = preg_match('/Content-Length: (\d+)/', $res, $match);
