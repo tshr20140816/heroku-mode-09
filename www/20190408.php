adfac619b44bc6165d0729253a57d2c208897f9e
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-04-08 12:39:59 +0900

    Create 20190408.php

---
+++ www/20190408.php
@@ -0,0 +1,21 @@
+<?php
+include(dirname(__FILE__) . '/../classes/MyUtils.php');
+$pid = getmypid();
+$requesturi = $_SERVER['REQUEST_URI'];
+$time_start = microtime(true);
+error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
+$mu = new MyUtils();
+
+func_20190408($mu, '/tmp/dummy');
+
+function func_20190408($mu_, $file_name_blog_)
+{
+    $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
+    
+    $url = 'https://baseball.yahoo.co.jp/npb/schedule/?date=20190406';
+    
+    $options = [CURLOPT_HEADER = true,];
+    $res = $mu_->get_contents($url, $options);
+
+    error_log($log_prefix . $res);
+}
