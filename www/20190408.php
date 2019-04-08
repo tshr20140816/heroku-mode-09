df218846392043c346a5f12e64fc7e0683682865
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-04-08 09:16:40 +0900

    Update 20190408.php

--- www/20190408.php
+++ www/20190408.php
@@ -21,6 +21,7 @@ function check_zoho_usage20190408($mu_, $file_name_blog_)
     $options = [CURLOPT_HEADER => true,
                 CURLOPT_NOBODY => true,
                ];
+    $options = [];
     foreach (json_decode($res)->FILES as $item) {
         $docid = $item->DOCID;
         $url = "https://apidocs.zoho.com/files/v1/content/${docid}?authtoken=${authtoken_zoho}&scope=docsapi";
@@ -37,8 +38,10 @@ function check_zoho_usage20190408($mu_, $file_name_blog_)
     error_log($log_prefix . print_r($list_contents, true));
     $size = 0;
     foreach ($list_contents as $res) {
-        $rc = preg_match('/Content-Length: (\d+)/', $res, $match);
-        $size += (int)$match[1];
+        // $rc = preg_match('/Content-Length: (\d+)/', $res, $match);
+        // $size += (int)$match[1];
+        error_log($log_prefix . strlen($res));
+        $size += strlen($res);
     }
 
     $percentage = substr($size / (5 * 1024 * 1024 * 1024) * 100, 0, 5);
