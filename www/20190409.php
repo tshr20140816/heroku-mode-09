c56b87bb63a2cbb95580cb3cf4a2b67192b40eb5
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-04-09 11:50:45 +0900

    Update 20190409.php

--- www/20190409.php
+++ www/20190409.php
@@ -55,6 +55,8 @@ function func_20190409($mu_)
             ];
     $url = 'https://quickchart.io/chart?width=300&height=100&c=' . json_encode($data);
     $url = 'https://quickchart.io/chart?c=' . json_encode($data);
-    header('Content-Type: image/png');
-    echo $mu_->get_contents($url);
+    // header('Content-Type: image/png');
+    // echo $mu_->get_contents($url);
+    $res = $mu_->get_contents($url);
+    error_log(base64_encode($res));
 }
