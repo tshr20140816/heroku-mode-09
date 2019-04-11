3f27a0e61a91ca5b79452a0c383554d0eda329e3
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-04-11 10:10:46 +0900

    Update get_results_noma.php

--- www/get_results_noma.php
+++ www/get_results_noma.php
@@ -26,6 +26,7 @@ function get_results_noma($mu_)
     $name = '野間 峻祥';
     $title = 'NOMA Takayoshi';
     $timestamp = strtotime('-1 day');
+    $timestamp = strtotime('-2 day');
 
     if (strpos($base_record, date('Y/m/d', $timestamp)) >= 0) {
         return;
