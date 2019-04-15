0a856c443fed2460519bcb4c35a7bb1ae450e13a
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-04-15 12:42:13 +0900

    Update get_results_noma.php

--- www/get_results_noma.php
+++ www/get_results_noma.php
@@ -133,7 +133,7 @@ function get_results_noma($mu_)
     $tmp = preg_split('/^\r\n/m', $res, 2);
 
     $rc = preg_match('/compression-count: (.+)/i', $tmp[0], $match);
-    error_log($log_prefix . 'Compression count : ' . $match[1]);
+    error_log($log_prefix . 'Compression count : ' . $match[1]); // Limits 500/month
     $json = json_decode($tmp[1]);
     error_log($log_prefix . print_r($json, true));
 
