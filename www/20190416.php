b3f751b20055ae884451382a2afd7b19c7d62c9b
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-04-16 10:56:08 +0900

    Update 20190416.php

--- www/20190416.php
+++ www/20190416.php
@@ -33,7 +33,7 @@ function func_20190416($mu_)
                                              ['data' => $data3,
                                               'fill' => false,
                                               'pointStyle' => 'line',
-                                              'borderDash' => [30, 30],
+                                              'borderColor' => 'rgba(255, 0, 0, 1)',
                                              ],
                                             ],
                              ],
