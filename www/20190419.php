2b5bf5835a05cbabbcebd754b17e802969e36acc
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-04-19 08:46:46 +0900

    Update 20190419.php

--- www/20190419.php
+++ www/20190419.php
@@ -9,8 +9,19 @@ $mu = new MyUtils();
 func_20190419($mu, '/tmp/dummy.txt');
 
 error_log("${pid} FINISH " . substr((microtime(true) - $time_start), 0, 6) . 's');
+
 function func_20190419($mu_, $file_name_blog_)
 {
     $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
 
+    $url = getenv('TEST_URL_01');
+    $host_name = parse_url($url, PHP_URL_HOST);
+    $res = $mu_->get_contents($url, null, true);
+    
+    $rc = preg_match_all('/<a class="title" href="(.+?)">/s', $res, $matches);
+    
+    foreach ($matches[1] as $item) {
+        $url = 'https://' . $host_name . $item;
+        $res = $mu_->get_contents($url, null, true);
+    }
 }
