2f8f98645cc9216236cc1538fba92f4825d83b3b
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-04-19 08:53:50 +0900

    Update daily030.php

--- www/daily030.php
+++ www/daily030.php
@@ -1,27 +1,31 @@
 <?php
+
 include(dirname(__FILE__) . '/../classes/MyUtils.php');
+
 $pid = getmypid();
 $requesturi = $_SERVER['REQUEST_URI'];
 $time_start = microtime(true);
 error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
+
 $mu = new MyUtils();
 
-func_20190419($mu, '/tmp/dummy.txt');
+$url = getenv('TEST_URL_01');
+$host_name = parse_url($url, PHP_URL_HOST);
+$res = $mu->get_contents($url, null, true);
 
-error_log("${pid} FINISH " . substr((microtime(true) - $time_start), 0, 6) . 's');
+$rc = preg_match_all('/<a class="title" href="(.+?)">/s', $res, $matches);
 
-function func_20190419($mu_, $file_name_blog_)
-{
-    $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
-
-    $url = getenv('TEST_URL_01');
-    $host_name = parse_url($url, PHP_URL_HOST);
-    $res = $mu_->get_contents($url, null, true);
-    
-    $rc = preg_match_all('/<a class="title" href="(.+?)">/s', $res, $matches);
-    
-    foreach ($matches[1] as $item) {
-        $url = 'https://' . $host_name . $item;
-        $res = $mu_->get_contents($url, null, true);
+$urls = [];
+foreach ($matches[1] as $item) {
+    $url = 'https://' . $host_name . $item;
+    $res = $mu->get_contents($url, null, true);
+    $rc = substr_count($res, '<item>');
+    error_log("${pid} ${rc} ${url}");
+    if ($rc == 0) {
+        $urls[] = $url;
     }
 }
+
+error_log(print_r($urls, true));
+
+error_log("${pid} FINISH " . substr((microtime(true) - $time_start), 0, 6) . 's');
