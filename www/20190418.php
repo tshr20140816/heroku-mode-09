8b231816fef889e221786ee3cddee950e4af21f5
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-04-18 15:29:12 +0900

    Update 20190418.php

--- www/20190418.php
+++ www/20190418.php
@@ -25,13 +25,14 @@ function func_20190418($mu_, $file_name_blog_)
     $urls = [];
     foreach ($matches[1] as $item) {
         $url = 'https://' . $host_name . $item;
-        $urls[$url] = null;
-        // $res = $mu_->get_contents($url);
+        // $urls[$url] = null;
+        $res = $mu_->get_contents($url);
         // error_log($res);
-        // $rc = substr_count($res, '<item>');
-        // error_log($rc . ' ' . $url);
+        $rc = substr_count($res, '<item>');
+        error_log($rc . ' ' . $url);
     }
     
+    /*
     $multi_options = [
         CURLMOPT_PIPELINING => 3,
         CURLMOPT_MAX_HOST_CONNECTIONS => 1,
@@ -50,5 +51,6 @@ function func_20190418($mu_, $file_name_blog_)
             break;
         }
     }
+    */
     
 }
