8f8f49ee75c341b1b7a4fc7cae85c6979fdcb816
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-04-23 08:56:48 +0900

    Update 20190421.php

--- www/20190421.php
+++ www/20190421.php
@@ -10,4 +10,8 @@ $res = $mu->get_contents($url);
 
 $tmp = explode('<h2>', $res, 3);
 
-error_log($tmp[1]);
+// error_log($tmp[1]);
+
+$rc = preg_match_all('/<li>(.+?)<\/li>/s', explode('<h2>', $res, 3)[1], $matches);
+
+error_log(print_r($matches, true));
