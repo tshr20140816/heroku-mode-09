0286b3a505e525dc3b76c444613c4476b0402c5c
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-04-23 14:52:36 +0900

    Update 20190423.php

--- www/20190423.php
+++ www/20190423.php
@@ -14,7 +14,12 @@ function func_20190423($mu_)
     
     // $mu_->post_blog_hatena('TEST', '<div class="0123456789abcdefghijklmnopqrstuvwxyz+/=">TEST</div>');
     // $title = 'toddledo quota';
+    /*
     $title = 'upeemfeprvpub';
     $description = '<div class="upeemfeprvpub">11163 33000 32293 31560 31560 30260 29577 28858 28858 28858 26680 26015 25392 24742 24121 23431 23431 22006 21356 20649 19927 19303 18661</div>';
     $mu_->post_blog_hatena($title, $description);
+    */
+    
+    $hatena_blog_id = $mu_->get_env('HATENA_BLOG_ID', true);
+    error_log($hatena_blog_id);
 }
