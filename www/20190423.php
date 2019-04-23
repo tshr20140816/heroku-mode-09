6d15522515652a564e5855b026255e34b1c9cc5d
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-04-23 13:24:49 +0900

    Create 20190423.php

---
+++ www/20190423.php
@@ -0,0 +1,14 @@
+<?php
+include(dirname(__FILE__) . '/../classes/MyUtils.php');
+$pid = getmypid();
+$requesturi = $_SERVER['REQUEST_URI'];
+$time_start = microtime(true);
+error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
+$mu = new MyUtils();
+func_20190423($mu);
+error_log("${pid} FINISH " . substr((microtime(true) - $time_start), 0, 6) . 's');
+
+function func_20190423($mu_)
+{
+    $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
+}
