4af9a45b48bf01621c6cdd0d645435663f5e7528
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-04-25 16:47:15 +0900

    Update daily020.php

--- www/daily020.php
+++ www/daily020.php
@@ -311,11 +311,15 @@ error_log("${pid} FINISH " . substr(($time_finish - $time_start), 0, 6) . 's ' .
 
 exit();
 
-function get_quota($mu_, $file_name_blog_)
+function get_quota($mu_, $file_name_blog_, $target_ = 'TOODLEDO')
 {
     $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
 
-    $api_key = base64_decode(getenv('HEROKU_API_KEY'));
+    if (getenv('HEROKU_API_KEY_' . $target_) == '') {
+        $api_key = base64_decode(getenv('HEROKU_API_KEY'));
+    } else {
+        $api_key = base64_decode(getenv('HEROKU_API_KEY_' . $target_));
+    }
     $url = 'https://api.heroku.com/account';
 
     $res = $mu_->get_contents(
@@ -349,24 +353,30 @@ function get_quota($mu_, $file_name_blog_)
 
     $quota = $dyno_quota - $dyno_used;
     
+    $keyword = strtolower($target_);
+    for ($i = 0; $i < strlen($keyword); $i++) {
+        $keyword[$i] = chr(ord($keyword[$i]) + 1);
+    }
+    $keyword .= 'rvpub';
+
     $description = '';
     if ((int)date('j', strtotime('+9hours')) != 1) {
         $hatena_blog_id = $mu_->get_env('HATENA_BLOG_ID', true);
-        $url = 'https://' . $hatena_blog_id . '/search?q=upeemfeprvpub';
+        $url = 'https://' . $hatena_blog_id . '/search?q=' . $keyword;
         $res = $mu_->get_contents($url);
         
         $rc = preg_match('/<a class="entry-title-link" href="(.+?)"/', $res, $match);
         $res = $mu_->get_contents($match[1]);
         
-        $rc = preg_match('/<div class="upeemfeprvpub">(.+?)</', $res, $match);
+        $rc = preg_match('/<div class="' . $keyword . '">(.+?)</', $res, $match);
         $description = $match[1];
     }
-    $description = '<div class="upeemfeprvpub">' . trim($description . ' ' . (int)($quota / 60)) . '</div>';
-    $mu_->post_blog_hatena('upeemfeprvpub', $description);
+    $description = '<div class="' . $keyword . '">' . trim($description . ' ' . (int)($quota / 60)) . '</div>';
+    $mu_->post_blog_hatena($keyword, $description);
     
     $quota = floor($quota / 86400) . 'd ' . ($quota / 3600 % 24) . 'h ' . ($quota / 60 % 60) . 'm';
 
-    file_put_contents($file_name_blog_, "\nQuota : ${quota}\n", FILE_APPEND);
+    file_put_contents($file_name_blog_, "\nQuota " . strtolower($target_) . " : ${quota}\n", FILE_APPEND);
 }
 
 function check_waon_balance($mu_, $file_name_blog_)
