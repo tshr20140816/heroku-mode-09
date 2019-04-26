686ac2290318050465df813da8db0f08091e9a44
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-04-26 10:03:14 +0900

    Update rss.php

--- www/rss.php
+++ www/rss.php
@@ -29,8 +29,6 @@ if ($rss_data != '') {
     echo gzdecode(base64_decode($rss_data));
 }
 
-$time_finish = microtime(true);
-// $mu->post_blog_wordpress('/rss.php ' . substr(($time_finish - $time_start), 0, 6) . 's');
-error_log("${pid} FINISH " . substr(($time_finish - $time_start), 0, 6) . 's ' . substr((microtime(true) - $time_start), 0, 6) . 's');
+error_log("${pid} FINISH " . substr((microtime(true) - $time_start), 0, 6) . 's');
 
 exit();
