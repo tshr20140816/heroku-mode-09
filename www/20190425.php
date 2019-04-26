4e6d2034eecedd30a5651c359ce7f62204bf96ff
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-04-26 09:03:09 +0900

    Update 20190425.php

--- www/20190425.php
+++ www/20190425.php
@@ -15,9 +15,66 @@ function func_20190425($mu_, $file_name_blog_, $target_ = 'TOODLEDO')
 {
     $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
 
+    if (getenv('HEROKU_API_KEY_' . $target_) == '') {
+        $api_key = base64_decode(getenv('HEROKU_API_KEY'));
+    } else {
+        $api_key = base64_decode(getenv('HEROKU_API_KEY_' . $target_));
+    }
+    $url = 'https://api.heroku.com/account';
+
+    $res = $mu_->get_contents(
+        $url,
+        [CURLOPT_HTTPHEADER => ['Accept: application/vnd.heroku+json; version=3',
+                                "Authorization: Bearer ${api_key}",
+                               ]],
+        true
+    );
+
+    $data = json_decode($res, true);
+    error_log($log_prefix . '$data : ' . print_r($data, true));
+    $account = explode('@', $data['email'])[0];
+    $url = "https://api.heroku.com/accounts/${data['id']}/actions/get-quota";
+
+    $res = $mu_->get_contents(
+        $url,
+        [CURLOPT_HTTPHEADER => ['Accept: application/vnd.heroku+json; version=3.account-quotas',
+                                "Authorization: Bearer ${api_key}",
+        ]]
+    );
+
+    $data = json_decode($res, true);
+    error_log($log_prefix . '$data : ' . print_r($data, true));
+
+    $dyno_used = (int)$data['quota_used'];
+    $dyno_quota = (int)$data['account_quota'];
+
+    error_log($log_prefix . '$dyno_used : ' . $dyno_used);
+    error_log($log_prefix . '$dyno_quota : ' . $dyno_quota);
+
+    $quota = $dyno_quota - $dyno_used;
+    
     $keyword = strtolower($target_);
     for ($i = 0; $i < strlen($keyword); $i++) {
         $keyword[$i] = chr(ord($keyword[$i]) + 1);
     }
-    error_log($keyword);
+    $keyword .= 'rvpub';
+
+    $description = '';
+    if ((int)date('j', strtotime('+9hours')) != 1) {
+        $hatena_blog_id = $mu_->get_env('HATENA_BLOG_ID', true);
+        $url = 'https://' . $hatena_blog_id . '/search?q=' . $keyword;
+        $res = $mu_->get_contents($url);
+        
+        $rc = preg_match('/<a class="entry-title-link" href="(.+?)"/', $res, $match);
+        $res = $mu_->get_contents($match[1]);
+        
+        $rc = preg_match('/<div class="' . $keyword . '">(.+?)</', $res, $match);
+        $description = $match[1];
+    }
+    $description = '<div class="' . $keyword . '">' . trim($description . ' ' . (int)($quota / 60)) . '</div>';
+    // $mu_->post_blog_hatena($keyword, $description);
+    
+    $quota = floor($quota / 86400) . 'd ' . ($quota / 3600 % 24) . 'h ' . ($quota / 60 % 60) . 'm';
+
+    file_put_contents($file_name_blog_, "\nQuota " . strtolower($target_) . " : ${quota}\n", FILE_APPEND);
 }
