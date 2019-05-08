b58f4a39fe74e48f98665fb99338b37c038b31bd
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-05-08 11:48:14 +0900

    Update 20190507.php

--- www/20190507.php
+++ www/20190507.php
@@ -110,6 +110,9 @@ function func_20190507($mu_)
                                                               ],
                                                              ],
                                            ],
+                           'animation' => ['duration' => 0,],
+                           'hover' => ['animationDuration' => 0,],
+                           'responsiveAnimationDuration' => 0,
                           ],
             ];
     $url = 'https://quickchart.io/chart?width=600&height=320&c=' . json_encode($data);
