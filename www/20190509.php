f36d7664f2be43629636d9ff4facafd10f3d20f8
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-05-09 13:42:32 +0900

    Delete 20190509.php

--- www/20190509.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include(dirname(__FILE__) . '/../classes/MyUtils.php');
-
-$mu = new MyUtils();
-
-error_log($mu->get_env('FC2_FTP_ID', true));
-error_log($mu->get_env('FC2_FTP_SERVER', true));
-error_log($mu->get_env('FC2_ID', true));
