c25ba7419ed88ec6030c5fc0cf79f8b56780432b
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-05-17 09:56:27 +0900

    Create 20190517.php

---
+++ www/20190517.php
@@ -0,0 +1,15 @@
+<?php
+
+include(dirname(__FILE__) . '/../classes/MyUtils.php');
+
+$pid = getmypid();
+$requesturi = $_SERVER['REQUEST_URI'];
+$time_start = microtime(true);
+error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
+
+$mu = new MyUtils();
+
+error_log($pid . print_r($argv, true));
+
+$time_finish = microtime(true);
+error_log("${pid} FINISH " . substr(($time_finish - $time_start), 0, 6) . 's ' . substr((microtime(true) - $time_start), 0, 6) . 's');
