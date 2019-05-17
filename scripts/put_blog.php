82748696798d333f008a7427fe57122d694d1e4e
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-05-17 11:43:13 +0900

    Update put_blog.php

--- scripts/put_blog.php
+++ scripts/put_blog.php
@@ -3,9 +3,8 @@
 include(dirname(__FILE__) . '/../classes/MyUtils.php');
 
 $pid = getmypid();
-$requesturi = $_SERVER['REQUEST_URI'];
 $time_start = microtime(true);
-error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
+error_log("${pid} START scripts/put_blog.php " . date('Y/m/d H:i:s'));
 
 $mu = new MyUtils();
 
