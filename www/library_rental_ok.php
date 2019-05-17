e58c1341adf0c2a4fab69239db65c57d693faefa
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-05-17 13:32:13 +0900

    Update library_rental_ok.php

--- www/library_rental_ok.php
+++ www/library_rental_ok.php
@@ -1,11 +1,16 @@
 <?php
+
 include(dirname(__FILE__) . '/../classes/MyUtils.php');
+
 $pid = getmypid();
 $requesturi = $_SERVER['REQUEST_URI'];
 $time_start = microtime(true);
 error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
+
 $mu = new MyUtils();
+
 $rc = check_library_rental_ok($mu, $requesturi);
+
 error_log("${pid} FINISH " . substr((microtime(true) - $time_start), 0, 6) . 's');
 
 function check_library_rental_ok($mu_, $requesturi_)
