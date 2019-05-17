c1eea662846a3261095ea2df46434cfbe1b6498e
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-05-17 10:02:49 +0900

    Update 20190517.php

--- www/20190517.php
+++ www/20190517.php
@@ -9,7 +9,11 @@ error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
 
 $mu = new MyUtils();
 
+sleep(3);
+
 error_log($pid . print_r($argv, true));
+
+sleep(3);
 
 $time_finish = microtime(true);
 error_log("${pid} FINISH " . substr(($time_finish - $time_start), 0, 6) . 's ' . substr((microtime(true) - $time_start), 0, 6) . 's');
