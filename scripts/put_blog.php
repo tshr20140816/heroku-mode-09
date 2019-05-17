cd3f66f8b3828c93939263b47fb6a1e2ab07dbe6
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-05-17 11:41:43 +0900

    Update put_blog.php

--- scripts/put_blog.php
+++ scripts/put_blog.php
@@ -9,7 +9,13 @@ error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
 
 $mu = new MyUtils();
 
-$time_finish = microtime(true);
-$mu->post_blog_wordpress('TEST');
+switch (count($argv) {
+    case 2:
+       $mu->post_blog_wordpress(base64_decode($argv[1]));
+       break;
+    case 3:
+       $mu->post_blog_wordpress(base64_decode($argv[1]), base64_decode($argv[2]));
+        break;
+}
 
-error_log("${pid} FINISH " . substr(($time_finish - $time_start), 0, 6) . 's ' . substr((microtime(true) - $time_start), 0, 6) . 's');
+error_log("${pid} FINISH " . substr((microtime(true) - $time_start), 0, 6) . 's');
