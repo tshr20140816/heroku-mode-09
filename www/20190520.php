0890a950643e9423a07db146571692cb4fa0da03
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-05-20 14:55:30 +0900

    Update 20190520.php

--- www/20190520.php
+++ www/20190520.php
@@ -9,7 +9,7 @@ error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
 
 $mu = new MyUtils();
 
-func_20190520b($mu);
+func_20190520($mu);
 
 error_log("${pid} FINISH " . substr((microtime(true) - $time_start), 0, 6) . 's');
 
