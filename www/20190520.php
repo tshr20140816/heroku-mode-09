6ba1ea0c6f3cf24f18fa13a6175da01f8dbec099
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-05-20 14:02:02 +0900

    Update 20190520.php

--- www/20190520.php
+++ www/20190520.php
@@ -104,7 +104,7 @@ function func_20190520($mu_)
                                 ],
                   ];
     $url = 'https://quickchart.io/chart?width=900&height=480&c=' . json_encode($chart_data);
-    $res = $mu_->get_contents($url, [CURLOPT_HTTPHEADER => ['Expect:']]);
+    $res = $mu_->get_contents($url, [CURLOPT_HTTPHEADER => ['Expect: 100-continue']]);
 
     $im1 = imagecreatefromstring($res);
     error_log($log_prefix . imagesx($im1) . ' ' . imagesy($im1));
