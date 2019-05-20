76fe56f3782e971b81a1370c95407b039bef3be3
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-05-20 17:22:25 +0900

    Update 20190520.php

--- www/20190520.php
+++ www/20190520.php
@@ -21,7 +21,7 @@ function func_20190520($mu_)
         $labels[] = $i + 1;
         // $data1[] = ((int)date('t') - $i) * 24;
         $tmp = new stdClass();
-        $tmp->x = $i;
+        $tmp->x = $i + 1;
         $tmp->y = ((int)date('t') - $i) * 24;
         $data1[] = $tmp;
     }
