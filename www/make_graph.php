9c744564987ae8892affa023b766b0f0b485dc9b
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-05-21 10:56:09 +0900

    Update make_graph.php

--- www/make_graph.php
+++ www/make_graph.php
@@ -13,8 +13,7 @@ $file_name_rss_items = tempnam('/tmp', md5(microtime(true)));
 @unlink($file_name_rss_items);
 
 make_score_map($mu, $file_name_rss_items);
-make_usage_graph($mu, $file_name_rss_items, 'TOODLEDO');
-make_usage_graph($mu, $file_name_rss_items, 'TTRSS');
+make_usage_graph($mu, $file_name_rss_items);
 make_loggly_usage($mu, $file_name_rss_items);
 
 $xml_text = <<< __HEREDOC__
@@ -385,60 +384,98 @@ __HEREDOC__;
     file_put_contents($file_name_rss_items_, $rss_item_text, FILE_APPEND);
 }
 
-function make_usage_graph($mu_, $file_name_rss_items_, $target_)
+function make_usage_graph($mu_, $file_name_rss_items_)
 {
     $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
 
-    $keyword = strtolower($target_);
-    for ($i = 0; $i < strlen($keyword); $i++) {
-        $keyword[$i] = chr(ord($keyword[$i]) + 1);
-    }
-
-    $hatena_blog_id = $mu_->get_env('HATENA_BLOG_ID', true);
-    $url = 'https://' . $hatena_blog_id . '/search?q=' . $keyword . 'rvpub';
-    $res = $mu_->get_contents($url);
-
-    $rc = preg_match('/<a class="entry-title-link" href="(.+?)"/', $res, $match);
-
-    $res = $mu_->get_contents($match[1]);
-    $rc = preg_match('/<div class="' . $keyword . 'rvpub">(.+?)</', $res, $match);
-
-    $data2 = [];
-    foreach (explode(' ', $match[1]) as $item) {
-        $data2[] = (int)($item / 60);
-    }
-    if (count($data2) < 3) {
-        return;
-    }
-    array_shift($data2);
-    $data2[0] = 550;
-    $dy = ($data2[0] - end($data2)) / count($data2) + 1;
     for ($i = 0; $i < (int)date('t'); $i++) {
         $labels[] = $i + 1;
-        $data1[] = ((int)date('t') - $i) * 24;
-        $data3[] = (int)($data2[0] - $dy * $i);
+        // $data1[] = ((int)date('t') - $i) * 24;
+        $tmp = new stdClass();
+        $tmp->x = $i + 1;
+        $tmp->y = ((int)date('t') - $i) * 24;
+        $data1[] = $tmp;
     }
+
+    $datasets = [];
+    $datasets[] = ['data' => $data1,
+                   'fill' => false,
+                   'pointStyle' => 'line',
+                   'backgroundColor' => 'black',
+                   'borderColor' => 'black',
+                   'borderWidth' => 1,
+                   'pointRadius' => 0,
+                   'label' => 'max',
+                  ];
+
+    $hatena_blog_id = $mu_->get_env('HATENA_BLOG_ID', true);
+    $list = [['target' => 'toodledo',
+              'color' => 'green',
+              'planColor' => 'red',
+             ],
+             ['target' => 'ttrss',
+              'color' => 'cyan',
+              'planColor' => 'orange',
+             ],
+            ];
+    foreach ($list as $one_data) {
+        error_log(print_r($one_data, true));
+        $keyword = strtolower($one_data['target']);
+        for ($i = 0; $i < strlen($keyword); $i++) {
+            $keyword[$i] = chr(ord($keyword[$i]) + 1);
+        }
+
+        $url = 'https://' . $hatena_blog_id . '/search?q=' . $keyword . 'rvpub';
+        $res = $mu_->get_contents($url);
+
+        $rc = preg_match('/<a class="entry-title-link" href="(.+?)"/', $res, $match);
+
+        $res = $mu_->get_contents($match[1]);
+        $rc = preg_match('/<div class="' . $keyword . 'rvpub">(.+?)</', $res, $match);
+
+        $data2 = [];
+        foreach (explode(' ', $match[1]) as $item) {
+            $data2[] = (int)($item / 60);
+        }
+
+        if (count($data2) < 3) {
+            return;
+        }
+        array_shift($data2);
+        $data2[0] = 550;
+
+        $datasets[] = ['data' => $data2,
+                       'fill' => false,
+                       'pointStyle' => 'circle',
+                       'backgroundColor' => $one_data['color'],
+                       'borderColor' => $one_data['color'],
+                       'borderWidth' => 3,
+                       'pointRadius' => 4,
+                       'pointBorderWidth' => 0,
+                       'label' => $one_data['target'],
+                      ];
+
+        $data3 = [];
+        $dy = ($data2[0] - end($data2)) / count($data2) + 1;
+        for ($i = 0; $i < (int)date('t'); $i++) {
+            $data3[] = (int)($data2[0] - $dy * $i);
+        }
+        $datasets[] = ['data' => $data3,
+                       'fill' => false,
+                       'backgroundColor' => $one_data['planColor'],
+                       'borderWidth' => 3,
+                       'borderColor' => $one_data['planColor'],
+                       'pointRadius' => 0,
+                       'label' => $one_data['target'] . ' plan',
+                      ];
+
+    }
+    
     $chart_data = ['type' => 'line',
                    'data' => ['labels' => $labels,
-                              'datasets' => [['data' => $data1,
-                                              'fill' => false,
-                                              'pointStyle' => 'line',
-                                              'borderColor' => 'black',
-                                             ],
-                                             ['data' => $data2,
-                                              'fill' => false,
-                                              'pointStyle' => 'cross',
-                                              'borderColor' => 'green',
-                                              'borderWidth' => 5,
-                                             ],
-                                             ['data' => $data3,
-                                              'fill' => false,
-                                              'pointStyle' => 'line',
-                                              'borderColor' => 'red',
-                                             ],
-                                            ],
+                              'datasets' => $datasets,
                              ],
-                   'options' => ['legend' => ['display' => false,
+                   'options' => ['legend' => ['display' => true,
                                              ],
                                  'animation' => ['duration' => 0,
                                                 ],
@@ -448,16 +485,10 @@ function make_usage_graph($mu_, $file_name_rss_items_, $target_)
                                  'annotation' => ['annotations' => [['type' => 'line',
                                                                      'mode' => 'vertical',
                                                                      'scaleID' => 'x-axis-0',
-                                                                     'value' => count($data2),
+                                                                     'value' => count($datasets[1]['data']),
                                                                     ],
                                                                    ],
                                                  ],
-                                 /*
-                                 'plugins' => ['datalabels' => ['display' => true,
-                                                                'align' => 'bottom',
-                                                               ],
-                                              ],
-                                 */
                                 ],
                   ];
     $url = 'https://quickchart.io/chart?width=900&height=480&c=' . urlencode(json_encode($chart_data));
