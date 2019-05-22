46fd4acfc2a28af1f795e8bfc40a43628743fea8
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-05-22 10:55:01 +0900

    Update 20190522.php

--- www/20190522.php
+++ www/20190522.php
@@ -16,4 +16,20 @@ error_log("${pid} FINISH " . substr((microtime(true) - $time_start), 0, 6) . 's'
 function func_20190522($mu_)
 {
     $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
+    
+    $sql = <<< __HEREDOC__
+SELECT T1.check_time
+      ,T1.balance
+  FROM t_waon_history T1
+ ORDER BY M1.check_time DESC
+ LIMIT 40
+;
+__HEREDOC__;
+    
+    $pdo = $mu_->get_pdo();
+    
+    foreach ($pdo->query($sql) as $row) {
+        print_r($row, true);
+    }
+    $pdo = null;
 }
