30f934c29060ccf6f5cee109cc8aeefc2102d115
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-05-22 09:03:38 +0900

    Create 20190522.php

---
+++ www/20190522.php
@@ -0,0 +1,192 @@
+<?php
+
+include(dirname(__FILE__) . '/../classes/MyUtils.php');
+
+$pid = getmypid();
+$requesturi = $_SERVER['REQUEST_URI'];
+$time_start = microtime(true);
+error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
+
+$mu = new MyUtils();
+
+func_20190522($mu);
+
+error_log("${pid} FINISH " . substr((microtime(true) - $time_start), 0, 6) . 's');
+
+function func_20190522($mu_, $file_name_rss_items_)
+{
+    $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
+
+    for ($i = 0; $i < (int)date('t'); $i++) {
+        $labels[] = $i + 1;
+        // $data1[] = ((int)date('t') - $i) * 24;
+        $tmp = new stdClass();
+        $tmp->x = $i + 1;
+        $tmp->y = ((int)date('t') - $i) * 24;
+        $data1[] = $tmp;
+    }
+
+    $datasets = [];
+    $datasets[] = ['data' => $data1,
+                   'fill' => false,
+                   'pointStyle' => 'line',
+                   'backgroundColor' => 'black',
+                   'borderColor' => 'black',
+                   'borderWidth' => 1,
+                   'pointRadius' => 0,
+                   'label' => 'max',
+                  ];
+
+    $hatena_blog_id = $mu_->get_env('HATENA_BLOG_ID', true);
+    $list = [['target' => 'toodledo',
+              'color' => 'green',
+              'planColor' => 'red',
+             ],
+             ['target' => 'ttrss',
+              'color' => 'cyan',
+              'planColor' => 'orange',
+             ],
+            ];
+    foreach ($list as $one_data) {
+        error_log(print_r($one_data, true));
+        $keyword = strtolower($one_data['target']);
+        for ($i = 0; $i < strlen($keyword); $i++) {
+            $keyword[$i] = chr(ord($keyword[$i]) + 1);
+        }
+
+        $url = 'https://' . $hatena_blog_id . '/search?q=' . $keyword . 'rvpub';
+        $res = $mu_->get_contents($url);
+
+        $rc = preg_match('/<a class="entry-title-link" href="(.+?)"/', $res, $match);
+
+        $res = $mu_->get_contents($match[1]);
+        $rc = preg_match('/<div class="' . $keyword . 'rvpub">(.+?)</', $res, $match);
+
+        $data2 = [];
+        foreach (explode(' ', $match[1]) as $item) {
+            // $data2[] = (int)($item / 60);
+            $tmp1 = explode(',', $item);
+            $tmp2 = new stdClass();
+            $tmp2->x = (int)$tmp1[0] - 1;
+            $tmp2->y = (int)$tmp1[1];
+            $data2[] = $tmp2;
+        }
+
+        if (count($data2) < 3) {
+            return;
+        }
+        if ($data2[0]->x == 0) {
+            array_shift($data2);
+            $data2[0] = 550;
+            $tmp = new stdClass();
+            $tmp->x = 1;
+            $tmp->y = 550;
+            $data2[0] = $tmp;
+        }
+
+        $datasets[] = ['data' => $data2,
+                       'fill' => false,
+                       'pointStyle' => 'circle',
+                       'backgroundColor' => $one_data['color'],
+                       'borderColor' => $one_data['color'],
+                       'borderWidth' => 3,
+                       'pointRadius' => 4,
+                       'pointBorderWidth' => 0,
+                       'label' => $one_data['target'],
+                      ];
+
+        $data3 = [];
+        $dy = ($data2[0] - end($data2)) / count($data2) + 1;
+        for ($i = 0; $i < (int)date('t'); $i++) {
+            $data3[] = (int)($data2[0] - $dy * $i);
+        }
+        $datasets[] = ['data' => $data3,
+                       'fill' => false,
+                       'backgroundColor' => $one_data['planColor'],
+                       'borderWidth' => 3,
+                       'borderColor' => $one_data['planColor'],
+                       'pointRadius' => 0,
+                       'label' => $one_data['target'] . ' plan',
+                      ];
+
+    }
+    
+    $chart_data = ['type' => 'line',
+                   'data' => ['labels' => $labels,
+                              'datasets' => $datasets,
+                             ],
+                   'options' => ['legend' => ['display' => true,
+                                             ],
+                                 'animation' => ['duration' => 0,
+                                                ],
+                                 'hover' => ['animationDuration' => 0,
+                                            ],
+                                 'responsiveAnimationDuration' => 0,
+                                 'annotation' => ['annotations' => [['type' => 'line',
+                                                                     'mode' => 'vertical',
+                                                                     'scaleID' => 'x-axis-0',
+                                                                     'value' => count($datasets[1]['data']),
+                                                                    ],
+                                                                   ],
+                                                 ],
+                                ],
+                  ];
+    $url = 'https://quickchart.io/chart?width=900&height=480&c=' . urlencode(json_encode($chart_data));
+    $res = $mu_->get_contents($url);
+
+    $im1 = imagecreatefromstring($res);
+    error_log($log_prefix . imagesx($im1) . ' ' . imagesy($im1));
+    $im2 = imagecreatetruecolor(imagesx($im1) / 3, imagesy($im1) / 3);
+    imagealphablending($im2, false);
+    imagesavealpha($im2, true);
+    imagecopyresampled($im2, $im1, 0, 0, 0, 0, imagesx($im1) / 3, imagesy($im1) / 3, imagesx($im1), imagesy($im1));
+    imagedestroy($im1);
+
+    $file = tempnam("/tmp", md5(microtime(true)));
+    imagepng($im2, $file, 9);
+    imagedestroy($im2);
+    $res = file_get_contents($file);
+    unlink($file);
+
+    $url = 'https://api.tinify.com/shrink';
+    $options = [CURLOPT_HTTPAUTH => CURLAUTH_BASIC,
+                CURLOPT_USERPWD => 'api:' . getenv('TINYPNG_API_KEY'),
+                CURLOPT_POST => true,
+                CURLOPT_BINARYTRANSFER => true,
+                CURLOPT_POSTFIELDS => $res,
+                CURLOPT_HEADER => true,
+               ];
+    $res = $mu_->get_contents($url, $options);
+
+    $tmp = preg_split('/^\r\n/m', $res, 2);
+
+    $rc = preg_match('/compression-count: (.+)/i', $tmp[0], $match);
+    error_log($log_prefix . 'Compression count : ' . $match[1]); // Limits 500/month
+    // $mu_->post_blog_wordpress('api.tinify.com', 'Compression count : ' . $match[1] . "\r\n" . 'Limits 500/month');
+    $json = json_decode($tmp[1]);
+    error_log($log_prefix . print_r($json, true));
+
+    $url = $json->output->url;
+    $options = [CURLOPT_HTTPAUTH => CURLAUTH_BASIC,
+                CURLOPT_USERPWD => 'api:' . getenv('TINYPNG_API_KEY'),
+               ];
+    $res = $mu_->get_contents($url, $options);
+    $description = '<img src="data:image/png;base64,' . base64_encode($res) . '" />';
+    $mu_->post_blog_hatena('heroku dyno usage', $description);
+    $mu_->post_blog_fc2('heroku dyno usage', $description);
+    $description = '<![CDATA[' . $description . ']]>';
+
+    $rss_item_text = <<< __HEREDOC__
+<item>
+<guid isPermaLink="false">__HASH__</guid>
+<pubDate />
+<title>heroku dyno usage</title>
+<link>http://dummy.local/</link>
+<description>__DESCRIPTION__</description>
+</item>
+__HEREDOC__;
+
+    $rss_item_text = str_replace('__DESCRIPTION__', $description, $rss_item_text);
+    $rss_item_text = str_replace('__HASH__', hash('sha256', $description), $rss_item_text);
+    file_put_contents($file_name_rss_items_, $rss_item_text, FILE_APPEND);
+}
