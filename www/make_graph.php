93cf215448ce8ac1529d8b6751abd461b7c15be1
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-05-22 14:42:11 +0900

    Update make_graph.php

--- www/make_graph.php
+++ www/make_graph.php
@@ -12,6 +12,7 @@ $mu = new MyUtils();
 $file_name_rss_items = tempnam('/tmp', md5(microtime(true)));
 @unlink($file_name_rss_items);
 
+make_waon_balance($mu, $file_name_rss_items);
 make_score_map($mu, $file_name_rss_items);
 make_heroku_dyno_usage_graph($mu, $file_name_rss_items);
 make_loggly_usage($mu, $file_name_rss_items);
@@ -563,6 +564,142 @@ function make_heroku_dyno_usage_graph($mu_, $file_name_rss_items_)
 <guid isPermaLink="false">__HASH__</guid>
 <pubDate />
 <title>heroku dyno usage</title>
+<link>http://dummy.local/</link>
+<description>__DESCRIPTION__</description>
+</item>
+__HEREDOC__;
+
+    $rss_item_text = str_replace('__DESCRIPTION__', $description, $rss_item_text);
+    $rss_item_text = str_replace('__HASH__', hash('sha256', $description), $rss_item_text);
+    file_put_contents($file_name_rss_items_, $rss_item_text, FILE_APPEND);
+}
+
+function make_waon_balance($mu_, $file_name_rss_items_)
+{
+    $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
+    
+    $sql = <<< __HEREDOC__
+SELECT to_char(T1.check_time, 'YYYY/MM/DD') check_date
+      ,MIN(T1.balance) balance
+  FROM t_waon_history T1
+ GROUP BY to_char(T1.check_time, 'YYYY/MM/DD')
+ ORDER BY to_char(T1.check_time, 'YYYY/MM/DD') DESC
+ LIMIT 20
+;
+__HEREDOC__;
+
+    $pdo = $mu_->get_pdo();
+
+    $labels = [];
+    $data1 = [];
+    foreach ($pdo->query($sql) as $row) {
+        $labels[$row['check_date']] = date('m/d', strtotime($row['check_date']));
+        $tmp = new stdClass();
+        $tmp->x = date('m/d', strtotime($row['check_date']));
+        $tmp->y = $row['balance'];
+        $data1[] = $tmp;
+    }
+    $pdo = null;
+
+    ksort($labels);
+    $labels = array_values($labels);
+
+    $datasets = [];
+
+    $datasets[] = ['data' => $data1,
+                   'fill' => false,
+                   'pointStyle' => 'circle',
+                   'backgroundColor' => 'deepskyblue',
+                   'borderColor' => 'deepskyblue',
+                   'borderWidth' => 3,
+                   'pointRadius' => 4,
+                   'pointBorderWidth' => 0,
+                  ];
+
+    $scales = new stdClass();
+    $scales->yAxes[] = ['display' => true,
+                        'ticks' => '__TICKS__',
+                       ];
+
+    $chart_data = ['type' => 'line',
+                   'data' => ['labels' => $labels,
+                              'datasets' => $datasets,
+                             ],
+                   'options' => ['legend' => ['display' => false,
+                                             ],
+                                 'animation' => ['duration' => 0,
+                                                ],
+                                 'hover' => ['animationDuration' => 0,
+                                            ],
+                                 'responsiveAnimationDuration' => 0,
+                                 'annotation' => ['annotations' => [['type' => 'line',
+                                                                     'mode' => 'horizontal',
+                                                                     'scaleID' => 'y-axis-0',
+                                                                     'value' => $data1[0]->y,
+                                                                     'borderColor' => 'black',
+                                                                     'borderWidth' => 1,
+                                                                     'label' => ['enabled' => true,
+                                                                                 'content' => number_format($data1[0]->y),
+                                                                                ],
+                                                                    ],
+                                                                   ],
+                                                 ],
+                                 'scales' => $scales,
+                                ],
+                  ];
+
+    $tmp = str_replace('"__TICKS__"', "{callback: function(value){return value.toLocaleString();}}", json_encode($chart_data));
+
+    $url = 'https://quickchart.io/chart?width=600&height=360&c=' . urlencode($tmp);
+    $res = $mu_->get_contents($url);
+
+    $im1 = imagecreatefromstring($res);
+    error_log($log_prefix . imagesx($im1) . ' ' . imagesy($im1));
+    $im2 = imagecreatetruecolor(imagesx($im1) / 2, imagesy($im1) / 2);
+    imagealphablending($im2, false);
+    imagesavealpha($im2, true);
+    imagecopyresampled($im2, $im1, 0, 0, 0, 0, imagesx($im1) / 2, imagesy($im1) / 2, imagesx($im1), imagesy($im1));
+    imagedestroy($im1);
+
+    $file = tempnam("/tmp", md5(microtime(true)));
+    imagepng($im2, $file, 9);
+    imagedestroy($im2);
+    $res = file_get_contents($file);
+    unlink($file);
+
+    $url = 'https://api.tinify.com/shrink';
+    $options = [CURLOPT_HTTPAUTH => CURLAUTH_BASIC,
+                CURLOPT_USERPWD => 'api:' . getenv('TINYPNG_API_KEY'),
+                CURLOPT_POST => true,
+                CURLOPT_BINARYTRANSFER => true,
+                CURLOPT_POSTFIELDS => $res,
+                CURLOPT_HEADER => true,
+               ];
+    $res = $mu_->get_contents($url, $options);
+
+    $tmp = preg_split('/^\r\n/m', $res, 2);
+
+    $rc = preg_match('/compression-count: (.+)/i', $tmp[0], $match);
+    error_log($log_prefix . 'Compression count : ' . $match[1]); // Limits 500/month
+    // $mu_->post_blog_wordpress('api.tinify.com', 'Compression count : ' . $match[1] . "\r\n" . 'Limits 500/month');
+    $json = json_decode($tmp[1]);
+    error_log($log_prefix . print_r($json, true));
+
+    $url = $json->output->url;
+    $options = [CURLOPT_HTTPAUTH => CURLAUTH_BASIC,
+                CURLOPT_USERPWD => 'api:' . getenv('TINYPNG_API_KEY'),
+               ];
+    $res = $mu_->get_contents($url, $options);
+    $description = '<img src="data:image/png;base64,' . base64_encode($res) . '" />';
+    $mu_->post_blog_hatena('waon balance', $description);
+    $mu_->post_blog_fc2('waon balance', $description);
+    $description = '<![CDATA[' . $description . ']]>';
+
+    $rss_item_text = <<< __HEREDOC__
+<item>
+<guid isPermaLink="false">__HASH__</guid>
+<pubDate />
+<title>waon balance</title>
 <link>http://dummy.local/</link>
 <description>__DESCRIPTION__</description>
 </item>
