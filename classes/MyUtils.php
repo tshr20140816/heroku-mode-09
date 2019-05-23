e5eb941d397e92facc2047ff44ff82fcba4a754b
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-05-24 06:22:57 +0900

    Update MyUtils.php

--- classes/MyUtils.php
+++ classes/MyUtils.php
@@ -566,52 +566,7 @@ __HEREDOC__;
 
         error_log($log_prefix . 'RESULT : ' . $res);
 
-        $this->update_ttrss();
-    }
-
-    public function update_ttrss()
-    {
-        $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
-
-        /*
-        $options = [
-            CURLOPT_HTTPAUTH => CURLAUTH_BASIC,
-            CURLOPT_USERPWD => getenv('BASIC_USER') . ':' . getenv('BASIC_PASSWORD'),
-            CURLOPT_HTTPHEADER => ['Content-Type: application/json',],
-            CURLOPT_POST => true,
-        ];
-
-        $url = $this->get_env('URL_TTRSS_1') . 'api/';
-        $login_user = base64_decode(getenv('TTRSS_USER'));
-        $login_password = base64_decode(getenv('TTRSS_PASSWORD'));
-        $json = '{"op":"login","user":"' . $login_user .'","password":"' . $login_password . '"}';
-        $res = $this->get_contents($url, $options + [CURLOPT_POSTFIELDS => $json,]);
-        $data = json_decode($res);
-        $session_id = $data->content->session_id;
-
-        $urls = [];
-
-        $livedoor_id = $this->get_env('LIVEDOOR_ID', true);
-        $urls[] = "http://blog.livedoor.jp/${livedoor_id}/atom.xml";
-
-        $fc2_fqdn = $this->get_env('FC2_FTP_SERVER', true);
-        $urls[] = "https://${fc2_fqdn}/" . getenv('FC2_RSS_01') . '.xml';
-        $urls[] = "https://${fc2_fqdn}/" . getenv('FC2_RSS_02') . '.xml';
-        $urls[] = "https://${fc2_fqdn}/" . getenv('FC2_RSS_03') . '.xml';
-
-        $json = '{"sid":"' . $session_id . '","op":"getFeeds","cat_id":-3}';
-        $res = $this->get_contents($url, $options + [CURLOPT_POSTFIELDS => $json,]);
-        $data = json_decode($res);
-        foreach ($data->content as $feed) {
-            error_log($log_prefix . 'feed_url : ' . $feed->feed_url);
-            error_log($log_prefix . 'feed_id : ' . $feed->id);
-            if (in_array($feed->feed_url, $urls)) {
-                $json = '{"sid":"' . $session_id . '","op":"updateFeed","feed_id":' . $feed->id . '}';
-                $res = $this->get_contents($url, $options + [CURLOPT_POSTFIELDS => $json,]);
-                error_log($log_prefix . print_r(json_decode($res), true));
-            }
-        }
-        */
+        // $this->update_ttrss();
 
         error_log($log_prefix . 'start exec');
         exec('php ../scripts/update_ttrss.php ' . base64_encode($title_) . ' ' . base64_encode($description_) . ' >/dev/null &');
