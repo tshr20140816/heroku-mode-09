2ab47c58fb748a86918d6af03338d99121283a2f
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-05-23 15:19:05 +0900

    Update 20190521.php

--- www/20190521.php
+++ www/20190521.php
@@ -87,15 +87,15 @@ function func_20190521($mu_)
                                  'responsiveAnimationDuration' => 0,
                                 ],
                   ];
-    $url = 'https://quickchart.io/chart?width=900&height=480&c=' . urlencode(json_encode($chart_data));
+    $url = 'https://quickchart.io/chart?width=600&height=360&c=' . urlencode(json_encode($chart_data));
     $res = $mu_->get_contents($url);
 
     $im1 = imagecreatefromstring($res);
     error_log($log_prefix . imagesx($im1) . ' ' . imagesy($im1));
-    $im2 = imagecreatetruecolor(imagesx($im1) / 3, imagesy($im1) / 3);
+    $im2 = imagecreatetruecolor(imagesx($im1) / 2, imagesy($im1) / 2);
     imagealphablending($im2, false);
     imagesavealpha($im2, true);
-    imagecopyresampled($im2, $im1, 0, 0, 0, 0, imagesx($im1) / 3, imagesy($im1) / 3, imagesx($im1), imagesy($im1));
+    imagecopyresampled($im2, $im1, 0, 0, 0, 0, imagesx($im1) / 2, imagesy($im1) / 2, imagesx($im1), imagesy($im1));
     imagedestroy($im1);
 
     $file = tempnam("/tmp", md5(microtime(true)));
