882d0e9cad82343967b42b0c7387ba1b9e4daf3c
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-05-23 16:28:43 +0900

    Update 20190523.php

--- www/20190523.php
+++ www/20190523.php
@@ -29,7 +29,7 @@ function func_20190523b($mu_)
     
     foreach ($pdo->query("SELECT pg_database_size('${database_name}') size") as $row) {
         error_log($log_prefix . print_r($row, true));
-        error_log((int)$row['size'] / 1024 / 1024);
+        error_log(round((int)$row['size'] / 1024 / 1024));
     }
     $pdo = null;
 }
