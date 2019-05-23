86338bacbb0072c764b743a6be1aa003b9403f85
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-05-23 13:38:09 +0900

    Update 20190523.php

--- www/20190523.php
+++ www/20190523.php
@@ -9,13 +9,17 @@ error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
 
 $mu = new MyUtils();
 
-func_20190523($mu, '/tmp/dummy');
-func_20190523($mu, '/tmp/dummy', 'TTRSS');
-func_20190523($mu, '/tmp/dummy', 'REDMINE');
+func_20190523b($mu);
 
 error_log(file_get_contents('/tmp/dummy'));
 
 error_log("${pid} FINISH " . substr((microtime(true) - $time_start), 0, 6) . 's');
+
+function func_20190523b($mu_)
+{
+    error_log($mu_->get_encrypt_string(getenv('DATABASE_URL_TTRSS'));
+    error_log($mu_->get_encrypt_string(getenv('DATABASE_URL_REDMINE'));
+}
 
 function func_20190523($mu_, $file_name_blog_, $target_ = 'TOODLEDO')
 {
