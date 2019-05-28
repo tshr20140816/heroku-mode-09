b76454e3dab440855dba2c119981427ce3be7c0a
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-05-28 12:53:15 +0900

    Update 20190527.php

--- www/20190527.php
+++ www/20190527.php
@@ -57,7 +57,7 @@ function func_20190527b($mu_, $file_name_rss_items_)
                              ],
                   ];
     
-    $url = 'https://quickchart.io/chart?c=' . urlencode($tmp);
+    $url = 'https://quickchart.io/chart?c=' . urlencode($chart_data);
     $res = $mu_->get_contents($url);
 
     header('Content-Type: image/png');
