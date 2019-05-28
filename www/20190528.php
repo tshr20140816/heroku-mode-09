6122604ed7216ed5a58a3a7e211709cee08b03c3
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-05-29 05:13:18 +0900

    Update 20190528.php

--- www/20190528.php
+++ www/20190528.php
@@ -59,7 +59,7 @@ function func_20190528($mu_)
 
     $cookie = tempnam('/tmp', 'cookie_' . md5(microtime(true)));
     
-    // $url = 'https://search.travel.rakuten.co.jp/ds/hotellist/Japan-Mie-Tsu-low?f_nen1=2019&f_tuki1=10&f_hi1=11&f_nen2=2019&f_tuki2=10&f_hi2=12&f_otona_su=2&f_s1=0&f_s2=0&f_y1=0&f_y2=0&f_y3=0&f_y4=0&f_heya_su=1&f_kin2=0&f_ido=0&f_kdo=0&f_km=7.0&f_hyoji=30&f_image=1&f_tab=hotel&f_datumType=WGS&f_point_min=0';
+    $url_org = 'https://search.travel.rakuten.co.jp/ds/hotellist/Japan-Mie-Tsu-low?f_nen1=2019&f_tuki1=10&f_hi1=11&f_nen2=2019&f_tuki2=10&f_hi2=12&f_otona_su=2&f_s1=0&f_s2=0&f_y1=0&f_y2=0&f_y3=0&f_y4=0&f_heya_su=1&f_kin2=0&f_ido=0&f_kdo=0&f_km=7.0&f_hyoji=30&f_image=1&f_tab=hotel&f_datumType=WGS&f_point_min=0';
     
     
     /*
@@ -77,7 +77,7 @@ function func_20190528($mu_)
 
     $url = 'https://www.hidemyass-freeproxy.com/process/ja-jp';
     
-    $post_data = ['form[url]' => 'https://www.yahoo.co.jp/',
+    $post_data = ['form[url]' => $url_org,
                   'form[dataCenter]' => 'random',
                   'terms-agreed' => '1',
                  ];
