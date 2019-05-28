0864a21b0de482e32c832f9cd8f8ab4926fc1cbf
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-05-28 15:57:41 +0900

    Create 20190528.php

---
+++ www/20190528.php
@@ -0,0 +1,24 @@
+<?php
+
+include(dirname(__FILE__) . '/../classes/MyUtils.php');
+
+$pid = getmypid();
+$requesturi = $_SERVER['REQUEST_URI'];
+$time_start = microtime(true);
+error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
+
+$mu = new MyUtils();
+
+$file_name_rss_items = tempnam('/tmp', 'rss_' . md5(microtime(true)));
+@unlink($file_name_rss_items);
+
+func_20190528($mu, $file_name_rss_items);
+
+$time_finish = microtime(true);
+
+error_log("${pid} FINISH " . substr(($time_finish - $time_start), 0, 6) . 's ' . substr((microtime(true) - $time_start), 0, 6) . 's');
+
+function func_20190528($mu_, $file_name_rss_items_)
+{
+    $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
+}
