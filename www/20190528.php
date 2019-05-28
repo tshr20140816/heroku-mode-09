9c73c2e6b651433ea92fae4bd261dd0e7cd2c9d5
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-05-28 17:05:55 +0900

    Update 20190528.php

--- www/20190528.php
+++ www/20190528.php
@@ -56,6 +56,8 @@ function func_20190528($mu_)
     $res = $mu_->get_contents($url);
     error_log($res);
     */
+    $cookie = tempnam('/tmp', 'cookie_' . md5(microtime(true)));
+    
     $url = 'https://search.travel.rakuten.co.jp/ds/hotellist/Japan-Mie-Tsu-low?f_nen1=2019&f_tuki1=10&f_hi1=11&f_nen2=2019&f_tuki2=10&f_hi2=12&f_otona_su=2&f_s1=0&f_s2=0&f_y1=0&f_y2=0&f_y3=0&f_y4=0&f_heya_su=1&f_kin2=0&f_ido=0&f_kdo=0&f_km=7.0&f_hyoji=30&f_image=1&f_tab=hotel&f_datumType=WGS&f_point_min=0';
     
     $options = [
@@ -68,6 +70,8 @@ function func_20190528($mu_)
             'DNT: 1',
             'Upgrade-Insecure-Requests: 1',
             ],
+        CURLOPT_COOKIEJAR => $cookie,
+        CURLOPT_COOKIEFILE => $cookie,
     ];
     
     $res = $mu_->get_contents($url, $options);
