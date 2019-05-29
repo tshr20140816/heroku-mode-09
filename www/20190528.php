5f41f853614c9e2dccd8b03676e33491d82b2dd7
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-05-29 10:22:49 +0900

    Update 20190528.php

--- www/20190528.php
+++ www/20190528.php
@@ -26,13 +26,13 @@ function func_20190528($mu_)
     // $tmp = explode('&', parse_url($url, PHP_URL_QUERY));
     parse_str(parse_url($url, PHP_URL_QUERY), $tmp);
     error_log(print_r($tmp, true));
-    /*
+
     $y = $tmp['f_nen1'];
     $m = $tmp['f_tuki1'];
     $d = $tmp['f_hi1'];
 
     $info = "${y}/${m}/${d}\r\n";
-    */
+
     $res = $mu_->get_contents_proxy($url);
     
     $tmp = explode('<dl class="htlGnrlInfo">', $res);
@@ -44,7 +44,7 @@ function func_20190528($mu_)
         $info .= $match[1];
         $rc = preg_match('/<span class="vPrice".*?>(.+)/', $hotel_info, $match);
         error_log(strip_tags($match[1]));
-        $info .= ' ' . $match[1] . "\r\n\r\n";
+        $info .= ' ' . strip_tags($match[1]) . "\r\n\r\n";
     }
     error_log($info);
 }
