e3171fbd0d6a87e94ffbe2f2ec3d3129e50a89d1
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-05-29 10:20:44 +0900

    Update 20190528.php

--- www/20190528.php
+++ www/20190528.php
@@ -23,13 +23,16 @@ function func_20190528($mu_)
     
     $url = 'https://search.travel.rakuten.co.jp/ds/hotellist/Japan-Mie-Tsu-low?f_nen1=2019&f_tuki1=10&f_hi1=11&f_nen2=2019&f_tuki2=10&f_hi2=12&f_otona_su=2&f_s1=0&f_s2=0&f_y1=0&f_y2=0&f_y3=0&f_y4=0&f_heya_su=1&f_kin2=0&f_ido=0&f_kdo=0&f_km=7.0&f_hyoji=30&f_image=1&f_tab=hotel&f_datumType=WGS&f_point_min=0';
     
-    $tmp = explode('&', parse_url($url, PHP_URL_QUERY));
+    // $tmp = explode('&', parse_url($url, PHP_URL_QUERY));
+    parse_str(parse_url($url, PHP_URL_QUERY), $tmp);
+    error_log(print_r($tmp, true));
+    /*
     $y = $tmp['f_nen1'];
     $m = $tmp['f_tuki1'];
     $d = $tmp['f_hi1'];
 
     $info = "${y}/${m}/${d}\r\n";
-    
+    */
     $res = $mu_->get_contents_proxy($url);
     
     $tmp = explode('<dl class="htlGnrlInfo">', $res);
