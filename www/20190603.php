f8b4ff6d7729abc9a7736027e9e3f9a37ce50ffb
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-06-03 09:02:31 +0900

    Update 20190603.php

--- www/20190603.php
+++ www/20190603.php
@@ -13,66 +13,21 @@ $mu = new MyUtils();
 
 $html = <<< __HEREDOC__
 <html><body>
-<form method="POST" action="./imap.php">
-<input type="text" name="user" />
-<input type="password" name="password" />
-<input type="text" name="message_number" />
+<form method="POST" action="./20190603.php" enctype="multipart/form-data">
+<input type="file" name="upload_file" />
 <input type="submit" /> 
 </form>
 </body></html>
 __HEREDOC__;
 
 if ($_SERVER["REQUEST_METHOD"] == 'POST') {
-    $user = $_POST['user'];
-    $password = $_POST['password'];
-    $message_number = $_POST['message_number'];
-
-    $imap = imap_open('{imap.mail.yahoo.co.jp:993/ssl}', $user, $password);
-
-    $header = imap_header($imap, $message_number);
-    $struct = imap_fetchstructure($imap, $message_number);
-
-    error_log('header : ' . print_r($header, true));
-    error_log('struct : ' . print_r($struct, true));
-
-    if (isset($struct->parts)) {
-        $loop_end = count($struct->parts);
-    } else {
-        $loop_end = 1;
-    }
-
-    for ($i = 0; $i < $loop_end; $i++) {
-        if (isset($struct->parts)) {
-            $charset = $struct->parts[$i]->parameters[0]->value;
-            $encoding = $struct->parts[$i]->encoding;
-        } else {
-            $charset = $struct->parameters[0]->value;
-            $encoding = $struct->encoding;
+    $upload_file = $_FILES['upload_file'];
+    if (is_uploaded_file($upload_file['tmp_name'])) {
+        if (move_uploaded_file($upload_file['tmp_name'], '/tmp/' . $upload_file['name'])) {
+            $filesize = filesize('/tmp/' . $upload_file['name']);
+            error_log('filesize : ' . $filesize);
         }
-
-        $body = imap_fetchbody($imap, $message_number, $i + 1);
-
-        switch ($encoding) {
-            case 1: // 8bit
-                $body = imap_8bit($body);
-                $body = imap_qprint($body);
-                break;
-            case 3: // Base64
-                $body = imap_base64($body);
-                break;
-            case 4: // Quoted-Printable
-                $body = imap_qprint($body);
-                break;
-            default:
-                break;
-        }
-        $body = mb_convert_encoding($body, 'UTF-8', $charset);
-
-        error_log('----- No. ' . ($i + 1) . ' -----');
-        error_log($body);
     }
-
-    imap_close($imap);
 } else {
     echo $html;
 }
