582016221bb6bf22189bc32e00721dea4e3b3871
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-06-03 10:09:53 +0900

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -12,8 +12,9 @@ echo 'START'
 cat /tmp/pear_php_net.log
 echo 'FINISH'
 is_succeeded=$(grep -c -e succeeded /tmp/pear_php_net.log)
-echo is_succeeded
-pear install XML_RPC2 &
+if [ ${is_succeeded} != '0' ]; then
+    pear install XML_RPC2 &
+fi
 
 # ***** phppgadmin *****
 
@@ -24,36 +25,39 @@ cp ../config.inc.php phppgadmin/conf/
 ls -lang phppgadmin
 popd
 
-# ***** XML_RPC2 *****
-
 mkdir lib
-pushd lib
-git clone --depth=1 -b 1.1.4 https://github.com/pear/XML_RPC2.git .
-pushd /tmp
-mkdir pear_exception
-pushd pear_exception
-git clone --depth=1 https://github.com/pear/pear_exception.git .
-popd
-popd
-cp -af /tmp/pear_exception/* ./
-pushd /tmp
-mkdir http_request2
-pushd http_request2
-git clone --depth=1 https://github.com/pear/http_request2.git .
-popd
-popd
-cp -af /tmp/http_request2/* ./
-pushd /tmp
-mkdir net_url2
-pushd net_url2
-git clone --depth=1 https://github.com/pear/net_url2.git .
-popd
-popd
-cp -af /tmp/net_url2/* ./
 
-rm -f *
-ls -lang
-popd
+if [ ${is_succeeded} = '0' ]; then
+    # ***** XML_RPC2 *****
+
+    pushd lib
+    git clone --depth=1 -b 1.1.4 https://github.com/pear/XML_RPC2.git .
+    pushd /tmp
+    mkdir pear_exception
+    pushd pear_exception
+    git clone --depth=1 https://github.com/pear/pear_exception.git .
+    popd
+    popd
+    cp -af /tmp/pear_exception/* ./
+    pushd /tmp
+    mkdir http_request2
+    pushd http_request2
+    git clone --depth=1 https://github.com/pear/http_request2.git .
+    popd
+    popd
+    cp -af /tmp/http_request2/* ./
+    pushd /tmp
+    mkdir net_url2
+    pushd net_url2
+    git clone --depth=1 https://github.com/pear/net_url2.git .
+    popd
+    popd
+    cp -af /tmp/net_url2/* ./
+
+    rm -f *
+    ls -lang
+    popd
+fi
 
 wget -q https://github.com/squizlabs/PHP_CodeSniffer/releases/download/3.4.2/phpcs.phar
 wget -q https://github.com/squizlabs/PHP_CodeSniffer/releases/download/3.4.2/phpcbf.phar
