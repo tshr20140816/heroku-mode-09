3b561e0a728d7ef6263f6f52a60d1ede1f510576
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-06-03 11:45:46 +0900

    Update 20190603.php

--- www/20190603.php
+++ www/20190603.php
@@ -30,10 +30,10 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST') {
         $tmpdir = '/tmp/' . $tmpdir . '/';
         if (move_uploaded_file($upload_file['tmp_name'], $tmpdir . $upload_file['name'])) {
             $filesize = filesize($tmpdir . $upload_file['name']);
-            error_log('filesize : ' . $filesize);
+            error_log('rar filesize : ' . number_format($filesize));
             exec('cd ' . $tmpdir . ' && /app/bin/unrar x ./' . $upload_file['name']);
             unlink($tmpdir . $upload_file['name']);
-            exec('ls -lang ' . $tmpdir . ' >>/tmp/log.txt');
+            exec('ls -Rlang ' . $tmpdir . ' >>/tmp/log.txt');
             error_log(file_get_contents('/tmp/log.txt'));
             $download_file_name = pathinfo($upload_file['name'],  PATHINFO_FILENAME) . '.zip';
             exec('cd ' . $tmpdir . ' && zip -9r ' . $download_file_name . ' ./');
@@ -41,6 +41,7 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST') {
             header('Content-type: application/octet-stream');
             header('Content-Disposition: attachment; filename="' . $download_file_name . '"');
             header('Content-Length: ' . filesize($tmpdir . $download_file_name));
+            error_log('zip filesize : ' . number_format(filesize($tmpdir . $download_file_name)));
             echo file_get_contents($tmpdir . $download_file_name);
             unlink($tmpdir . $download_file_name);
         }
