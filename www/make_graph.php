23a5720f3dccad10e757b531e232fa14d534fe51
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-06-04 11:01:24 +0900

    Update make_graph.php

--- www/make_graph.php
+++ www/make_graph.php
@@ -17,6 +17,7 @@ $url_length = [];
 $url_length['make_waon_balance'] = make_waon_balance($mu, $file_name_rss_items);
 $url_length['make_score_map'] = make_score_map($mu, $file_name_rss_items);
 $url_length['make_heroku_dyno_usage_graph'] = make_heroku_dyno_usage_graph($mu, $file_name_rss_items);
+$url_length['make_heroku_dyno_usage_graph2'] = make_heroku_dyno_usage_graph2($mu, $file_name_rss_items);
 $url_length['make_database'] = make_database($mu, $file_name_rss_items);
 $url_length['make_process_time'] = make_process_time($mu, $file_name_rss_items);
 $url_length['make_post_count'] = make_post_count($mu, $file_name_rss_items);
@@ -591,6 +592,185 @@ function make_heroku_dyno_usage_graph($mu_, $file_name_rss_items_)
 <guid isPermaLink="false">__HASH__</guid>
 <pubDate>__PUBDATE__</pubDate>
 <title>heroku dyno usage</title>
+<link>http://dummy.local/</link>
+<description>__DESCRIPTION__</description>
+</item>
+__HEREDOC__;
+
+    $rss_item_text = str_replace('__PUBDATE__', date('D, j M Y G:i:s +0900', strtotime('+9 hours')), $rss_item_text);
+    $rss_item_text = str_replace('__DESCRIPTION__', $description, $rss_item_text);
+    $rss_item_text = str_replace('__HASH__', hash('sha256', $description), $rss_item_text);
+    file_put_contents($file_name_rss_items_, $rss_item_text, FILE_APPEND);
+
+    return $url_length;
+}
+
+function make_heroku_dyno_usage_graph2($mu_, $file_name_rss_items_)
+{
+    $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
+
+    for ($i = 0; $i < (int)date('t'); $i++) {
+        $labels[] = $i + 1;
+        $tmp = new stdClass();
+        $tmp->x = $i + 1;
+        $tmp->y = ((int)date('t') - $i) * 24;
+        $data1[] = $tmp;
+    }
+
+    $datasets = [];
+    $datasets[] = ['data' => $data1,
+                   'fill' => false,
+                   'pointStyle' => 'line',
+                   'backgroundColor' => 'black',
+                   'borderColor' => 'black',
+                   'borderWidth' => 1,
+                   'pointRadius' => 0,
+                   'label' => 'max',
+                  ];
+
+    $list = [['target' => 'first',
+              'color' => 'green',
+              'planColor' => 'red',
+             ],
+             ['target' => 'kyoto',
+              'color' => 'deepskyblue',
+              'planColor' => 'orange',
+             ],
+            ];
+    foreach ($list as $one_data) {
+        error_log(print_r($one_data, true));
+        $keyword = strtolower($one_data['target']);
+        for ($i = 0; $i < strlen($keyword); $i++) {
+            $keyword[$i] = chr(ord($keyword[$i]) + 1);
+        }
+
+        $res = $mu_->search_blog($keyword . 'rvpub');
+
+        $data2 = [];
+        foreach (explode(' ', $res) as $item) {
+            $tmp1 = explode(',', $item);
+            $tmp2 = new stdClass();
+            $tmp2->x = (int)$tmp1[0] - 1;
+            $tmp2->y = (int)($tmp1[1] / 60);
+            $data2[] = $tmp2;
+        }
+
+        if (count($data2) < 3) {
+            return 0;
+        }
+        if ($data2[0]->x == 0) {
+            array_shift($data2);
+            $tmp = new stdClass();
+            $tmp->x = 1;
+            $tmp->y = 550;
+            $data2[0] = $tmp;
+        }
+
+        $datasets[] = ['data' => $data2,
+                       'fill' => false,
+                       'pointStyle' => 'circle',
+                       'backgroundColor' => $one_data['color'],
+                       'borderColor' => $one_data['color'],
+                       'borderWidth' => 3,
+                       'pointRadius' => 4,
+                       'pointBorderWidth' => 0,
+                       'label' => $one_data['target'],
+                      ];
+
+        $data3 = [];
+        $tmp = new stdClass();
+        $tmp->x = 1;
+        $tmp->y = 550;
+        $data3[] = $tmp;
+        $tmp = new stdClass();
+        $tmp->x = (int)date('t');
+        $tmp->y = 550 - (int)((550 - end($data2)->y) / end($data2)->x + 1) * (int)date('t');
+        $data3[] = $tmp;
+
+        $datasets[] = ['data' => $data3,
+                       'fill' => false,
+                       'backgroundColor' => $one_data['planColor'],
+                       'borderWidth' => 3,
+                       'borderColor' => $one_data['planColor'],
+                       'pointRadius' => 0,
+                       'label' => $one_data['target'] . ' plan',
+                      ];
+
+    }
+
+    $chart_data = ['type' => 'line',
+                   'data' => ['labels' => $labels,
+                              'datasets' => $datasets,
+                             ],
+                   'options' => ['legend' => ['display' => true,
+                                              'labels' => ['boxWidth' => 10,
+                                                          ],
+                                             ],
+                                 'animation' => ['duration' => 0,
+                                                ],
+                                 'hover' => ['animationDuration' => 0,
+                                            ],
+                                 'responsiveAnimationDuration' => 0,
+                                 'annotation' => ['annotations' => [['type' => 'line',
+                                                                     'mode' => 'vertical',
+                                                                     'scaleID' => 'x-axis-0',
+                                                                     'value' => count($datasets[1]['data']),
+                                                                    ],
+                                                                   ],
+                                                 ],
+                                ],
+                  ];
+    $url = 'https://quickchart.io/chart?width=900&height=480&c=' . urlencode(json_encode($chart_data));
+    $res = $mu_->get_contents($url);
+    $url_length = strlen($url);
+
+    $im1 = imagecreatefromstring($res);
+    error_log($log_prefix . imagesx($im1) . ' ' . imagesy($im1));
+    $im2 = imagecreatetruecolor(imagesx($im1) / 3, imagesy($im1) / 3);
+    imagealphablending($im2, false);
+    imagesavealpha($im2, true);
+    imagecopyresampled($im2, $im1, 0, 0, 0, 0, imagesx($im1) / 3, imagesy($im1) / 3, imagesx($im1), imagesy($im1));
+    imagedestroy($im1);
+
+    $file = tempnam('/tmp', 'png_' . md5(microtime(true)));
+    imagepng($im2, $file, 9);
+    imagedestroy($im2);
+    $res = file_get_contents($file);
+    unlink($file);
+
+    $url = 'https://api.tinify.com/shrink';
+    $options = [CURLOPT_HTTPAUTH => CURLAUTH_BASIC,
+                CURLOPT_USERPWD => 'api:' . getenv('TINYPNG_API_KEY'),
+                CURLOPT_POST => true,
+                CURLOPT_BINARYTRANSFER => true,
+                CURLOPT_POSTFIELDS => $res,
+                CURLOPT_HEADER => true,
+               ];
+    $res = $mu_->get_contents($url, $options);
+
+    $tmp = preg_split('/^\r\n/m', $res, 2);
+
+    $rc = preg_match('/compression-count: (.+)/i', $tmp[0], $match);
+    error_log($log_prefix . 'Compression count : ' . $match[1]); // Limits 500/month
+    // $mu_->post_blog_wordpress('api.tinify.com', 'Compression count : ' . $match[1] . "\r\n" . 'Limits 500/month');
+    $json = json_decode($tmp[1]);
+    error_log($log_prefix . print_r($json, true));
+
+    $url = $json->output->url;
+    $options = [CURLOPT_HTTPAUTH => CURLAUTH_BASIC,
+                CURLOPT_USERPWD => 'api:' . getenv('TINYPNG_API_KEY'),
+               ];
+    $res = $mu_->get_contents($url, $options);
+    $description = '<img src="data:image/png;base64,' . base64_encode($res) . '" />';
+    // $mu_->post_blog_hatena('heroku dyno usage 2', $description);
+    // $mu_->post_blog_fc2('heroku dyno usage 2', $description);
+    $description = '<![CDATA[' . $description . ']]>';
+
+    $rss_item_text = <<< __HEREDOC__
+<item>
+<guid isPermaLink="false">__HASH__</guid>
+<pubDate>__PUBDATE__</pubDate>
+<title>heroku dyno usage 2</title>
 <link>http://dummy.local/</link>
 <description>__DESCRIPTION__</description>
 </item>
