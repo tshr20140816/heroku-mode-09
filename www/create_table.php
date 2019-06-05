ba91f042fb9aa3eaf88fbc2bf19b8c4ff2bb67d1
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-06-05 10:50:16 +0900

    Update create_table.php

--- www/create_table.php
+++ www/create_table.php
@@ -152,4 +152,15 @@ __HEREDOC__;
 $count = $pdo->exec($sql);
 error_log('create table result : ' . $count);
 
+//
+
+$sql = <<< __HEREDOC__
+CREATE TABLE t_data_log (
+    key character varying(128) PRIMARY KEY,
+    value character text
+);
+__HEREDOC__;
+$count = $pdo->exec($sql);
+error_log('create table result : ' . $count);
+
 $pdo = null;
