06e4aae642562c23ab218a6cb19b658f502c4585
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-06-06 10:21:05 +0900

    Update daily020.php

--- www/daily020.php
+++ www/daily020.php
@@ -25,29 +25,32 @@ $file_name_blog = tempnam("/tmp", 'blog_' .  md5(microtime(true)));
 exec('cd /app && composer update > /dev/null 2>&1 &');
 exec('curl --head ' . $mu->get_env('URL_TTRSS_1') . ' > /dev/null 2>&1 &');
 
-// quota TOODLEDO
+// quota
 get_quota($mu, $file_name_blog);
+
+// quota TOODLEDO
+// get_quota($mu, $file_name_blog);
 
 // Database Backup TOODLEDO
 backup_db($mu, $file_name_blog);
 
 // quota TTRSS
-get_quota($mu, $file_name_blog, 'TTRSS');
+// get_quota($mu, $file_name_blog, 'TTRSS');
 
 // Database Backup TTRSS
 backup_db($mu, $file_name_blog, 'TTRSS');
 
 // quota REDMINE
-get_quota($mu, $file_name_blog, 'REDMINE');
+// get_quota($mu, $file_name_blog, 'REDMINE');
 
 // Database Backup REDMINE
 backup_db($mu, $file_name_blog, 'REDMINE');
 
 // quota FIRST
-get_quota($mu, $file_name_blog, 'FIRST');
+// get_quota($mu, $file_name_blog, 'FIRST');
 
 // quota KYOTO
-get_quota($mu, $file_name_blog, 'KYOTO');
+// get_quota($mu, $file_name_blog, 'KYOTO');
 
 // WAON balance check
 check_waon_balance($mu, $file_name_blog);
@@ -123,7 +126,121 @@ error_log("${pid} FINISH " . substr(($time_finish - $time_start), 0, 6) . 's ' .
 
 exit();
 
-function get_quota($mu_, $file_name_blog_, $target_ = 'TOODLEDO')
+function get_quota($mu_, $file_name_blog_)
+{
+    $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
+
+    $list_targets = [];
+    $list_targets[] = 'TOODLEDO';
+    $list_targets[] = 'TTRSS';
+    $list_targets[] = 'REDMINE';
+    $list_targets[] = 'FIRST';
+    $list_targets[] = 'KYOTO';
+
+    $urls = [];
+    foreach ($list_targets as $target) {
+        if (getenv('HEROKU_API_KEY_' . $target) == '') {
+            $api_key = getenv('HEROKU_API_KEY');
+        } else {
+            $api_key = base64_decode(getenv('HEROKU_API_KEY_' . $target));
+        }
+        $options = [CURLOPT_HTTPHEADER => ['Accept: application/vnd.heroku+json; version=3',
+                                           "Authorization: Bearer ${api_key}",
+                                          ]];
+
+        $urls['https://api.heroku.com/account?' . hash('md5', $target)] = $options;
+    }
+
+    $multi_options = [
+        CURLMOPT_PIPELINING => 3,
+        CURLMOPT_MAX_HOST_CONNECTIONS => 10,
+    ];
+    $list_contents = $mu_->get_contents_multi($urls, null, $multi_options);
+
+    $urls = [];
+    foreach ($list_targets as $target) {
+        $data = json_decode($list_contents['https://api.heroku.com/account?' . hash('md5', $target)], true);
+        error_log($log_prefix . '$data : ' . print_r($data, true));
+
+        $account = explode('@', $data['email'])[0];
+        if (getenv('HEROKU_API_KEY_' . $target) == '') {
+            $api_key = getenv('HEROKU_API_KEY');
+        } else {
+            $api_key = base64_decode(getenv('HEROKU_API_KEY_' . $target));
+        }
+        $options = [CURLOPT_HTTPHEADER => ['Accept: application/vnd.heroku+json; version=3.account-quotas',
+                                           "Authorization: Bearer ${api_key}",
+                                          ]];
+        $urls["https://api.heroku.com/accounts/${data['id']}/actions/get-quota?" . hash('md5', $target)] = $options;
+    }
+    $list_contents = null;
+
+    $multi_options = [
+        CURLMOPT_PIPELINING => 3,
+        CURLMOPT_MAX_HOST_CONNECTIONS => 10,
+    ];
+    $list_contents = $mu_->get_contents_multi($urls, null, $multi_options);
+
+    $sql_select = <<< __HEREDOC__
+SELECT T1.value
+  FROM t_data_log T1
+ WHERE T1.key = :b_key
+__HEREDOC__;
+
+    $sql_upsert = <<< __HEREDOC__
+INSERT INTO t_data_log VALUES(:b_key, :b_value)
+    ON CONFLICT (key)
+    DO UPDATE SET value = :b_value
+__HEREDOC__;
+
+    $description = '';
+
+    $j = (int)date('j', strtotime('+9hours'));
+    $pdo = $mu_->get_pdo();
+    $statement_select = $pdo->prepare($sql_select);
+    $statement_upsert = $pdo->prepare($sql_upsert);
+    foreach ($list_targets as $target) {
+        $hash = hash('md5', $target);
+        foreach ($list_contents as $url => $contents) {
+            if (substr($url, strlen($hash) * -1) === $hash) {
+                $data = json_decode($contents, true);
+                error_log($log_prefix . '$data : ' . print_r($data, true));
+
+                $dyno_used = (int)$data['quota_used'];
+                $dyno_quota = (int)$data['account_quota'];
+
+                error_log($log_prefix . '$dyno_used : ' . $dyno_used);
+                error_log($log_prefix . '$dyno_quota : ' . $dyno_quota);
+
+                $quota = $dyno_quota - $dyno_used;
+
+                $quotas = [];
+                if ($j != 1) {
+                    $statement_select->execute([':b_key' => $target]);
+                    $result = $statement_select->fetchAll();
+                    if (count($result) != 0) {
+                        $quotas = json_decode($result[0]['value'], true);
+                    }
+                    $result = null;
+                }
+                $quotas[date('Ymd', strtotime('+9 hours'))] = $quota;
+                $rc = $statement_upsert->execute([':b_key' => $target,
+                                                  ':b_value' => json_encode($quotas),
+                                                 ]);
+                error_log($log_prefix . 'UPSERT $rc : ' . $rc);
+
+                $description .= $target . ' : ' . floor($quota / 86400) . 'd ' . ($quota / 3600 % 24) . 'h ' . ($quota / 60 % 60) . "m\n";
+                break;
+            }
+        }
+    }
+    $list_contents = null;
+    $pdo = null;
+
+    file_put_contents($file_name_blog_, "Quota\n" . trim($description), FILE_APPEND);
+}
+
+function get_quota_old($mu_, $file_name_blog_, $target_ = 'TOODLEDO')
 {
     $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
 
@@ -376,7 +493,7 @@ __HEREDOC__;
         error_log($log_prefix . print_r($row, true));
         $record_count = $row['cnt'];
     }
-    
+
     $database_size = 0;
     foreach ($pdo->query("SELECT pg_database_size('${database_name}') size") as $row) {
         error_log($log_prefix . print_r($row, true));
