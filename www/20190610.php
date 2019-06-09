a8fdb1dfb438075cc65dcfe00f52296e163c25f2
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-06-10 08:42:43 +0900

    Update 20190610.php

--- www/20190610.php
+++ www/20190610.php
@@ -9,17 +9,16 @@ error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
 
 $mu = new MyUtils();
 
-$file_name_rss_items = tempnam('/tmp', 'rss_' . md5(microtime(true)));
-@unlink($file_name_rss_items);
-
-$rc = func_20190601($mu, $file_name_rss_items);
+$rc = func_20190610($mu);
 
 $time_finish = microtime(true);
 
 error_log("${pid} FINISH " . substr(($time_finish - $time_start), 0, 6) . 's ' . substr((microtime(true) - $time_start), 0, 6) . 's');
 exit();
 
-function func_20190601($mu_, $file_name_rss_items_)
+function func_20190610($mu_)
 {
-
+    $url = 'https://twitter.com/bs_ponta';
+    $res = $mu_->get_contents($url);
+    error_log($res);
 }
