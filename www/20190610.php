34d4a272be60d9dd5d82fae163c9652bce05fa59
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-06-10 09:55:17 +0900

    Update 20190610.php

--- www/20190610.php
+++ www/20190610.php
@@ -71,5 +71,6 @@ __HEREDOC__;
     $file = '/tmp/' . getenv('FC2_RSS_02') . '.xml';
     file_put_contents($file, str_replace('__ITEMS__', implode('', $rss_items), $xml_text));
     $mu_->upload_fc2($file);
+    error_log('filesize : ' . filesize($file));
     unlink($file);
 }
