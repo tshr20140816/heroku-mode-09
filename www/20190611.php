db45c3252b5aba57bc4d62fdbe20da9832c7b70f
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-06-11 12:40:36 +0900

    Update 20190611.php

--- www/20190611.php
+++ www/20190611.php
@@ -22,7 +22,13 @@ function func_20190611($mu_)
     
     $url = 'http://twitrss.me/twitter_user_to_rss/?user=JAXA_JP';
     // $res = $mu_->get_contents($url);
-    $res = simplexml_load_file($url);
+    
+    // $res = simplexml_load_file($url);
+    // error_log(print_r($res, true));
+    
+    $doc = new DOMDocument();
+    $doc->load($url);
+    $res = $doc->saveXML();
     error_log(print_r($res, true));
 }
 
