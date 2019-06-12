51827f93ba505cc3ce89a432701a9a8ea750b3fb
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-06-12 11:23:32 +0900

    Update get_twitter_jaxa.php

--- www/get_twitter_jaxa.php
+++ www/get_twitter_jaxa.php
@@ -3,8 +3,9 @@
 include(dirname(__FILE__) . '/../classes/MyUtils.php');
 
 $pid = getmypid();
+$requesturi = $_SERVER['REQUEST_URI'];
 $time_start = microtime(true);
-error_log("${pid} START scripts/get_twitter_jaxa.php " . date('Y/m/d H:i:s'));
+error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
 
 $mu = new MyUtils();
 
