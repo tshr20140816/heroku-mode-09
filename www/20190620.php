32a6d61fe60d8122701c605c5cbcfabfb39dbc2a
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-06-20 08:40:28 +0900

    Create 20190620.php

---
+++ www/20190620.php
@@ -0,0 +1,26 @@
+<?php
+
+include(dirname(__FILE__) . '/../classes/MyUtils.php');
+
+$pid = getmypid();
+$requesturi = $_SERVER['REQUEST_URI'];
+$time_start = microtime(true);
+error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
+
+$mu = new MyUtils();
+
+$rc = func_20190620($mu);
+
+$time_finish = microtime(true);
+
+error_log("${pid} FINISH " . substr((microtime(true) - $time_start), 0, 6) . 's');
+exit();
+
+function func_20190620($mu_)
+{
+    $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
+
+    $url = 'https://www.train-guide.westjr.co.jp/api/v3/sanyo2_st.json';
+    $url = 'https://www.train-guide.westjr.co.jp/api/v3/sanyo2.json';
+}
+    
