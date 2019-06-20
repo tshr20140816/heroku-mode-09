9943035df3e0037e02b9c7f89bb3a8175b2b3a65
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-06-20 13:01:16 +0900

    Update and rename 20190620.php to check_train.php

--- www/check_train.php
+++ www/check_train.php
@@ -9,14 +9,14 @@ error_log("${pid} START ${requesturi} " . date('Y/m/d H:i:s'));
 
 $mu = new MyUtils();
 
-$rc = func_20190620($mu);
+$rc = check_train($mu);
 
 $time_finish = microtime(true);
 
 error_log("${pid} FINISH " . substr((microtime(true) - $time_start), 0, 6) . 's');
 exit();
 
-function func_20190620($mu_)
+function check_train($mu_)
 {
     $log_prefix = getmypid() . ' [' . __METHOD__ . '] ';
 
@@ -29,7 +29,7 @@ function func_20190620($mu_)
         $stations[$station['info']['code']] = $station['info']['name'];
     }
     
-    error_log(print_r($stations, true));
+    // error_log(print_r($stations, true));
     
     $url = 'https://www.train-guide.westjr.co.jp/api/v3/sanyo2.json';
 
@@ -54,7 +54,19 @@ function func_20190620($mu_)
             }
         }
     }
+    /*
     error_log(print_r($delays_up, true));
     error_log(print_r($delays_down, true));
+    */
+    $description = '';
+    if (count($delays_up) > 0) {
+        $description = implode("\n", $delays_up);
+    }
+    if (count($delays_down) > 0) {
+        $description .= implode("\n", $delays_down);
+    }
+    if ($description != '') {
+        $mu_->post_blog_livedoor('TRAIN', $description);
+    }
 }
     
