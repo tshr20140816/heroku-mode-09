687458519e9fa64d874c2fb85188b4d2dccc00c0
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-06-21 10:14:42 +0900

    Update start_web.sh

--- start_web.sh
+++ start_web.sh
@@ -15,34 +15,23 @@ if [ ! -v BASIC_PASSWORD ]; then
 fi
 
 export USER_AGENT=$(curl https://raw.githubusercontent.com/tshr20140816/heroku-mode-07/master/useragent.txt)
-
+export DATABASE_URL=$DATABASE_URL_TOODLEDO
 htpasswd -c -b .htpasswd ${BASIC_USER} ${BASIC_PASSWORD}
 
-set +x
 pushd classes
-for file in $( ls . | grep .php$ ); do
-  php -l ${file}
-done
+wget https://raw.githubusercontent.com/tshr20140816/heroku-mode-07/master/classes/MyUtils.php
 popd
-pushd scripts
-for file in $( ls . | grep .php$ ); do
-  php -l ${file}
-done
-popd
+
 pushd www
-for file in $( ls . | grep .php$ ); do
-  php -l ${file}
-done
+wget https://raw.githubusercontent.com/tshr20140816/heroku-mode-07/master/www/check_train.php
+wget https://raw.githubusercontent.com/tshr20140816/heroku-mode-07/master/www/opcache_compile_file.php
 popd
-set -x
 
 printenv > /tmp/printenv.txt
 wc -c < /tmp/printenv.txt
 rm /tmp/printenv.txt
 
 ls -lang /tmp
-
-wait
 
 curl -s -m 1 --basic -u ${BASIC_USER}:${BASIC_PASSWORD} https://${HEROKU_APP_NAME}.herokuapp.com/opcache_compile_file.php
 
