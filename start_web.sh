ee52b948a47cb742f13f5254103e4052f73320ef
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-06-21 09:47:17 +0900

    Update start_web.sh

--- start_web.sh
+++ start_web.sh
@@ -14,39 +14,9 @@ if [ ! -v BASIC_PASSWORD ]; then
   exit
 fi
 
-grep -c -e processor /proc/cpuinfo
-cat /proc/cpuinfo | head -n $(($(cat /proc/cpuinfo | wc -l) / $(grep -c -e processor /proc/cpuinfo)))
-
-httpd -V
-# httpd -M | sort
-php --version
-# php -i
-# whereis php
-# php -m
-cat /proc/version
-curl --version
-
-getconf ARG_MAX
-
-echo "$(httpd -v)" > /tmp/apache_current_version
-echo "$(php -v | head -n 1)" > /tmp/php_current_version
-echo "$(curl -V | head -n 1)" > /tmp/curl_current_version
-
-# if [ $(date +%-M) -lt 10 ]; then
-  # heroku-buildpack-php
-#   composer update > /dev/null 2>&1 &
-# fi
-
 export USER_AGENT=$(curl https://raw.githubusercontent.com/tshr20140816/heroku-mode-07/master/useragent.txt)
 
 htpasswd -c -b .htpasswd ${BASIC_USER} ${BASIC_PASSWORD}
-
-dig -t txt _netblocks.google.com | grep ^[^\;] > /tmp/_netblocks.google.com.txt
-
-pushd www
-mv ical.php ${ICS_ADDRESS}.php
-mv rss.php ${RSS_ADDRESS}.php
-popd
 
 set +x
 pushd classes
