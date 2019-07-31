1f45f4074247adcce79ba25e07384714254c7516
tshr20140816 <tshr20140816@yahoo.co.jp>
2019-08-01 08:47:55 +0900

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -7,6 +7,16 @@ date
 time pear channel-update pear.php.net
 time pear install XML_RPC2
 
+wget -q https://oscdl.ipa.go.jp/IPAexfont/ipaexg00401.zip
+
+mkdir .fonts
+mv ipaexg00401.zip .fonts/
+pushd .fonts
+unzip ipaexg00401.zip
+rm ipaexg00401.zip
+popd
+ls -lang .fonts/
+
 mkdir classes
 chmod 755 ./start_web.sh
 
